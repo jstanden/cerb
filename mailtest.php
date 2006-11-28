476ffbc0d162f54fe1d3dc6ebf0f3b123d117333
Dan Hildebrandt <hildy@webgroupmedia.com>
2006-11-28 00:38:20 +0000

    ""
    git-svn-id: http://svn.webgroupmedia.com/cerb4/cerb4@12 58cff151-b722-0410-b014-f78a459f8f02

---
+++ mailtest.php
@@ -0,0 +1,44 @@
+<?php
+echo "<html><head><title>TEST WINDOW</title></head><body>";
+
+
+
+require(getcwd() . '/framework.config.php');
+require(UM_PATH . '/libs/ump/UserMeetPlatform.class.php');
+require(UM_PATH . '/api/CerberusApplication.class.php');
+//
+//UserMeetPlatform::init();
+
+//$email = new UserMeetEmailObject(array("jeff@webgroupmedia.com"), "hildy@webgroupmedia.com", "test email from usermeet", "this email was sent through a pear implementation embedded in usermeet.");
+
+//print_r($email);
+
+//$email->send();
+
+//echo "random body text";
+//
+//if (extension_loaded("imap")) {
+//	$mailbox = imap_open("{cylon.webgroupmedia.com:110/service=pop3}INBOX","pop1","poptester")
+//		or die("Failed with error: ".imap_last_error());
+//	$check = imap_check($mailbox);
+//	
+//	for ($i=1; $i<=$check->Nmsgs; $i++) {
+//		$header = preg_replace("/\n/","<br>",preg_replace("/\r\n/","<br>",imap_fetchheader($mailbox, $i)));
+//		$body = imap_body($mailbox, $i);
+//		echo "Message with id ".$i." follows:<br><br>";
+//		echo $header."<br><br>".$body."<br><br><hr><br>";
+//	}
+//	
+//	imap_close($mailbox);	
+//} else {
+//	echo "IMAP extension not loaded!  Unable to download email!";
+//}
+
+
+$cfg = new UserMeetEmailConfig("cylon.webgroupmedia.com","110","pop3","pop1","poptester");
+$msgs = UserMeetEmailManager::getMail($cfg);
+
+print_r($msgs);
+
+echo "</body></html>";
+?>
