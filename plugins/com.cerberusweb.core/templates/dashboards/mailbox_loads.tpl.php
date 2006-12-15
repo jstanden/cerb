ac0ae8ca1991d66a73342c984c1bd3f71f02fe9e
Jeff Standen <jeff@webgroupmedia.com>
2006-12-15 07:51:40 +0000

    * Added placeholders for the Mailbox/Team graphs on Dashboards
    git-svn-id: http://svn.webgroupmedia.com/cerb4/cerb4@46 58cff151-b722-0410-b014-f78a459f8f02

--- plugins/com.cerberusweb.core/templates/dashboards/mailbox_loads.tpl.php
+++ plugins/com.cerberusweb.core/templates/dashboards/mailbox_loads.tpl.php
@@ -1,13 +1,14 @@
-<table cellpadding="0" cellspacing="0" border="0" class="tableGreen" width="200" class="tableBg">
+<table cellpadding="0" cellspacing="0" border="0" class="tableGreen" width="250" class="tableBg">
 	<tr>
 		<td class="tableThGreen" nowrap="nowrap"> <img src="images/folder_network.gif"> {$translate->say('dashboard.mailbox_loads')|capitalize}</td>
 	</tr>
 	<tr>
 		<td>
-			<table cellpadding="3" cellspacing="1" border="0" width="200" class="tableBg">
+			<table cellpadding="0" cellspacing="1" border="0" width="100%" class="tableBg">
 				{foreach from=$mailboxes item=mailbox}
 				<tr>
-					<td class="tableCellBg"><a href="index.php?c=core.module.dashboard&a=clickmailbox&id={$mailbox->id}"><b>{$mailbox->name}</b></a> (xx)</td>
+					<td class="tableCellBg" width="100%" style="padding:2px;"><a href="index.php?c=core.module.dashboard&a=clickmailbox&id={$mailbox->id}"><b>{$mailbox->name}</b></a> (xx)</td>
+					<td class="tableCellBgIndent" width="0%" nowrap="nowrap" style="width:50px;"><img src="images/cerb_graph.gif" width="30" height="15"><img src="images/cer_graph_cap.gif" height="15" width="1"></td>
 				</tr>
 				{foreachelse}
 				<tr>
--- plugins/com.cerberusweb.core/templates/dashboards/team_loads.tpl.php
+++ plugins/com.cerberusweb.core/templates/dashboards/team_loads.tpl.php
@@ -1,17 +1,18 @@
-<table cellpadding="0" cellspacing="0" border="0" class="tableOrange" width="200" class="tableBg">
+<table cellpadding="0" cellspacing="0" border="0" class="tableOrange" width="250" class="tableBg">
 	<tr>
 		<td class="tableThOrange" nowrap="nowrap"> <img src="images/businessmen.gif"> {$translate->say('dashboard.team_loads')|capitalize}</td>
 	</tr>
 	<tr>
 		<td>
-			<table cellpadding="3" cellspacing="1" border="0" width="200" class="tableBg">
+			<table cellpadding="0" cellspacing="1" border="0" width="100%" class="tableBg">
 				{foreach from=$teams item=team}
 				<tr>
-					<td class="tableCellBg"><a href="index.php?c=core.module.dashboard&a=clickteam&id={$team->id}"><b>{$team->name}</b></a> (xx)</td>
+					<td class="tableCellBg" width="100%" style="padding:2px;"><a href="index.php?c=core.module.dashboard&a=clickteam&id={$team->id}"><b>{$team->name}</b></a> (xx)</td>
+					<td class="tableCellBgIndent" width="0%" nowrap="nowrap" style="width:50px;"><img src="images/cerb_graph.gif" width="10" height="15"><img src="images/cer_graph_cap.gif" height="15" width="1"></td>
 				</tr>
 				{foreachelse}
 				<tr>
-					<td class="tableCellBg">{$translate->say('dashboard.no_teams')}</td>
+					<td colspan="2" class="tableCellBg">{$translate->say('dashboard.no_teams')}</td>
 				</tr>
 				{/foreach}
 			</table>
