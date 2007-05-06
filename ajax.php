033d6a39f332e3db54a0f1702ef1cf64e6d6e05c
Jeff Standen <jeff@webgroupmedia.com>
2007-05-06 11:48:58 +0000

    * Prioritized the Ajax request to allow resource/proxy overload.
    git-svn-id: http://svn.webgroupmedia.com/cerb4/cerb4@139 58cff151-b722-0410-b014-f78a459f8f02

--- ajax.php
+++ ajax.php
@@ -6,6 +6,8 @@ require(APP_PATH . '/api/Application.class.php');
 @$uri = DevblocksPlatform::importGPC($_REQUEST['c']); // extension
 @$listener = DevblocksPlatform::importGPC($_REQUEST['a']); // listener
 
+$request = new DevblocksHttpRequest(array($uri,$listener));
+
 // [JAS]: [TODO] Is an explicit init() really required?
 DevblocksPlatform::init();
 
@@ -18,7 +20,6 @@ $tpl->assign('session', $_SESSION);
 $tpl->assign('visit', $session->getVisit());
 $tpl->assign('settings', $settings);
 
-$request = new DevblocksHttpRequest(array($uri,$listener));
 DevblocksPlatform::processRequest($request,true);
 
 exit;
