53f33d9c9b6238d6ca47f6c6822c7f8012bcdb47
Jeff Standen <jeff@webgroupmedia.com>
2007-05-10 19:20:36 +0000

    git-svn-id: http://svn.webgroupmedia.com/cerb4/cerb4@144 58cff151-b722-0410-b014-f78a459f8f02

---
+++ plugins/cerberusweb.core/templates/footer.tpl.php
@@ -0,0 +1,11 @@
+<table align="center" border="0" cellpadding="2" cellspacing="0" width="100%">
+    <tr>
+      <td nowrap="nowrap"><b>Cerberus Helpdesk</b>&trade; &copy; Copyright 2002-2007, WebGroup Media&trade; LLC - Version 4.0 Beta (Build {$smarty.const.APP_BUILD})</td>
+      <td align="right"><a href="http://www.cerberusweb.com/"><img alt="powered by cerberus helpdesk" src="{devblocks_url}c=resource&p=cerberusweb.core&f=images/cerberus_logo_small.gif{/devblocks_url}" border="0"></a></td>
+    </tr>
+</table>
+
+<br>
+
+</body>
+</html>
---
+++ plugins/cerberusweb.core/templates/header.tpl.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+	<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+	<!--
+	<META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+	<META HTTP-EQUIV="Cache-Control" CONTENT="no-cache">
+	<META HTTP-EQUIV="Pragma-directive" CONTENT="no-cache">
+	<META HTTP-EQUIV="Cache-Directive" CONTENT="no-cache">
+	<META HTTP-EQUIV="Expires" CONTENT="0">
+	-->
+
+  <title>{$settings->get('helpdesk_title')}</title>
+  <link type="text/css" rel="stylesheet" href="{devblocks_url}styles/default/cerberus.css?v={$smarty.const.APP_BUILD}{/devblocks_url}"></link>
+  <link type="text/css" rel="stylesheet" href="{devblocks_url}styles/default/container.css?v={$smarty.const.APP_BUILD}{/devblocks_url}" />
+  <link rel="shortcut icon" href="{devblocks_url}favicon.ico{/devblocks_url}" type="image/x-icon" />
+  
+  <script language="javascript" type="text/javascript" src="{devblocks_url}c=resource&p=cerberusweb.core&f=scripts/yahoo/YAHOO.js{/devblocks_url}?v={$smarty.const.APP_BUILD}"></script>
+  <script language="javascript" type="text/javascript" src="{devblocks_url}c=resource&p=cerberusweb.core&f=scripts/yahoo/dom.js{/devblocks_url}?v={$smarty.const.APP_BUILD}"></script>
+  <script language="javascript" type="text/javascript" src="{devblocks_url}c=resource&p=cerberusweb.core&f=scripts/yahoo/event.js{/devblocks_url}?v={$smarty.const.APP_BUILD}"></script>
+  <script language="javascript" type="text/javascript" src="{devblocks_url}c=resource&p=cerberusweb.core&f=scripts/yahoo/connection.js{/devblocks_url}?v={$smarty.const.APP_BUILD}"></script>
+  <script language="javascript" type="text/javascript" src="{devblocks_url}c=resource&p=cerberusweb.core&f=scripts/yahoo/animation.js{/devblocks_url}?v={$smarty.const.APP_BUILD}"></script>
+  <script language="javascript" type="text/javascript" src="{devblocks_url}c=resource&p=cerberusweb.core&f=scripts/yahoo/dragdrop.js{/devblocks_url}?v={$smarty.const.APP_BUILD}"></script>
+  <script language="javascript" type="text/javascript" src="{devblocks_url}c=resource&p=cerberusweb.core&f=scripts/yahoo/autocomplete.js{/devblocks_url}?v={$smarty.const.APP_BUILD}"></script>
+  <script language="javascript" type="text/javascript" src="{devblocks_url}c=resource&p=cerberusweb.core&f=scripts/yahoo/container.js{/devblocks_url}?v={$smarty.const.APP_BUILD}"></script>
+
+  <script language="javascript" type="text/javascript">{php}DevblocksPlatform::printJavascriptLibrary();{/php}</script>
+  
+  <script language="javascript" type="text/javascript" src="{devblocks_url}c=resource&p=cerberusweb.core&f=scripts/cerberus/cerberus.js{/devblocks_url}?v={$smarty.const.APP_BUILD}"></script>
+  <script language="javascript" type="text/javascript" src="{devblocks_url}c=resource&p=cerberusweb.core&f=scripts/cerberus/display.js{/devblocks_url}?v={$smarty.const.APP_BUILD}"></script>
+  <script language="javascript" type="text/javascript" src="{devblocks_url}c=resource&p=cerberusweb.core&f=scripts/cerberus/config.js{/devblocks_url}?v={$smarty.const.APP_BUILD}"></script>
+  <script language="javascript" type="text/javascript" src="{devblocks_url}c=resource&p=cerberusweb.core&f=scripts/cerberus/kb.js{/devblocks_url}?v={$smarty.const.APP_BUILD}"></script>
+  
+</head>
+
+<body>
