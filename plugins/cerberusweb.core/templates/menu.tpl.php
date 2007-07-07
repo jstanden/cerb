7d2503da7c65765c43c42bfa24d0c838c2f1c5b2
Mike Fogg <mike@webgroupmedia.com>
2007-07-07 02:03:22 +0000

    * Header menu items are no longer displayed when not logged in
    git-svn-id: http://svn.webgroupmedia.com/cerb4/cerb4@188 58cff151-b722-0410-b014-f78a459f8f02

--- plugins/cerberusweb.core/templates/menu.tpl.php
+++ plugins/cerberusweb.core/templates/menu.tpl.php
@@ -1,12 +1,14 @@
 <div id="tourHeaderMenu"></div>
 <table cellpadding="0" cellspacing="0" border="0" width="100%" class="headerMenu">
 	<tr>
-		{foreach from=$pages item=m}
-			{if !empty($m->manifest->params.menutitle)}
-				<td width="0%" nowrap="nowrap" {if $page->id==$m->id}id="headerMenuSelected"{/if}><img src="{devblocks_url}c=resource&p=cerberusweb.core&f=images/spacer.gif{/devblocks_url}" width="10" height="1"><a href="{devblocks_url}c={$m->manifest->params.uri}{/devblocks_url}">{$m->manifest->params.menutitle|lower}</a><img src="{devblocks_url}c=resource&p=cerberusweb.core&f=images/spacer.gif{/devblocks_url}" width="10" height="1"></td>
-				<td width="0%" nowrap="nowrap" valign="bottom"><img src="{devblocks_url}c=resource&p=cerberusweb.core&f=images/menuSep.gif{/devblocks_url}"></td>
-			{/if}
-		{/foreach}
+		{if !empty($visit)}
+			{foreach from=$pages item=m}
+				{if !empty($m->manifest->params.menutitle)}
+					<td width="0%" nowrap="nowrap" {if $page->id==$m->id}id="headerMenuSelected"{/if}><img src="{devblocks_url}c=resource&p=cerberusweb.core&f=images/spacer.gif{/devblocks_url}" width="10" height="1"><a href="{devblocks_url}c={$m->manifest->params.uri}{/devblocks_url}">{$m->manifest->params.menutitle|lower}</a><img src="{devblocks_url}c=resource&p=cerberusweb.core&f=images/spacer.gif{/devblocks_url}" width="10" height="1"></td>
+					<td width="0%" nowrap="nowrap" valign="bottom"><img src="{devblocks_url}c=resource&p=cerberusweb.core&f=images/menuSep.gif{/devblocks_url}"></td>
+				{/if}
+			{/foreach}
+		{/if}
 		<td width="100%"><img src="{devblocks_url}c=resource&p=cerberusweb.core&f=images/spacer.gif{/devblocks_url}" height="22" width="1"></td>
 	</tr>
 </table>
