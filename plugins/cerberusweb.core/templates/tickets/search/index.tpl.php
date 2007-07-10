bb3d02973c943d646466a1fbd5f6e4210734b592
Jeff Standen <jeff@webgroupmedia.com>
2007-07-10 19:26:10 +0000

    git-svn-id: http://svn.webgroupmedia.com/cerb4/cerb4@211 58cff151-b722-0410-b014-f78a459f8f02

--- plugins/cerberusweb.core/templates/tickets/search/index.tpl.php
+++ plugins/cerberusweb.core/templates/tickets/search/index.tpl.php
@@ -9,8 +9,8 @@
 		<input type="hidden" name="c" value="tickets">
 		<input type="hidden" name="a" value="doQuickSearch">
 		<span id="tourHeaderQuickLookup"><b>Quick Search:</b></span> <select name="type">
+			<option value="sender"{if $quick_search_type eq 'sender'}selected{/if}>Sender</option>
 			<option value="mask"{if $quick_search_type eq 'mask'}selected{/if}>Ticket ID</option>
-			<option value="req"{if $quick_search_type eq 'req'}selected{/if}>Requester</option>
 			<option value="subject"{if $quick_search_type eq 'subject'}selected{/if}>Subject</option>
 			<option value="content"{if $quick_search_type eq 'content'}selected{/if}>Content</option>
 		</select><input type="text" name="query" size="24"><input type="submit" value="go!">
