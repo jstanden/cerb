1001814eb479645376644105c273f93e7ae1ba11
Mike Fogg <mike@webgroupmedia.com>
2007-08-01 20:06:35 +0000

    * Fixed problem where accessing some RSS feeds resulted in an error screen by filtering out 0xC and ESC control characters from subjects when displaying the feeds.
    git-svn-id: http://svn.webgroupmedia.com/cerb4/cerb4@305 58cff151-b722-0410-b014-f78a459f8f02

--- plugins/cerberusweb.core/classes.php
+++ plugins/cerberusweb.core/classes.php
@@ -3331,9 +3331,14 @@ XML;
             if(empty($created)) $created = time();
 
             $eItem = $channel->addChild('item');
-            $eTitle = $eItem->addChild('title', htmlentities($ticket[SearchFields_Ticket::TICKET_SUBJECT]));
-            $eDesc = $eItem->addChild('description', htmlentities($this->_getTicketLastAction($ticket)));
-            	
+            
+            $escapedSubject = htmlentities($ticket[SearchFields_Ticket::TICKET_SUBJECT]);
+            //filter out a couple non-UTF-8 characters (0xC and ESC)
+            $escapedSubject = preg_replace("/[]/", '', $escapedSubject);
+            $eTitle = $eItem->addChild('title',$escapedSubject );
+
+            $eDesc = $eItem->addChild('description', $this->_getTicketLastAction($ticket));
+            
             $url = DevblocksPlatform::getUrlService();
             $link = $url->write('c=display&id='.$ticket[SearchFields_Ticket::TICKET_MASK], true);
             $eLink = $eItem->addChild('link', $link);
