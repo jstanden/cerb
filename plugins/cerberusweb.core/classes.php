94f47de9b6d54601f3c8a195c37a8b5bfe4764e2
Mike Fogg <mike@webgroupmedia.com>
2007-08-17 01:41:25 +0000

    * Added the ability to create a new contact org
    git-svn-id: http://svn.webgroupmedia.com/cerb4/cerb4@326 58cff151-b722-0410-b014-f78a459f8f02

--- plugins/cerberusweb.core/classes.php
+++ plugins/cerberusweb.core/classes.php
@@ -3771,7 +3771,12 @@ class ChContactsPage extends CerberusPageExtension {
 				DAO_ContactOrg::WEBSITE => $website
 		);
 		
-		DAO_ContactOrg::update($id, $fields);
+		if($id==0) {
+			$id = DAO_ContactOrg::create($fields);
+		}
+		else {
+			DAO_ContactOrg::update($id, $fields);	
+		}
 		
 		DevblocksPlatform::redirect(new DevblocksHttpResponse(array('contacts','orgs',$id)));
 	}
--- plugins/cerberusweb.core/templates/contacts/orgs/index.tpl.php
+++ plugins/cerberusweb.core/templates/contacts/orgs/index.tpl.php
@@ -5,6 +5,7 @@
 		<td valign="top" width="0%" nowrap="nowrap">
 			{include file="file:$path/contacts/criteria_list.tpl.php" divName="searchCriteriaDialog"}
 			<div id="searchCriteriaDialog" style="visibility:visible;"></div>
+			<a href="{devblocks_url}c=contacts&a=orgs&id=0{/devblocks_url}" class="ticketLink" style="font-size:12px;"><b>Add Organization</b></a>
 		</td>
 		<td valign="top" width="0%" nowrap="nowrap"><img src="{devblocks_url}c=resource&p=cerberusweb.core&f=images/spacer.gif{/devblocks_url}" width="5" height="1"></td>
 		<td valign="top" width="100%">
