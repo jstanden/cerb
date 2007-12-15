6f3a9ccbc5d8ea4fcc99c1d2cb1be61405b27ea7
Jeff Standen <jeff@webgroupmedia.com>
2007-12-15 05:09:18 +0000

    * Added close/spam/delete shortcuts to the super sorter lists
    git-svn-id: http://svn.webgroupmedia.com/cerb4/cerb4@470 58cff151-b722-0410-b014-f78a459f8f02

--- plugins/cerberusweb.core/templates/tickets/rpc/ticket_view_assist.tpl.php
+++ plugins/cerberusweb.core/templates/tickets/rpc/ticket_view_assist.tpl.php
@@ -1,4 +1,4 @@
-<form action="{devblocks_url}{/devblocks_url}" method="post">
+<form action="{devblocks_url}{/devblocks_url}" method="post" id="viewAssist{$view_id}">
 <input type="hidden" name="c" value="tickets">
 <input type="hidden" name="a" value="viewAutoAssist">
 <input type="hidden" name="view_id" value="{$view_id}">
@@ -25,13 +25,13 @@ Sort biggest piles by:
 </tr>
 {foreach from=$biggest item=stats key=hash}
 <tr>
-	<td width="0%" nowrap="nowrap" align="center">
+	<td width="1%" nowrap="nowrap" align="center">
 		{if $mode=="senders"}
 		<input type="checkbox" name="piles_always[]" value="{$hash}">
 		{/if}
 	</td>
-	<td width="0%" nowrap="nowrap">
-		<select name="piles_moveto[]">
+	<td width="1%" nowrap="nowrap">
+		<select name="piles_moveto[]" id="select{$hash}">
 			<option value=""></option>
 			<optgroup label="Move to Group" style="color:rgb(0,150,0);font-weight:bold;">
 				{foreach from=$teams item=team}
@@ -54,8 +54,11 @@ Sort biggest piles by:
 				{if $active_worker && ($active_worker->is_superuser || $active_worker->can_delete)}<option value="ad">Delete</option>{/if}
 			</optgroup>
 		</select>
+		<a href="javascript:;" onclick="document.getElementById('viewAssist{$view_id}').select{$hash}.value='ac';"><img src="{devblocks_url}c=resource&p=cerberusweb.core&f=images/folder_ok.gif{/devblocks_url}" align="top" border="0"></a>
+		<a href="javascript:;" onclick="document.getElementById('viewAssist{$view_id}').select{$hash}.value='as';"><img src="{devblocks_url}c=resource&p=cerberusweb.core&f=images/spam.gif{/devblocks_url}" align="top" border="0"></a>
+		{if $active_worker && ($active_worker->is_superuser || $active_worker->can_delete)}<a href="javascript:;" onclick="document.getElementById('viewAssist{$view_id}').select{$hash}.value='ad';"><img src="{devblocks_url}c=resource&p=cerberusweb.core&f=images/delete.gif{/devblocks_url}" align="top" border="0"></a>{/if}
 	</td>
-	<td width="100%" align="top">
+	<td width="98%" align="top">
 		<input type="hidden" name="piles_hash[]" value="{$hash}">
 		<input type="hidden" name="piles_type[]" value="{$stats[0]}">
 		<input type="hidden" name="piles_type_param[]" value="{$stats[4]}">
@@ -66,13 +69,13 @@ Sort biggest piles by:
 {if !empty($stats[3]) && is_array($stats[3])} {*$stats[0] == 'domain' && *}
 	{foreach from=$stats[3] item=sender key=sender_hash}
 	<tr>
-		<td width="0%" nowrap="nowrap" align="center">
+		<td width="1%" nowrap="nowrap" align="center">
 			{if $mode=="senders"}
 			<input type="checkbox" name="piles_always[]" value="{$sender_hash}">
 			{/if}
 		</td>
-		<td width="0%" nowrap="nowrap">
-			<select name="piles_moveto[]">
+		<td width="1%" nowrap="nowrap">
+			<select name="piles_moveto[]" id="select{$sender_hash}">
 				<option value=""></option>
 				<optgroup label="Move to Group" style="color:rgb(0,150,0);font-weight:bold;">
 					{foreach from=$teams item=team}
@@ -95,8 +98,11 @@ Sort biggest piles by:
 					<option value="ad">Delete</option>
 				</optgroup>
 			</select>
+			<a href="javascript:;" onclick="document.getElementById('viewAssist{$view_id}').select{$sender_hash}.value='ac';"><img src="{devblocks_url}c=resource&p=cerberusweb.core&f=images/folder_ok.gif{/devblocks_url}" align="top" border="0"></a>
+			<a href="javascript:;" onclick="document.getElementById('viewAssist{$view_id}').select{$sender_hash}.value='as';"><img src="{devblocks_url}c=resource&p=cerberusweb.core&f=images/spam.gif{/devblocks_url}" align="top" border="0"></a>
+			{if $active_worker && ($active_worker->is_superuser || $active_worker->can_delete)}<a href="javascript:;" onclick="document.getElementById('viewAssist{$view_id}').select{$sender_hash}.value='ad';"><img src="{devblocks_url}c=resource&p=cerberusweb.core&f=images/delete.gif{/devblocks_url}" align="top" border="0"></a>{/if}
 		</td>
-		<td>
+		<td width="98%">
 			<blockquote style="margin-bottom:0px;">
 				<input type="hidden" name="piles_hash[]" value="{$sender_hash}">
 				<input type="hidden" name="piles_type[]" value="{$sender[0]}">
