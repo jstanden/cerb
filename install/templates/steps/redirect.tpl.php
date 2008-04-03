9cc763a56928ff6a7205026a2659e300a326b52d
Mike Fogg <mike@webgroupmedia.com>
2008-04-03 22:23:24 +0000

    * CHD-601: Fixed bug where running the installer using Opera resulted in multiple redirects and incomplete installation.
    git-svn-id: http://svn.webgroupmedia.com/cerb4/cerb4@570 58cff151-b722-0410-b014-f78a459f8f02

--- install/templates/steps/redirect.tpl.php
+++ install/templates/steps/redirect.tpl.php
@@ -3,17 +3,9 @@
 	<title>Cerberus Helpdesk - Web Installer</title>
 	<link rel="stylesheet" href="install.css" type="text/css">
 	<meta http-equiv="refresh" content="1;url=index.php?step={$step}">
-	
-<script language="javascript">
-
- function onward() {literal}{{/literal}
- 	setTimeout("window.location.replace('index.php?step={$step}')",2);
- {literal}}{/literal}
-
-</script>	
 </head>
 
-<body onload="onward()">
+<body>
 
 <H1>Installing Cerberus Helpdesk 4.0</H1>
 Please wait...
