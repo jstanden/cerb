3010895333edf67a6018d5dd987381fac048dd31
Jeff Standen <jeff@webgroupmedia.com>
2009-09-04 10:56:29 -0700

    Version 4.3 (Build 935)

--- api/Application.class.php
+++ api/Application.class.php
@@ -48,7 +48,7 @@
  * 		and Joe Geck.
  *   WEBGROUP MEDIA LLC. - Developers of Cerberus Helpdesk
  */
-define("APP_BUILD", 934);
+define("APP_BUILD", 935);
 define("APP_MAIL_PATH", APP_STORAGE_PATH . '/mail/');
 
 require_once(APP_PATH . "/api/DAO.class.php");
