997b03e5dcf997ef1afbdc39dc2b1740f68ad6d7
Jeff Standen <jeff@webgroupmedia.com>
2009-11-17 18:22:07 -0800

    * [Mail] [CHD-1452] cerberus(at)localhost is being added to the list of requesters when using 'Open Ticket' with 'reply to all' enabled.
    * [Mail] [CHD-1453] Group reply addresses not being used in reply-to header on tickets created from 'Open Ticket'.

--- plugins/cerberusweb.core/api/uri/tickets.php
+++ plugins/cerberusweb.core/api/uri/tickets.php
@@ -1056,7 +1056,7 @@ class ChTicketsPage extends CerberusPageExtension {
 		
 		$message = new CerberusParserMessage();
 		$message->headers['date'] = date('r'); 
-		$message->headers['to'] = 'cerberus@localhost';
+		$message->headers['to'] = $to;
 		$message->headers['subject'] = $subject;
 		$message->headers['message-id'] = CerberusApplication::generateMessageId();
 		//$message->headers['x-cerberus-portal'] = 1; 
