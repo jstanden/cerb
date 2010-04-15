8ca4edc8a8e5c7a7e3b371d10b843360052d5ece
Jeff Standen <jeff@webgroupmedia.com>
2010-04-15 15:36:23 -0700

    * [Snippets] Added an 'opportunity' context to snippets.  This also links the lead to the 'e-mail address' context, and links the current worker and assignee to the 'worker' context.

--- api/Application.class.php
+++ api/Application.class.php
@@ -635,6 +635,7 @@ class CerberusSnippetContexts {
 	const CONTEXT_BUCKET = 'cerberusweb.snippets.bucket';
 	const CONTEXT_GROUP = 'cerberusweb.snippets.group';
 	const CONTEXT_MESSAGE = 'cerberusweb.snippets.message';
+	const CONTEXT_OPPORTUNITY = 'cerberusweb.snippets.opportunity';
 	const CONTEXT_ORG = 'cerberusweb.snippets.org';
 	const CONTEXT_TICKET = 'cerberusweb.snippets.ticket';
 	const CONTEXT_WORKER = 'cerberusweb.snippets.worker';
@@ -652,6 +653,9 @@ class CerberusSnippetContexts {
 				break;
 			case 'cerberusweb.snippets.message':
 				self::_getMessageContext($context_object, $labels, $values, $prefix);
+				break;
+			case 'cerberusweb.snippets.opportunity':
+				self::_getOpportunityContext($context_object, $labels, $values, $prefix);
 				break;
 			case 'cerberusweb.snippets.org':
 				self::_getOrganizationContext($context_object, $labels, $values, $prefix);
@@ -1268,6 +1272,127 @@ class CerberusSnippetContexts {
 
 		return true;
 	}
+	
+	/**
+	 * 
+	 * @param mixed $opp
+	 * @param array $token_labels
+	 * @param array $token_values
+	 */
+	private static function _getOpportunityContext($opp, &$token_labels, &$token_values, $prefix=null) {
+		if(is_null($prefix))
+			$prefix = 'Opportunity:';
+		
+		$translate = DevblocksPlatform::getTranslationService();
+		$fields = DAO_CustomField::getBySource(CrmCustomFieldSource_Opportunity::ID);
+
+		// Polymorph
+		if(is_numeric($opp)) {
+			$opp = DAO_CrmOpportunity::get($opp);
+		} elseif($opp instanceof Model_CrmOpportunity) {
+			// It's what we want already.
+		} else {
+			$opp = null;
+		}
+		
+		// Token labels
+		$token_labels = array(
+			'created|date' => $prefix.$translate->_('crm.opportunity.created_date'),
+			'is_closed' => $prefix.$translate->_('crm.opportunity.is_closed'),
+			'is_won' => $prefix.$translate->_('crm.opportunity.is_won'),
+			'title' => $prefix.$translate->_('crm.opportunity.name'),
+			'updated|date' => $prefix.$translate->_('crm.opportunity.updated_date'),
+		);
+		
+		if(is_array($fields))
+		foreach($fields as $cf_id => $field) {
+			$token_labels['custom_'.$cf_id] = $prefix.$field->name;
+		}
+
+		// Token values
+		$token_values = array();
+		
+		// Opp token values
+		if($opp) {
+			$token_values['created'] = $opp->created_date;
+			$token_values['is_closed'] = $opp->is_closed;
+			$token_values['is_won'] = $opp->is_won;
+			$token_values['title'] = $opp->name;
+			$token_values['updated'] = $opp->updated_date;
+//			if(!empty($org->city))
+//				$token_values['city'] = $org->city;
+
+			$token_values['custom'] = array();
+			
+			$field_values = array_shift(DAO_CustomFieldValue::getValuesBySourceIds(CrmCustomFieldSource_Opportunity::ID, $opp->id));
+			if(is_array($field_values) && !empty($field_values)) {
+				foreach($field_values as $cf_id => $cf_val) {
+					if(!isset($fields[$cf_id]))
+						continue;
+					
+					// The literal value
+					if(null != $opp)
+						$token_values['custom'][$cf_id] = $cf_val;
+					
+					// Stringify
+					if(is_array($cf_val))
+						$cf_val = implode(', ', $cf_val);
+						
+					if(is_string($cf_val)) {
+						if(null != $opp)
+							$token_values['custom_'.$cf_id] = $cf_val;
+					}
+				}
+			}
+		}
+		
+		// Person
+		@$address_id = $opp->primary_email_id;
+		$merge_token_labels = array();
+		$merge_token_values = array();
+		self::getContext(self::CONTEXT_ADDRESS, $address_id, $merge_token_labels, $merge_token_values, '', true);
+
+		self::_merge(
+			'email_',
+			'Lead:',
+			$merge_token_labels,
+			$merge_token_values,
+			$token_labels,
+			$token_values
+		);
+		
+		// Current worker
+		$active_worker = CerberusApplication::getActiveWorker();
+		$merge_token_labels = array();
+		$merge_token_values = array();
+		self::getContext(self::CONTEXT_WORKER, $active_worker, $merge_token_labels, $merge_token_values, '', true);
+
+		self::_merge(
+			'worker_',
+			'Current:Worker:',
+			$merge_token_labels,
+			$merge_token_values,
+			$token_labels,
+			$token_values
+		);
+		
+		// Assignee
+		@$assignee_id = $opp->worker_id;
+		$merge_token_labels = array();
+		$merge_token_values = array();
+		self::getContext(self::CONTEXT_WORKER, $assignee_id, $merge_token_labels, $merge_token_values, '', true);
+
+		self::_merge(
+			'assignee_',
+			'Assignee:',
+			$merge_token_labels,
+			$merge_token_values,
+			$token_labels,
+			$token_values
+		);		
+		
+		return true;
+	}	
 	
 	/**
 	 * 
