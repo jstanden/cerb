81a6b18c7346bf93bea855fd5675ecaf3f715cd6
Jeff Standen <jeff@webgroupmedia.com>
2010-04-23 18:51:35 -0700

    [Web-API] Added time tracking 'GET /timetracking/1.[json|xml]' and 'POST /timetracking/search.[json|xml]'

---
+++ features/cerberusweb.timetracking/api/rest/timetracking.php
@@ -0,0 +1,142 @@
+<?php
+class ChRest_TimeTracking extends Extension_RestController implements IExtensionRestController {
+	function __construct($manifest) {
+		parent::__construct($manifest);
+	}
+	
+	function getAction($stack) {
+		@$action = array_shift($stack);
+		
+		// Looking up a single ID?
+		if(is_numeric($action)) {
+			$this->getId(intval($action));
+			
+		} else { // actions
+			switch($action) {
+				default:
+					$this->error(self::ERRNO_NOT_IMPLEMENTED);
+					break;
+			}
+		}
+	}
+	
+	function putAction($stack) {
+		$this->error(self::ERRNO_NOT_IMPLEMENTED);
+	}
+	
+	function postAction($stack) {
+		@$action = array_shift($stack);
+		
+		switch($action) {
+			case 'search':
+				$this->postSearch();
+				break;
+		}
+		
+		$this->error(self::ERRNO_NOT_IMPLEMENTED);
+	}
+	
+	function deleteAction($stack) {
+		$this->error(self::ERRNO_NOT_IMPLEMENTED);
+	}
+	
+	private function getId($id) {
+		$worker = $this->getActiveWorker();
+		
+		// ACL
+		if(!$worker->hasPriv('plugin.cerberusweb.timetracking'))
+			$this->error(self::ERRNO_ACL);
+
+		$container = $this->search(array(
+			array('id', '=', $id),
+		));
+		
+		if(is_array($container) && isset($container['results']) && isset($container['results'][$id]))
+			$this->success($container['results'][$id]);
+
+		// Error
+		$this->error(self::ERRNO_CUSTOM, sprintf("Invalid time entry id '%d'", $id));
+	}
+
+	function translateToken($token, $type='dao') {
+		$tokens = array();
+		
+		if('dao'==$type) {
+			$tokens = array(
+//				'is_banned' => DAO_Address::IS_BANNED,
+//				'is_registered' => DAO_Address::IS_REGISTERED,
+			);
+		} else {
+			$tokens = array(
+				'created' => SearchFields_TimeTrackingEntry::LOG_DATE,
+				'id' => SearchFields_TimeTrackingEntry::ID,
+				'org_id' => SearchFields_TimeTrackingEntry::DEBIT_ORG_ID,
+				'org_name' => SearchFields_TimeTrackingEntry::ORG_NAME,
+				'mins' => SearchFields_TimeTrackingEntry::TIME_ACTUAL_MINS,
+				'worker_id' => SearchFields_TimeTrackingEntry::WORKER_ID,
+			);
+		}
+		
+		if(isset($tokens[$token]))
+			return $tokens[$token];
+		
+		return NULL;
+	}	
+	
+	function getContext($id) {
+		$labels = array();
+		$values = array();
+		$context = CerberusContexts::getContext(CerberusContexts::CONTEXT_TIMETRACKING, $id, $labels, $values, null, true);
+
+		return $values;
+	}
+	
+	function search($filters=array(), $sortToken='id', $sortAsc=1, $page=1, $limit=10) {
+		$worker = $this->getActiveWorker();
+
+		$params = $this->_handleSearchBuildParams($filters);
+		
+		// Sort
+		$sortBy = $this->translateToken($sortToken, 'search');
+		$sortAsc = !empty($sortAsc) ? true : false;
+		
+		// Search
+		list($results, $total) = DAO_TimeTrackingEntry::search(
+			array(),
+			$params,
+			$limit,
+			max(0,$page-1),
+			$sortBy,
+			$sortAsc,
+			true
+		);
+		
+		$objects = array();
+		
+		foreach($results as $id => $result) {
+			$values = $this->getContext($id);
+			$objects[$id] = $values;
+		}
+		
+		$container = array(
+			'total' => $total,
+			'count' => count($objects),
+			'page' => $page,
+			'results' => $objects,
+		);
+		
+		return $container;		
+	}
+	
+	function postSearch() {
+		$worker = $this->getActiveWorker();
+		
+		// ACL
+		if(!$worker->hasPriv('plugin.cerberusweb.timetracking'))
+			$this->error(self::ERRNO_ACL);
+
+		$container = $this->_handlePostSearch();
+		
+		$this->success($container);
+	}
+};
