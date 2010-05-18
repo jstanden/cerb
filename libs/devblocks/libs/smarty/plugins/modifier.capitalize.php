b4bb0849a7393fa8daaa203ef44d0fc1f50842b4
Jeff Standen <jeff@webgroupmedia.com>
2010-05-18 11:48:46 -0700

    * [CHD-1890] [Translations] Smarty 'capitalize' modifier considers umlauts to be word breaks.

--- libs/devblocks/libs/smarty/plugins/modifier.capitalize.php
+++ libs/devblocks/libs/smarty/plugins/modifier.capitalize.php
@@ -18,24 +18,13 @@
  * @param string
  * @return string
  */
-function smarty_modifier_capitalize($string, $uc_digits = false)
+function smarty_modifier_capitalize($string)
 {
-    smarty_modifier_capitalize_ucfirst(null, $uc_digits);
-    return preg_replace_callback('!\'?\b\w(\w|\')*\b!', 'smarty_modifier_capitalize_ucfirst', $string);
-}
-
-function smarty_modifier_capitalize_ucfirst($string, $uc_digits = null)
-{
-    static $_uc_digits = false;
-    
-    if(isset($uc_digits)) {
-        $_uc_digits = $uc_digits;
-        return;
-    }
-    
-    if(substr($string[0],0,1) != "'" && !preg_match("!\d!",$string[0]) || $_uc_digits)
-        return ucfirst($string[0]);
-    else
-        return $string[0];
+	// Jeff@WGM
+	if(extension_loaded('mbstring')) {
+		return mb_convert_case($string, MB_CASE_TITLE);
+	} else {
+		return $string;
+	}
 }
 ?>
