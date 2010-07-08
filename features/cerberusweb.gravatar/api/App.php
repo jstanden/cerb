8bada7ee4055fea8c55f9109c7f928be6ab1fb34
Jeff Standen <jeff@webgroupmedia.com>
2010-07-08 12:37:10 -0700

    * [Gravatar/Support Center] The Gravatar plugin will now render profile pictures next to e-mail addresses in ticket history.

--- features/cerberusweb.gravatar/api/App.php
+++ features/cerberusweb.gravatar/api/App.php
@@ -13,7 +13,7 @@ class WgmGravatarCommentBadge extends Extension_CommentBadge {
 	}
 };
 
-class WgmGravatarMessageBadge extends Extension_CommentBadge {
+class WgmGravatarMessageBadge extends Extension_MessageBadge {
 	function __construct($manifest) {
 		parent::__construct($manifest);
 	}
@@ -26,3 +26,18 @@ class WgmGravatarMessageBadge extends Extension_CommentBadge {
 		}
 	}
 };
+
+class WgmGravatarSupportCenterMessageBadge extends Extension_SupportCenterMessageBadge {
+	function __construct($manifest) {
+		parent::__construct($manifest);
+	}
+	
+	function render(Model_Message $message) {
+		if(null != ($email_address = $message->getSender())) {
+			$tpl = DevblocksPlatform::getTemplateService();
+			$tpl->assign('gravatar_email', $email_address->email);
+			$tpl->display('devblocks:cerberusweb.gravatar::gravatar_icon.tpl');
+		}
+	}
+};
+
