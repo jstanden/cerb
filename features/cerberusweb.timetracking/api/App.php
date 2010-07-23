a57dc67902ba06397fb287c7442db8c1bac450a2
Jeff Standen <jeff@webgroupmedia.com>
2010-07-22 19:00:41 -0700

    * [Reports/Time Tracking/Time Spent per Worker] The report chart has been drastically improved with a time dimension to show trends in the data over time.  Times can be displayed as histograms of days, months, or years.  Multiple workers are now rendered as series that can be displayed and compared over time as a line chart, stacked line chart, bar chart, or stacked bar chart.

--- features/cerberusweb.timetracking/api/App.php
+++ features/cerberusweb.timetracking/api/App.php
@@ -1654,31 +1654,64 @@ class ChReportTimeSpentWorker extends Extension_Report {
 		
 		mysql_free_result($rs);
 		
+		// Calculate the # of ticks between the dates (and the scale -- day, month, etc)
+		$range = $end_time - $start_time;
+		$range_days = $range/86400;
+		$plots = $range/15;
+		
+		$ticks = array();
+		
+		if($range_days > 365) {
+			$date_group = '%Y';
+			$date_increment = 'year';
+		} elseif($range_days > 32) {
+			$date_group = '%Y-%m';
+			$date_increment = 'month';
+		} elseif($range_days > 1) {
+			$date_group = '%Y-%m-%d';
+			$date_increment = 'day';
+		} else {
+			$date_group = '%Y-%m-%d %H';
+			$date_increment = 'hour';
+		}
+		
+		// Find unique values
+		$time = strtotime(sprintf("-1 %s", $date_increment), $start_time);
+		while($time < $end_time) {
+			$time = strtotime(sprintf("+1 %s", $date_increment), $time);
+			$ticks[strftime($date_group, $time)] = 0;
+		}
+
 		// Chart
 		
-		$sql = sprintf("SELECT sum(tte.time_actual_mins) mins, tte.worker_id, w.first_name, w.last_name ".
-				"FROM timetracking_entry tte ".
-				"INNER JOIN worker w ON tte.worker_id = w.id ".
-				"WHERE log_date > %d AND log_date <= %d ".
-				"GROUP BY tte.worker_id, w.first_name, w.last_name ".
-				"ORDER BY w.first_name desc, w.last_name desc ",
-				$start_time,
-				$end_time
-				);
+		$sql = sprintf("SELECT tte.worker_id, DATE_FORMAT(FROM_UNIXTIME(tte.log_date),'%s') AS date_plot, ".
+			"SUM(tte.time_actual_mins) AS mins, w.first_name, w.last_name ".
+			"FROM timetracking_entry tte ".
+			"INNER JOIN worker w ON tte.worker_id = w.id ".
+			"WHERE log_date > %d AND log_date <= %d ".
+			"GROUP BY tte.worker_id, date_plot, w.first_name, w.last_name ".
+			"ORDER BY w.first_name desc, w.last_name desc ",
+			$date_group,
+			$start_time,
+			$end_time
+		);
 		$rs = $db->Execute($sql);
-
+		
 		$data = array();
-		$iter = 0;
-	    
-	    while($row = mysql_fetch_assoc($rs)) {
-	    	$mins = intval($row['mins']);
-			$worker_name = $row['first_name'] . ' ' . $row['last_name'];
-
-			$data[$iter++] = array('value'=>$worker_name,'mins'=>$mins);
-	    }
-	    $tpl->assign('data', $data);
-	    
-	    mysql_free_result($rs);
+		while($row = mysql_fetch_assoc($rs)) {
+			$worker_id = intval($row['worker_id']);
+			$date_plot = $row['date_plot'];
+			
+			if(!isset($data[$worker_id]))
+				$data[$worker_id] = $ticks;
+			
+			$data[$worker_id][$date_plot] = intval($row['mins']);
+		}
+		
+		$tpl->assign('xaxis_ticks', array_keys($ticks));
+		$tpl->assign('data', $data);
+		
+		mysql_free_result($rs);		
 		
 		// Template
 		
