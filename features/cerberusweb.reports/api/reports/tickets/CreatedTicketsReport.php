521ef6d1e0880ff994ab58840b29d0d9019f6e79
Jeff Standen <jeff@webgroupmedia.com>
2010-07-25 12:37:41 -0700

    * [Reports/Created Tickets by Group] Revamped the report.  Added the time dimension to track trends over time.  Times can be displayed as histograms of days, months, or years.  Multiple groups are now rendered as series that can be displayed and compared over time as a line chart, stacked line chart, bar chart, or stacked bar chart.  Added a groups chooser to filter to specific groups.  Users can now override the date grouping (day,month,year) for histograms.  The report table will now display all the date-grouped data w/ sum and mean.  Implemented a custom legend that displays outside the chart without obscuring it.  If there are more than 90 ticks on the x-axis then the labels are hidden.  You can now hover over a series on the 'filled line' chart to highlight it.  Tooltips are now displayed when you hover over data on the chart.

--- features/cerberusweb.reports/api/reports/tickets/CreatedTicketsReport.php
+++ features/cerberusweb.reports/api/reports/tickets/CreatedTicketsReport.php
@@ -5,12 +5,14 @@ class ChReportNewTickets extends Extension_Report {
 	}
 	
 	function render() {
+		$db = DevblocksPlatform::getDatabaseService();
 		$tpl = DevblocksPlatform::getTemplateService();
+
+		@$filter_group_ids = DevblocksPlatform::importGPC($_REQUEST['group_id'],'array',array());
+		$tpl->assign('filter_group_ids', $filter_group_ids);
 		
 		@$start = DevblocksPlatform::importGPC($_REQUEST['start'],'string','-30 days');
 		@$end = DevblocksPlatform::importGPC($_REQUEST['end'],'string','now');
-		
-		$db = DevblocksPlatform::getDatabaseService();
 
 		// Start + End
 		@$start_time = strtotime($start);
@@ -44,19 +46,44 @@ class ChReportNewTickets extends Extension_Report {
 		
 		$ticks = array();
 		
-		if($range_days > 365) {
-			$date_group = '%Y';
-			$date_increment = 'year';
-		} elseif($range_days > 32) {
-			$date_group = '%Y-%m';
-			$date_increment = 'month';
-		} elseif($range_days > 1) {
-			$date_group = '%Y-%m-%d';
-			$date_increment = 'day';
-		} else {
-			$date_group = '%Y-%m-%d %H';
-			$date_increment = 'hour';
+		@$report_date_grouping = DevblocksPlatform::importGPC($_REQUEST['report_date_grouping'],'string','');
+		$date_group = '';
+		$date_increment = '';
+		
+		// Did the user choose a specific grouping?
+		switch($report_date_grouping) {
+			case 'year':
+				$date_group = '%Y';
+				$date_increment = 'year';
+				break;
+			case 'month':
+				$date_group = '%Y-%m';
+				$date_increment = 'month';
+				break;
+			case 'day':
+				$date_group = '%Y-%m-%d';
+				$date_increment = 'day';
+				break;
 		}
+		
+		// Fallback to automatic grouping
+		if(empty($date_group) || empty($date_increment)) {
+			if($range_days > 365) {
+				$date_group = '%Y';
+				$date_increment = 'year';
+			} elseif($range_days > 32) {
+				$date_group = '%Y-%m';
+				$date_increment = 'month';
+			} elseif($range_days > 1) {
+				$date_group = '%Y-%m-%d';
+				$date_increment = 'day';
+			} else {
+				$date_group = '%Y-%m-%d %H';
+				$date_increment = 'hour';
+			}
+		}
+		
+		$tpl->assign('report_date_grouping', $date_increment);
 		
 		// Find unique values
 		$time = strtotime(sprintf("-1 %s", $date_increment), $start_time);
@@ -67,19 +94,20 @@ class ChReportNewTickets extends Extension_Report {
 		}
 		
 		// Chart
-		$sql = sprintf("SELECT team.id as group_id, DATE_FORMAT(FROM_UNIXTIME(t.created_date),'%s') as date_plot, ".
-			"count(*) as hits ".
-			"FROM ticket t inner join team on t.team_id = team.id ".
+		$sql = sprintf("SELECT t.team_id AS group_id, DATE_FORMAT(FROM_UNIXTIME(t.created_date),'%s') AS date_plot, ".
+			"COUNT(*) AS hits ".
+			"FROM ticket t ".
 			"WHERE t.created_date >= %d ".
 			"AND t.created_date <= %d ".
+			"%s ".
 			"AND t.is_deleted = 0 ".
 			"AND t.spam_score < 0.9000 ".
 			"AND t.spam_training != 'S' ".
-			"GROUP BY group_id, date_plot ".
-			"ORDER BY hits DESC",
+			"GROUP BY group_id, date_plot ",
 			$date_group,
 			$start_time,
-			$end_time
+			$end_time,
+			(is_array($filter_group_ids) && !empty($filter_group_ids) ? sprintf("AND t.team_id IN (%s)", implode(',', $filter_group_ids)) : "")
 		);
 		$rs = $db->Execute($sql);
 		
@@ -94,40 +122,14 @@ class ChReportNewTickets extends Extension_Report {
 			$data[$group_id][$date_plot] = intval($row['hits']);
 		}
 		
+		// Sort the data in descending order
+		uasort($data, array('ChReportSorters','sortDataDesc'));
+		
 		$tpl->assign('xaxis_ticks', array_keys($ticks));
 		$tpl->assign('data', $data);
 		
 		mysql_free_result($rs);
 
-		// Table
-		$sql = sprintf("SELECT count(*) AS hits, team_id, category_id ".
-			"FROM ticket ".
-			"WHERE created_date > %d AND created_date <= %d ".
-			"AND is_deleted = 0 ".
-			"AND spam_score < 0.9000 ".
-			"AND spam_training != 'S' ".
-			"GROUP BY team_id, category_id ",
-			$start_time,
-			$end_time
-		);
-		$rs = $db->Execute($sql);
-	
-		$group_counts = array();
-		while($row = mysql_fetch_assoc($rs)) {
-			$team_id = intval($row['team_id']);
-			$category_id = intval($row['category_id']);
-			$hits = intval($row['hits']);
-			
-			if(!isset($group_counts[$team_id]))
-				$group_counts[$team_id] = array();
-				
-			$group_counts[$team_id][$category_id] = $hits;
-			@$group_counts[$team_id]['total'] = intval($group_counts[$team_id]['total']) + $hits;
-		}
-		$tpl->assign('group_counts', $group_counts);
-		
-		mysql_free_result($rs);
-		
 		$tpl->display('devblocks:cerberusweb.reports::reports/ticket/new_tickets/index.tpl');
 	}
 };
