6bcea0933ab3200da820373b557f59ef0d2b6ad6
Jeff Standen <jeff@webgroupmedia.com>
2010-08-26 08:56:34 -0700

    * [Platform/Windows] Replaced a getcwd() call with dirname(__FILE__) for index.php and ajax.php, which should fix another path issue with Windows/IIS during the installation process.

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
 if(version_compare(PHP_VERSION, "5.2", "<"))
 	die("Cerberus Helpdesk 5.x requires PHP 5.2 or later.");
 
-require(getcwd() . '/framework.config.php');
+require(dirname(__FILE__) . '/framework.config.php');
 require(DEVBLOCKS_PATH . 'Devblocks.class.php');
 
 // If this is our first run, redirect to the installer
