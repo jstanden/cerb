c3b61ba0f55281e3ecca1192ba2f09ed57bba008
Jeff Standen <jeff@webgroupmedia.com>
2010-09-07 18:04:32 -0700

    5.1.1-dev (Build 2010090701)

--- api/Application.class.php
+++ api/Application.class.php
@@ -47,8 +47,8 @@
  * 		and Jerry Kanoholani. 
  *	 WEBGROUP MEDIA LLC. - Developers of Cerberus Helpdesk
  */
-define("APP_BUILD", 2010082601);
-define("APP_VERSION", '5.1.0');
+define("APP_BUILD", 2010090701);
+define("APP_VERSION", '5.1.1-dev');
 define("APP_MAIL_PATH", APP_STORAGE_PATH . '/mail/');
 
 require_once(APP_PATH . "/api/DAO.class.php");
