a37b0f4d2854e12cc5c9b409e1c4e3fd7b4a6e60
Jeff Standen <jeff@webgroupmedia.com>
2010-10-08 17:12:01 -0700

    Squash with contact person commit

---
+++ features/cerberusweb.core/api/dao/contact_person.php
@@ -0,0 +1,518 @@
+<?php
+class DAO_ContactPerson extends DevblocksORMHelper {
+	const ID = 'id';
+	const EMAIL_ID = 'email_id';
+	const CREATED = 'created';
+	const LAST_LOGIN = 'last_login';
+	const AUTH_SALT = 'auth_salt';
+	const AUTH_PASSWORD = 'auth_password';
+
+	static function create($fields) {
+		$db = DevblocksPlatform::getDatabaseService();
+		
+		$sql = "INSERT INTO contact_person () VALUES ()";
+		$db->Execute($sql);
+		$id = $db->LastInsertId();
+		
+		self::update($id, $fields);
+		
+		return $id;
+	}
+	
+	static function update($ids, $fields) {
+		parent::_update($ids, 'contact_person', $fields);
+	}
+	
+	static function updateWhere($fields, $where) {
+		parent::_updateWhere('contact_person', $fields, $where);
+	}
+	
+	/**
+	 * @param string $where
+	 * @param mixed $sortBy
+	 * @param mixed $sortAsc
+	 * @param integer $limit
+	 * @return Model_ContactPerson[]
+	 */
+	static function getWhere($where=null, $sortBy=null, $sortAsc=true, $limit=null) {
+		$db = DevblocksPlatform::getDatabaseService();
+
+		list($where_sql, $sort_sql, $limit_sql) = self::_getWhereSQL($where, $sortBy, $sortAsc, $limit);
+		
+		// SQL
+		$sql = "SELECT id, email_id, created, last_login, auth_salt, auth_password ".
+			"FROM contact_person ".
+			$where_sql.
+			$sort_sql.
+			$limit_sql
+		;
+		$rs = $db->Execute($sql);
+		
+		return self::_getObjectsFromResult($rs);
+	}
+
+	/**
+	 * @param integer $id
+	 * @return Model_ContactPerson	 */
+	static function get($id) {
+		$objects = self::getWhere(sprintf("%s = %d",
+			self::ID,
+			$id
+		));
+		
+		if(isset($objects[$id]))
+			return $objects[$id];
+		
+		return null;
+	}
+	
+	/**
+	 * @param resource $rs
+	 * @return Model_ContactPerson[]
+	 */
+	static private function _getObjectsFromResult($rs) {
+		$objects = array();
+		
+		while($row = mysql_fetch_assoc($rs)) {
+			$object = new Model_ContactPerson();
+			$object->id = intval($row['id']);
+			$object->email_id = intval($row['email_id']);
+			$object->created = intval($row['created']);
+			$object->last_login = intval($row['last_login']);
+			$object->auth_salt = $row['auth_salt'];
+			$object->auth_password = $row['auth_password'];
+			$objects[$object->id] = $object;
+		}
+		
+		mysql_free_result($rs);
+		
+		return $objects;
+	}
+	
+	static function delete($ids) {
+		if(!is_array($ids)) $ids = array($ids);
+		$db = DevblocksPlatform::getDatabaseService();
+		
+		if(empty($ids))
+			return;
+		
+		$ids_list = implode(',', $ids);
+		
+		$db->Execute(sprintf("DELETE FROM contact_person WHERE id IN (%s)", $ids_list));
+		
+		return true;
+	}
+	
+	public static function getSearchQueryComponents($columns, $params, $sortBy=null, $sortAsc=null) {
+		$fields = SearchFields_ContactPerson::getFields();
+		
+		// Sanitize
+		if(!isset($fields[$sortBy]))
+			$sortBy=null;
+
+        list($tables,$wheres) = parent::_parseSearchParams($params, $columns, $fields, $sortBy);
+		
+		$select_sql = sprintf("SELECT ".
+			"contact_person.id as %s, ".
+			"contact_person.email_id as %s, ".
+			"contact_person.created as %s, ".
+			"contact_person.last_login as %s, ".
+			"contact_person.auth_salt as %s, ".
+			"contact_person.auth_password as %s, ".
+			"address.first_name as %s, ".
+			"address.last_name as %s, ".
+			"address.email as %s ",
+				SearchFields_ContactPerson::ID,
+				SearchFields_ContactPerson::EMAIL_ID,
+				SearchFields_ContactPerson::CREATED,
+				SearchFields_ContactPerson::LAST_LOGIN,
+				SearchFields_ContactPerson::AUTH_SALT,
+				SearchFields_ContactPerson::AUTH_PASSWORD,
+				SearchFields_ContactPerson::ADDRESS_FIRST_NAME,
+				SearchFields_ContactPerson::ADDRESS_LAST_NAME,
+				SearchFields_ContactPerson::ADDRESS_EMAIL
+			);
+			
+		$join_sql = "FROM contact_person ".
+			"INNER JOIN address ON (contact_person.email_id=address.id) "
+			;
+		
+		// Custom field joins
+		//list($select_sql, $join_sql, $has_multiple_values) = self::_appendSelectJoinSqlForCustomFieldTables(
+		//	$tables,
+		//	$params,
+		//	'contact_person.id',
+		//	$select_sql,
+		//	$join_sql
+		//);
+		$has_multiple_values = false; // [TODO] Temporary when custom fields disabled
+				
+		$where_sql = "".
+			(!empty($wheres) ? sprintf("WHERE %s ",implode(' AND ',$wheres)) : "WHERE 1 ");
+			
+		$sort_sql = (!empty($sortBy)) ? sprintf("ORDER BY %s %s ",$sortBy,($sortAsc || is_null($sortAsc))?"ASC":"DESC") : " ";
+	
+		return array(
+			'primary_table' => 'contact_person',
+			'select' => $select_sql,
+			'join' => $join_sql,
+			'where' => $where_sql,
+			'has_multiple_values' => $has_multiple_values,
+			'sort' => $sort_sql,
+		);
+	}
+	
+    /**
+     * Enter description here...
+     *
+     * @param array $columns
+     * @param DevblocksSearchCriteria[] $params
+     * @param integer $limit
+     * @param integer $page
+     * @param string $sortBy
+     * @param boolean $sortAsc
+     * @param boolean $withCounts
+     * @return array
+     */
+    static function search($columns, $params, $limit=10, $page=0, $sortBy=null, $sortAsc=null, $withCounts=true) {
+		$db = DevblocksPlatform::getDatabaseService();
+		
+		// Build search queries
+		$query_parts = self::getSearchQueryComponents($columns,$params,$sortBy,$sortAsc);
+
+		$select_sql = $query_parts['select'];
+		$join_sql = $query_parts['join'];
+		$where_sql = $query_parts['where'];
+		$has_multiple_values = $query_parts['has_multiple_values'];
+		$sort_sql = $query_parts['sort'];
+		
+		$sql = 
+			$select_sql.
+			$join_sql.
+			$where_sql.
+			($has_multiple_values ? 'GROUP BY contact_person.id ' : '').
+			$sort_sql;
+			
+		if($limit > 0) {
+    		$rs = $db->SelectLimit($sql,$limit,$page*$limit) or die(__CLASS__ . '('.__LINE__.')'. ':' . $db->ErrorMsg()); /* @var $rs ADORecordSet */
+		} else {
+		    $rs = $db->Execute($sql) or die(__CLASS__ . '('.__LINE__.')'. ':' . $db->ErrorMsg()); /* @var $rs ADORecordSet */
+            $total = mysql_num_rows($rs);
+		}
+		
+		$results = array();
+		$total = -1;
+		
+		while($row = mysql_fetch_assoc($rs)) {
+			$result = array();
+			foreach($row as $f => $v) {
+				$result[$f] = $v;
+			}
+			$object_id = intval($row[SearchFields_ContactPerson::ID]);
+			$results[$object_id] = $result;
+		}
+
+		// [JAS]: Count all
+		if($withCounts) {
+			$count_sql = 
+				($has_multiple_values ? "SELECT COUNT(DISTINCT contact_person.id) " : "SELECT COUNT(contact_person.id) ").
+				$join_sql.
+				$where_sql;
+			$total = $db->GetOne($count_sql);
+		}
+		
+		mysql_free_result($rs);
+		
+		return array($results,$total);
+	}
+
+};
+
+class SearchFields_ContactPerson implements IDevblocksSearchFields {
+	const ID = 'c_id';
+	const EMAIL_ID = 'c_email_id';
+	const CREATED = 'c_created';
+	const LAST_LOGIN = 'c_last_login';
+	const AUTH_SALT = 'c_auth_salt';
+	const AUTH_PASSWORD = 'c_auth_password';
+	
+	const ADDRESS_EMAIL = 'a_email';
+	const ADDRESS_FIRST_NAME = 'a_first_name';
+	const ADDRESS_LAST_NAME = 'a_last_name';
+	
+	/**
+	 * @return DevblocksSearchField[]
+	 */
+	static function getFields() {
+		$translate = DevblocksPlatform::getTranslationService();
+		
+		$columns = array(
+			self::ID => new DevblocksSearchField(self::ID, 'contact_person', 'id', $translate->_('common.id')),
+			self::EMAIL_ID => new DevblocksSearchField(self::EMAIL_ID, 'contact_person', 'email_id'),
+			self::CREATED => new DevblocksSearchField(self::CREATED, 'contact_person', 'created', $translate->_('common.created')),
+			self::LAST_LOGIN => new DevblocksSearchField(self::LAST_LOGIN, 'contact_person', 'last_login', $translate->_('dao.contact_person.last_login')),
+			self::AUTH_SALT => new DevblocksSearchField(self::AUTH_SALT, 'contact_person', 'auth_salt', $translate->_('dao.contact_person.auth_salt')),
+			self::AUTH_PASSWORD => new DevblocksSearchField(self::AUTH_PASSWORD, 'contact_person', 'auth_password', $translate->_('dao.contact_person.auth_password')),
+			
+			self::ADDRESS_EMAIL => new DevblocksSearchField(self::ADDRESS_EMAIL, 'address', 'email', $translate->_('common.email')),
+			self::ADDRESS_FIRST_NAME => new DevblocksSearchField(self::ADDRESS_FIRST_NAME, 'address', 'first_name', $translate->_('address.first_name')),
+			self::ADDRESS_LAST_NAME => new DevblocksSearchField(self::ADDRESS_LAST_NAME, 'address', 'last_name', $translate->_('address.last_name')),
+		);
+		
+		// Custom Fields
+		//$fields = DAO_CustomField::getBySource(CustomFieldSource_XXX::ID);
+
+		//if(is_array($fields))
+		//foreach($fields as $field_id => $field) {
+		//	$key = 'cf_'.$field_id;
+		//	$columns[$key] = new DevblocksSearchField($key,$key,'field_value',$field->name);
+		//}
+		
+		// Sort by label (translation-conscious)
+		uasort($columns, create_function('$a, $b', "return strcasecmp(\$a->db_label,\$b->db_label);\n"));
+
+		return $columns;		
+	}
+};
+
+class Model_ContactPerson {
+	public $id;
+	public $email_id;
+	public $created;
+	public $last_login;
+	public $auth_salt;
+	public $auth_password;
+};
+
+class View_ContactPerson extends C4_AbstractView {
+	const DEFAULT_ID = 'contactperson';
+
+	function __construct() {
+		$translate = DevblocksPlatform::getTranslationService();
+	
+		$this->id = self::DEFAULT_ID;
+		$this->name = $translate->_('People');
+		$this->renderLimit = 25;
+		$this->renderSortBy = SearchFields_ContactPerson::ID;
+		$this->renderSortAsc = true;
+
+		$this->view_columns = array(
+			SearchFields_ContactPerson::ADDRESS_FIRST_NAME,
+			SearchFields_ContactPerson::ADDRESS_LAST_NAME,
+			SearchFields_ContactPerson::ADDRESS_EMAIL,
+			SearchFields_ContactPerson::CREATED,
+			SearchFields_ContactPerson::LAST_LOGIN,
+		);
+		// Filter fields
+		$this->addColumnsHidden(array(
+			SearchFields_ContactPerson::EMAIL_ID,
+			SearchFields_ContactPerson::AUTH_PASSWORD,
+			SearchFields_ContactPerson::AUTH_SALT,
+		));
+		
+		// Filter fields
+		$this->addParamsHidden(array(
+			SearchFields_ContactPerson::EMAIL_ID,
+			SearchFields_ContactPerson::AUTH_PASSWORD,
+			SearchFields_ContactPerson::AUTH_SALT,
+		));
+		
+		$this->doResetCriteria();
+	}
+
+	function getData() {
+		$objects = DAO_ContactPerson::search(
+			$this->view_columns,
+			$this->getParams(),
+			$this->renderLimit,
+			$this->renderPage,
+			$this->renderSortBy,
+			$this->renderSortAsc,
+			$this->renderTotal
+		);
+		return $objects;
+	}
+	
+	function getDataSample($size) {
+		return $this->_doGetDataSample('DAO_ContactPerson', $size);
+	}
+
+	function render() {
+		$this->_sanitize();
+		
+		$tpl = DevblocksPlatform::getTemplateService();
+		$tpl->assign('id', $this->id);
+		$tpl->assign('view', $this);
+
+		// Custom fields
+		//$custom_fields = DAO_CustomField::getBySource(ChCustomFieldSource_Example::ID);
+		//$tpl->assign('custom_fields', $custom_fields);
+
+		$tpl->display('devblocks:cerberusweb.core::contacts/people/view.tpl');
+	}
+
+	function renderCriteria($field) {
+		$tpl = DevblocksPlatform::getTemplateService();
+		$tpl->assign('id', $this->id);
+
+		switch($field) {
+			case SearchFields_ContactPerson::ADDRESS_EMAIL:
+			case SearchFields_ContactPerson::ADDRESS_FIRST_NAME:
+			case SearchFields_ContactPerson::ADDRESS_LAST_NAME:
+			case SearchFields_ContactPerson::AUTH_SALT:
+			case SearchFields_ContactPerson::AUTH_PASSWORD:
+				$tpl->display('devblocks:cerberusweb.core::internal/views/criteria/__string.tpl');
+				break;
+			case SearchFields_ContactPerson::ID:
+				$tpl->display('devblocks:cerberusweb.core::internal/views/criteria/__number.tpl');
+				break;
+			case 'placeholder_bool':
+				$tpl->display('devblocks:cerberusweb.core::internal/views/criteria/__bool.tpl');
+				break;
+			case 'placeholder_date':
+				$tpl->display('devblocks:cerberusweb.core::internal/views/criteria/__date.tpl');
+				break;
+			/*
+			default:
+				// Custom Fields
+				if('cf_' == substr($field,0,3)) {
+					$this->_renderCriteriaCustomField($tpl, substr($field,3));
+				} else {
+					echo ' ';
+				}
+				break;
+			*/
+		}
+	}
+
+	function renderCriteriaParam($param) {
+		$field = $param->field;
+		$values = !is_array($param->value) ? array($param->value) : $param->value;
+
+		switch($field) {
+			default:
+				parent::renderCriteriaParam($param);
+				break;
+		}
+	}
+
+	function getFields() {
+		return SearchFields_ContactPerson::getFields();
+	}
+
+	function doSetCriteria($field, $oper, $value) {
+		$criteria = null;
+
+		switch($field) {
+			case SearchFields_ContactPerson::ADDRESS_EMAIL:
+			case SearchFields_ContactPerson::ADDRESS_FIRST_NAME:
+			case SearchFields_ContactPerson::ADDRESS_LAST_NAME:
+			case SearchFields_ContactPerson::AUTH_SALT:
+			case SearchFields_ContactPerson::AUTH_PASSWORD:
+				// force wildcards if none used on a LIKE
+				if(($oper == DevblocksSearchCriteria::OPER_LIKE || $oper == DevblocksSearchCriteria::OPER_NOT_LIKE)
+				&& false === (strpos($value,'*'))) {
+					$value = $value.'*';
+				}
+				$criteria = new DevblocksSearchCriteria($field, $oper, $value);
+				break;
+			case SearchFields_ContactPerson::ID:
+				$criteria = new DevblocksSearchCriteria($field,$oper,$value);
+				break;
+				
+			case 'placeholder_date':
+				@$from = DevblocksPlatform::importGPC($_REQUEST['from'],'string','');
+				@$to = DevblocksPlatform::importGPC($_REQUEST['to'],'string','');
+
+				if(empty($from)) $from = 0;
+				if(empty($to)) $to = 'today';
+
+				$criteria = new DevblocksSearchCriteria($field,$oper,array($from,$to));
+				break;
+				
+			case 'placeholder_bool':
+				@$bool = DevblocksPlatform::importGPC($_REQUEST['bool'],'integer',1);
+				$criteria = new DevblocksSearchCriteria($field,$oper,$bool);
+				break;
+				
+			/*
+			default:
+				// Custom Fields
+				if(substr($field,0,3)=='cf_') {
+					$criteria = $this->_doSetCriteriaCustomField($field, substr($field,3));
+				}
+				break;
+			*/
+		}
+
+		if(!empty($criteria)) {
+			$this->addParam($criteria, $field);
+			$this->renderPage = 0;
+		}
+	}
+		
+	function doBulkUpdate($filter, $do, $ids=array()) {
+		@set_time_limit(0);
+	  
+		$change_fields = array();
+		$custom_fields = array();
+
+		// Make sure we have actions
+		if(empty($do))
+			return;
+
+		// Make sure we have checked items if we want a checked list
+		if(0 == strcasecmp($filter,"checks") && empty($ids))
+			return;
+			
+		if(is_array($do))
+		foreach($do as $k => $v) {
+			switch($k) {
+				// [TODO] Implement actions
+				case 'example':
+					//$change_fields[DAO_ContactPerson::EXAMPLE] = 'some value';
+					break;
+				/*
+				default:
+					// Custom fields
+					if(substr($k,0,3)=="cf_") {
+						$custom_fields[substr($k,3)] = $v;
+					}
+					break;
+				*/
+			}
+		}
+
+		$pg = 0;
+
+		if(empty($ids))
+		do {
+			list($objects,$null) = DAO_ContactPerson::search(
+				array(),
+				$this->getParams(),
+				100,
+				$pg++,
+				SearchFields_ContactPerson::ID,
+				true,
+				false
+			);
+			$ids = array_merge($ids, array_keys($objects));
+			 
+		} while(!empty($objects));
+
+		$batch_total = count($ids);
+		for($x=0;$x<=$batch_total;$x+=100) {
+			$batch_ids = array_slice($ids,$x,100);
+			
+			DAO_ContactPerson::update($batch_ids, $change_fields);
+
+			// Custom Fields
+			//self::_doBulkSetCustomFields(ChCustomFieldSource_ContactPerson::ID, $custom_fields, $batch_ids);
+			
+			unset($batch_ids);
+		}
+
+		unset($ids);
+	}			
+};
+
