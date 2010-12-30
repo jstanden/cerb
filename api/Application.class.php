918b5719d06b96e99d7bf47ab61bc2d7e3bad2f8
Jeff Standen <jeff@webgroupmedia.com>
2010-12-29 16:31:22 -0800

    Syncing up JIRA issues.
    * [Plugins] [CHD-806] Call Logging functionality
    
    * [Address Book] [CHD-1767] Working with Contacts/Companies: Add notes to contacts and attachments to organizations.

--- api/Application.class.php
+++ api/Application.class.php
@@ -47,7 +47,7 @@
  * 		and Jerry Kanoholani. 
  *	 WEBGROUP MEDIA LLC. - Developers of Cerberus Helpdesk
  */
-define("APP_BUILD", 2010122801);
+define("APP_BUILD", 2010122802);
 define("APP_VERSION", '5.3.0-dev');
 define("APP_MAIL_PATH", APP_STORAGE_PATH . '/mail/');
 
