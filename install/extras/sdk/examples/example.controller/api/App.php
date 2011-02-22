7f0e098261083baac292a7ae097b0b4676a99e90
Jeff Standen <jeff@webgroupmedia.com>
2011-02-22 14:54:26 -0800

    * [SDK/Plugins] Added an 'example.controller' plugin example to the SDK to demonstrate creating new controllers, which handle requests and write responses at a lower level than cerberusweb.page extensions.

---
+++ install/extras/sdk/examples/example.controller/api/App.php
@@ -0,0 +1,42 @@
+<?php
+class ExampleController extends DevblocksControllerExtension {
+	function isVisible() {
+		// The current session must be a logged-in worker to use this page.
+		if(null == ($worker = CerberusApplication::getActiveWorker()))
+			return false;
+		return true;
+	}
+
+	/*
+	 * Request Overload
+	 */
+	function handleRequest(DevblocksHttpRequest $request) {
+		$stack = $request->path;
+		array_shift($stack); // example
+		
+	    @$action = array_shift($stack) . 'Action';
+
+	    switch($action) {
+	        case NULL:
+	            // [TODO] Index/page render
+	            break;
+	            
+	        default:
+			    // Default action, call arg as a method suffixed with Action
+				if(method_exists($this,$action)) {
+					call_user_func(array(&$this, $action));
+				}
+	            break;
+	    }
+	    
+	    exit;
+	}
+
+	function writeResponse(DevblocksHttpResponse $response) {
+		return;
+	}
+	
+	function testAction() {
+		echo "Test Response!";
+	}
+};
