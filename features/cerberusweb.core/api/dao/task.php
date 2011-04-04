aff9fef27f21ac9ec074ff9198cc6aa716df9817
Jeff Standen <jeff@webgroupmedia.com>
2011-04-04 01:10:58 -0700

    * [Worklists/Subtotals/Task] Implemented the new subtotals functionality on Task worklists.

--- features/cerberusweb.core/api/dao/task.php
+++ features/cerberusweb.core/api/dao/task.php
@@ -369,7 +369,7 @@ class Model_Task {
 	public $updated_date;
 };
 
-class View_Task extends C4_AbstractView {
+class View_Task extends C4_AbstractView implements IAbstractView_Subtotals {
 	const DEFAULT_ID = 'tasks';
 	const DEFAULT_TITLE = 'Tasks';
 
@@ -419,6 +419,63 @@ class View_Task extends C4_AbstractView {
 		return $this->_doGetDataSample('DAO_Task', $size);
 	}
 
+	function getSubtotalFields() {
+		$all_fields = $this->getFields();
+		
+		$fields = array();
+
+		if(is_array($all_fields))
+		foreach($all_fields as $field_key => $field_model) {
+			$pass = false;
+			
+			switch($field_key) {
+				// Booleans
+				case SearchFields_Task::IS_COMPLETED:
+					$pass = true;
+					break;
+					
+				// Valid custom fields
+				default:
+					if('cf_' == substr($field_key,0,3))
+						$pass = $this->_canSubtotalCustomField($field_key);
+					break;
+			}
+			
+			if($pass)
+				$fields[$field_key] = $field_model;
+		}
+		
+		return $fields;
+	}
+	
+	function getSubtotalCounts($column=null) {
+		$counts = array();
+		$fields = $this->getFields();
+
+		if(!isset($fields[$column]))
+			return array();
+		
+		switch($column) {
+//			case SearchFields_Task::EXAMPLE:
+//				$counts = $this->_getSubtotalCountForStringColumn('DAO_Task', $column);
+//				break;
+
+			case SearchFields_Task::IS_COMPLETED:
+				$counts = $this->_getSubtotalCountForBooleanColumn('DAO_Task', $column);
+				break;
+			
+			default:
+				// Custom fields
+				if('cf_' == substr($column,0,3)) {
+					$counts = $this->_getSubtotalCountForCustomColumn('DAO_Task', $column, 't.id');
+				}
+				
+				break;
+		}
+		
+		return $counts;
+	}
+	
 	function render() {
 		$this->_sanitize();
 		
@@ -444,7 +501,8 @@ class View_Task extends C4_AbstractView {
 				$tpl->display('devblocks:cerberusweb.core::tasks/view_contextlinks_chooser.tpl');
 				break;
 			default:
-				$tpl->display('devblocks:cerberusweb.core::tasks/view.tpl');
+				$tpl->assign('view_template', 'devblocks:cerberusweb.core::tasks/view.tpl');
+				$tpl->display('devblocks:cerberusweb.core::internal/views/subtotals_and_view.tpl');
 				break;
 		}
 		
