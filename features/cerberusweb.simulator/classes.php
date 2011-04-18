a546d330f9900b25915c9c99d5a2d903bd8768a3
Jeff Standen <jeff@webgroupmedia.com>
2011-04-17 18:36:54 -0700

    * [Simulator] The simulator can now create sample organizations and tasks in addition to tickets.

--- features/cerberusweb.simulator/classes.php
+++ features/cerberusweb.simulator/classes.php
@@ -53,7 +53,7 @@ class PageMenuItem_SetupSimulator extends Extension_PageMenuItem {
 	
 	public function render() {
 		$tpl = DevblocksPlatform::getTemplateService();
-		$tpl->display('devblocks:cerberusweb.simulator::setup/menu_mail_simulator.tpl');
+		$tpl->display('devblocks:cerberusweb.simulator::setup/menu_simulator.tpl');
 	}
 };
 
@@ -146,4 +146,308 @@ class PageSection_SetupSimulator extends Extension_PageSection {
 			
 		}
 	}
+	
+	function generateTasksJsonAction() {
+		try {
+			@$how_many = DevblocksPlatform::importGPC($_POST['how_many'],'integer',0);
+			
+			$tpl_builder = DevblocksPlatform::getTemplateBuilder();
+			
+			$title = "{{action}} {{whos}} {{thing}} {{because}}";
+			
+			$variables = array(
+				'action' => array(
+					'Erase',
+					'RMA',
+					'Locate',
+					'Destroy',
+					'Replace',
+					'Update',
+					'Audit',
+					'Fix',
+					'Repair',
+					'Find',
+					'Migrate',
+				),
+				'whos' => array(
+					"customer's",
+					"client's",
+					"CEO's",
+					"janitor's",
+					"I.T.'s",
+					"the new sales guy's",
+					"HQ's",
+				),
+				'thing' => array(
+					'website',
+					'server',
+					'hard drive',
+					'Windows machine',
+					'Blackberry',
+					'iPhone',
+					'laptop',
+					'login',
+					'iPad',
+					'Linux box',
+					'iMac',
+				),
+				'because' => array(
+					'because the shareholders are on to us',
+					'since I.T. funding was scaled back',
+					"since all they really asked for was coffee",
+					"because they can't figure it out",
+					'due to cutbacks',
+					'since the new one is provisioned',
+					'since we only purchased it because of flashy marketing anyway',
+					"because it won't fit in the shredder",
+					'because they asked for more disk space',
+					"because it's " . strftime("%A"),
+					"because it is " . strftime("%B"),
+				),
+			);
+
+			for($x=0;$x<$how_many;$x++) {
+				$params = array();
+				foreach($variables as $k=>$data) {
+					shuffle($data);
+					$params[$k] = $data[array_rand($data)];
+				}
+				
+				$task_title = $tpl_builder->build(
+					$title,
+					$params
+				);
+
+				$due_increment = mt_rand(1,24);
+				$due_unit = (mt_rand(0,1)) ? 'hours' : 'days';
+				$due_timestamp = strtotime('+'.$due_increment.' '.$due_unit);
+				
+				// [TODO] Randomly context link to orgs
+				
+				$task_id = DAO_Task::create(array(
+					DAO_Task::TITLE => $task_title,
+					DAO_Task::UPDATED_DATE => time(),
+					DAO_Task::DUE_DATE => $due_timestamp,
+				));
+			}
+			
+			echo json_encode(array('status'=>true, 'message'=>sprintf("Success! %d simulated tasks were generated.", $how_many)));
+			return;
+			
+		} catch(Exception $e) {
+			echo json_encode(array('status'=>false,'error'=>$e->getMessage()));
+			return;
+			
+		}
+		
+	}
+	
+	function generateOrgsJsonAction() {
+		try {
+			@$how_many = DevblocksPlatform::importGPC($_POST['how_many'],'integer',0);
+			
+			$tpl_builder = DevblocksPlatform::getTemplateBuilder();
+			
+			$org_name_tpl = "{{intro}}{{consonant}}{{vowel}} {{industry}}{{suffix}}";
+			$website_tpl = "http://{{intro}}{{consonant}}{{vowel}}{{industry}}.cerberusdemo.com/";
+			
+			$variables = array(
+				'intro' => array(
+					'hi',
+					'en',
+					'er',
+					'je',
+					'twi',
+					'ri',
+					'lo',
+					'ire',
+					'fi',
+					'do',
+					'sta',
+					'sti',
+					'gho',
+					'we',
+					'ba',
+					'bra',
+					'hu',
+					'ge',
+					'bi',
+					'zo',
+					'ze',
+					'zu',
+					'li',
+					'fa',
+					'gro',
+					'ne',
+					'vi',
+					'via',
+					'gri',
+					'lo',
+					'wa',
+					'go',
+				),
+				'vowel' => array(
+					'i',
+					'e',
+					'u',
+					'y',
+					'a',
+					'ee',
+					'ie',
+					'ri',
+					'ilo',
+					'ilio',
+					'io',
+					'int',
+					'ist',
+					'ily',
+					'ux',
+					'us',
+					'er',
+					'ard',
+					'ii',
+					'oo',
+					'ious',
+					'on',
+					'ou',
+					'',
+					'',
+					'',
+				),
+				'consonant' => array(
+					'fu',
+					'fi',
+					'ld',
+					'cr',
+					'ch',
+					'st',
+					'rst',
+					'gh',
+					'bb',
+					'br',
+					'b',
+					'z',
+					'nt',
+					'gr',
+					'rd',
+					'l',
+					'nd',
+					'',
+					'',
+					'',
+				),
+				'industry' => array(
+					'Media',
+					'Institute',
+					'Loans',
+					'Online',
+					'Mobile',
+					'Defense',
+					'Education',
+					'Pharmaceuticals',
+					'Finance',
+					'United',
+					'Ventures',
+					'Airways',
+					'Aerospace',
+					'Events',
+					'Music',
+					'Software',
+					'Games',
+					'Collective',
+					'Republic',
+					'',
+				),
+				'suffix' => array(
+					' LLC',
+					', Inc.',
+					' Ltd.',
+					' GmbH',
+					'',
+					'',
+					'.com',
+					' Incorporated',
+				),
+			);
+		
+			$org_name_tpl = str_replace(
+				array(
+					'  ',
+					' ,',
+				),
+				array(
+					' ',
+					',',
+				),
+				trim($org_name_tpl)
+			);
+			
+			$countries = array(
+				'USA',
+				'United Kingdom',
+				'Australia',
+				'Canada',
+				'Netherlands',
+				'Germany',
+				'Brazil',
+				'Spain',
+				'India',
+				'Norway',
+				'France',
+				'Russia',
+				'Belgium',
+				'South Africa',
+				'Italy',
+				'Singapore',
+				'Sweden',
+				'Poland',
+				'Mexico',
+			);
+			
+			for($x=0;$x<$how_many;$x++) {
+				$params = array();
+				foreach($variables as $k=>$data) {
+					shuffle($data);
+					$params[$k] = $data[array_rand($data)];
+				}
+				
+				$org_name = $tpl_builder->build(
+					$org_name_tpl,
+					$params
+				);
+				
+				$org_name = ucfirst($org_name);
+				
+				$website = $tpl_builder->build(
+					$website_tpl,
+					$params
+				);
+				
+				$website = strtolower($website);
+				
+				shuffle($countries);
+				
+				$country = $countries[array_rand($countries)];
+
+				// [TODO] Randomly context link to orgs
+				
+				$org_id = DAO_ContactOrg::create(array(
+					DAO_ContactOrg::NAME => $org_name,
+					DAO_ContactOrg::CREATED => time(),
+					DAO_ContactOrg::COUNTRY => $country,
+					DAO_ContactOrg::WEBSITE => $website,
+				));
+			}
+			
+			echo json_encode(array('status'=>true, 'message'=>sprintf("Success! %d simulated orgs were generated.", $how_many)));
+			return;
+			
+		} catch(Exception $e) {
+			echo json_encode(array('status'=>false,'error'=>$e->getMessage()));
+			return;
+			
+		}
+		
+	}
+	
 };
--- features/cerberusweb.simulator/templates/setup/section.tpl
+++ features/cerberusweb.simulator/templates/setup/section.tpl
@@ -3,7 +3,7 @@
 <fieldset>
 	<legend>{$translate->_('simulator.ui.cfg.sample_tickets')}</legend>
 
-	<form method="post" action="{devblocks_url}{/devblocks_url}" id="simulatorForm">
+	<form method="post" action="{devblocks_url}{/devblocks_url}" id="simulatorMailForm">
 	<input type="hidden" name="c" value="config">
 	<input type="hidden" name="a" value="handleSectionAction">
 	<input type="hidden" name="section" value="simulator">
@@ -25,7 +25,7 @@
 			</td>
 			<td>
 				<b>{$translate->_('simulator.ui.cfg.how_many')}</b><br>
-				<input type="text" name="how_many" size="4" maxlength="3" value="25">
+				<input type="text" name="how_many" size="4" maxlength="3" value="10">
 			</td>
 		</tr>
 	</table>
@@ -37,16 +37,126 @@
 </fieldset>
 
 <script type="text/javascript">
-	$('#simulatorForm BUTTON.submit')
+	$('#simulatorMailForm BUTTON.submit')
 	.click(function(e) {
-		Devblocks.showSuccess('#simulatorForm div.status', "{$translate->_('simulator.ui.cfg.generate_wait')}", false, false);
+		Devblocks.showSuccess('#simulatorMailForm div.status', "{$translate->_('simulator.ui.cfg.generate_wait')}", false, false);
 		
-		genericAjaxPost('simulatorForm','',null,function(json) {
+		genericAjaxPost('simulatorMailForm','',null,function(json) {
 			$o = $.parseJSON(json);
 			if(false == $o || false == $o.status) {
-				Devblocks.showError('#simulatorForm div.status',$o.error);
+				Devblocks.showError('#simulatorMailForm div.status',$o.error);
 			} else {
-				Devblocks.showSuccess('#simulatorForm div.status',$o.message);
+				Devblocks.showSuccess('#simulatorMailForm div.status',$o.message);
+			}
+		});
+	})
+	;	
+</script>
+
+<fieldset>
+	<legend>Create Sample Tasks</legend>
+
+	<form method="post" action="{devblocks_url}{/devblocks_url}" id="simulatorTaskForm">
+	<input type="hidden" name="c" value="config">
+	<input type="hidden" name="a" value="handleSectionAction">
+	<input type="hidden" name="section" value="simulator">
+	<input type="hidden" name="action" value="generateTasksJson">
+	
+	<table cellpadding="5" cellspacing="0" border="0">
+		<tr>
+			{*
+			<td>
+				<b>{$translate->_('simulator.ui.cfg.to_address')}</b><br>
+				<input type="text" name="address" value="{$address}" size="45">
+			</td>
+			<td>
+				<b>{$translate->_('simulator.ui.cfg.flavor')}</b><br>
+				<select name="dataset">
+					{foreach from=$flavors item=flavor key=flavor_id}
+						<option value="{$flavor_id}">{$flavor}
+					{/foreach}
+				</select>
+			</td>
+			*}
+			<td>
+				<b>{$translate->_('simulator.ui.cfg.how_many')}</b><br>
+				<input type="text" name="how_many" size="4" maxlength="3" value="10">
+			</td>
+		</tr>
+	</table>
+	
+	<div class="status"></div>
+	
+	<button type="button" class="submit"><span class="cerb-sprite sprite-gear"></span> {$translate->_('simulator.ui.cfg.generate')}</button>
+	</form>
+</fieldset>
+
+<script type="text/javascript">
+	$('#simulatorTaskForm BUTTON.submit')
+	.click(function(e) {
+		Devblocks.showSuccess('#simulatorTaskForm div.status', "{$translate->_('simulator.ui.cfg.generate_wait')}", false, false);
+		
+		genericAjaxPost('simulatorTaskForm','',null,function(json) {
+			$o = $.parseJSON(json);
+			if(false == $o || false == $o.status) {
+				Devblocks.showError('#simulatorTaskForm div.status',$o.error);
+			} else {
+				Devblocks.showSuccess('#simulatorTaskForm div.status',$o.message);
+			}
+		});
+	})
+	;	
+</script>
+
+<fieldset>
+	<legend>Create Sample Organizations</legend>
+
+	<form method="post" action="{devblocks_url}{/devblocks_url}" id="simulatorOrgForm">
+	<input type="hidden" name="c" value="config">
+	<input type="hidden" name="a" value="handleSectionAction">
+	<input type="hidden" name="section" value="simulator">
+	<input type="hidden" name="action" value="generateOrgsJson">
+	
+	<table cellpadding="5" cellspacing="0" border="0">
+		<tr>
+			{*
+			<td>
+				<b>{$translate->_('simulator.ui.cfg.to_address')}</b><br>
+				<input type="text" name="address" value="{$address}" size="45">
+			</td>
+			<td>
+				<b>{$translate->_('simulator.ui.cfg.flavor')}</b><br>
+				<select name="dataset">
+					{foreach from=$flavors item=flavor key=flavor_id}
+						<option value="{$flavor_id}">{$flavor}
+					{/foreach}
+				</select>
+			</td>
+			*}
+			<td>
+				<b>{$translate->_('simulator.ui.cfg.how_many')}</b><br>
+				<input type="text" name="how_many" size="4" maxlength="3" value="10">
+			</td>
+		</tr>
+	</table>
+	
+	<div class="status"></div>
+	
+	<button type="button" class="submit"><span class="cerb-sprite sprite-gear"></span> {$translate->_('simulator.ui.cfg.generate')}</button>
+	</form>
+</fieldset>
+
+<script type="text/javascript">
+	$('#simulatorOrgForm BUTTON.submit')
+	.click(function(e) {
+		Devblocks.showSuccess('#simulatorOrgForm div.status', "{$translate->_('simulator.ui.cfg.generate_wait')}", false, false);
+		
+		genericAjaxPost('simulatorOrgForm','',null,function(json) {
+			$o = $.parseJSON(json);
+			if(false == $o || false == $o.status) {
+				Devblocks.showError('#simulatorOrgForm div.status',$o.error);
+			} else {
+				Devblocks.showSuccess('#simulatorOrgForm div.status',$o.message);
 			}
 		});
 	})
