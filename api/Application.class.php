d7bff3a3151b5485b0b3f04c2ead878c02747b60
Jeff Standen <jeff@webgroupmedia.com>
2011-05-02 16:18:34 -0700

    5.4-beta2 Push (2011050201)

--- api/Application.class.php
+++ api/Application.class.php
@@ -47,8 +47,8 @@
  * 		and Jerry Kanoholani. 
  *	 WEBGROUP MEDIA LLC. - Developers of Cerberus Helpdesk
  */
-define("APP_BUILD", 2011043001);
-define("APP_VERSION", '5.4.0-beta1');
+define("APP_BUILD", 2011050201);
+define("APP_VERSION", '5.4.0-beta2');
 
 define("APP_MAIL_PATH", APP_STORAGE_PATH . '/mail/');
 
