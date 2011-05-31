dad5ff4b6df0cdc447f7855488c9d5cf522bce51
Jeff Standen <jeff@webgroupmedia.com>
2011-05-31 11:17:45 -0700

    * [Parser/Mail] [CHD-2386] The ticket 'created' timestamp will now reflect when the message was actually processed, without regard for 'Date:' header (which is stored on the individual message record).

--- api/app/Parser.php
+++ api/app/Parser.php
@@ -853,7 +853,7 @@ class CerberusParser {
 				DAO_Ticket::IS_CLOSED => 0,
 				DAO_Ticket::FIRST_WROTE_ID => intval($model->getSenderAddressModel()->id),
 				DAO_Ticket::LAST_WROTE_ID => intval($model->getSenderAddressModel()->id),
-				DAO_Ticket::CREATED_DATE => $model->getDate(),
+				DAO_Ticket::CREATED_DATE => time(),
 				DAO_Ticket::UPDATED_DATE => time(),
 				DAO_Ticket::LAST_ACTION_CODE => CerberusTicketActionCode::TICKET_OPENED,
 			);
