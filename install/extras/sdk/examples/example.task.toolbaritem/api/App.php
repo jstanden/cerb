7a0d4db18b001dbc9d3a228d41b1852199a1758d
Jeff Standen <jeff@webgroupmedia.com>
2011-06-06 10:19:53 -0700

    [Code Cleanup] SDK to 5.4

--- install/extras/sdk/examples/example.task.toolbaritem/api/App.php
+++ install/extras/sdk/examples/example.task.toolbaritem/api/App.php
@@ -3,9 +3,7 @@ if (class_exists('Extension_TaskToolbarItem',true)):
 	class ExTaskToolbarItem extends Extension_TaskToolbarItem {
 		function render(Model_Task $task) { 
 			$tpl = DevblocksPlatform::getTemplateService();
-			$translate = DevblocksPlatform::getTranslationService();
 			
-			$tpl->assign('translate', $translate);
 			$tpl->assign('task', $task); /* @var $message Model_Ticket */			
 			$tpl->display('devblocks:example.task.toolbaritem::button.tpl');
 		}
--- install/extras/sdk/examples/example.ticket.toolbaritem/api/App.php
+++ install/extras/sdk/examples/example.ticket.toolbaritem/api/App.php
@@ -3,9 +3,7 @@ if (class_exists('Extension_TicketToolbarItem',true)):
 	class ExTicketToolbarItem extends Extension_TicketToolbarItem {
 		function render(Model_Ticket $ticket) { 
 			$tpl = DevblocksPlatform::getTemplateService();
-			$translate = DevblocksPlatform::getTranslationService();
 			
-			$tpl->assign('translate', $translate);
 			$tpl->assign('ticket', $ticket); /* @var $message Model_Ticket */			
 			$tpl->display('devblocks:example.ticket.toolbaritem::button.tpl');
 		}
