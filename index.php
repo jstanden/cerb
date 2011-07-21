dee57ce1542c077e2dd71dcaeae57fef7eb7f8c7
Jeff Standen <jeff@webgroupmedia.com>
2011-07-21 15:00:37 -0700

    [Devblocks/Platform] Rearranged the sequence of events in the bootloader so the platform patch + cache clear has a change to run before the rest of Devblocks initializes.  This prevents issues where major changes happen to Devblocks and index.php requests services (e.g. sessions, URL rewriting) before redirecting to /update.

--- index.php
+++ index.php
@@ -71,17 +71,17 @@ require(APP_PATH . '/api/Application.class.php');
 DevblocksPlatform::init();
 DevblocksPlatform::setExtensionDelegate('C4_DevblocksExtensionDelegate');
 
-// Request
-$request = DevblocksPlatform::readRequest();
-
-// Patches (if not on the patch page)
-if(0 != strcasecmp(@$request->path[0],"update") 
-	&& !DevblocksPlatform::versionConsistencyCheck()) {
+// Do we need an update first?
+if(!DevblocksPlatform::versionConsistencyCheck()) {
+	$request = DevblocksPlatform::readRequest();
+	if(0 != strcasecmp(@$request->path[0],"update")) {
 		DevblocksPlatform::redirect(new DevblocksHttpResponse(array('update','locked')));
 		exit;
+	}
 }
 
-//DevblocksPlatform::readPlugins();
+// Request
+$request = DevblocksPlatform::readRequest();
 $session = DevblocksPlatform::getSessionService();
 
 // Localization
