fac6d7e2b73e29545944cebaa6940fe7e5e64b5b
Jeff Standen <jeff@webgroupmedia.com>
2011-08-30 13:27:33 -0700

    * [Support Center/Knowledgebase/RSS] Implemented a 'Recently Added' RSS feed for knowledgebase articles in the Support Center.  This syndicates each article once and it won't move an article to the top when updated like 'Recently Updated' does.
    * [Support Center/Knowledgebase/RSS] [CHD-2557] Fixed an issue where the knowledgebase articles in Support Center RSS feeds didn't always sort properly by Most Popular or Recent Changes.

--- features/cerberusweb.kb/api/sc/rss/kb.php
+++ features/cerberusweb.kb/api/sc/rss/kb.php
@@ -23,6 +23,10 @@ class UmScKbRssController extends Extension_UmScRssController {
 			case 'recent_changes':
 				$this->_renderRecentChangesRss($portal);
 				break;
+				
+			case 'new_articles':
+				$this->_renderNewArticlesRss($portal);
+				break;
 		}
 	}
 	
@@ -65,10 +69,14 @@ XML;
 			false
 		);
 
-		$articles = DAO_KbArticle::getWhere(sprintf("%s IN (%s)",
-			DAO_KbArticle::ID,
-			implode(',', array_keys($results))
-		));
+		$articles = DAO_KbArticle::getWhere(
+			sprintf("%s IN (%s)",
+				DAO_KbArticle::ID,
+				implode(',', array_keys($results))
+			),
+			DAO_KbArticle::VIEWS,
+			false
+		);
 		
         foreach($articles as $article) { /* @var $article Model_KbArticle */
         	$created = intval($article->updated);
@@ -114,7 +122,7 @@ XML;
 
         // Channel
         $channel = $xml->addChild('channel');
-		$channel->addChild('title', (!empty($portal_name) ? ('['.$portal_name.'] ') : '') . "Recently Changed Articles");
+		$channel->addChild('title', (!empty($portal_name) ? ('['.$portal_name.'] ') : '') . "Recently Updated Articles");
         $channel->addChild('link', $url->write(sprintf('c=rss&kb=kb&a=recent_changes', $portal),true));
         $channel->addChild('description', '');
         
@@ -141,10 +149,94 @@ XML;
 			return;
 		}
 		
-		$articles = DAO_KbArticle::getWhere(sprintf("%s IN (%s)",
+		$articles = DAO_KbArticle::getWhere(
+			sprintf("%s IN (%s)",
+				DAO_KbArticle::ID,
+				implode(',', array_keys($results))
+			),
+			DAO_KbArticle::UPDATED,
+			false
+		);
+		
+        foreach($articles as $article) { /* @var $article Model_KbArticle */
+        	$created = intval($article->updated);
+            if(empty($created)) $created = time();
+
+            $eItem = $channel->addChild('item');
+            
+            $escapedSubject = htmlspecialchars($article->title,null,LANG_CHARSET_CODE);
+            //filter out a couple non-UTF-8 characters (0xC and ESC)
+            $escapedSubject = mb_convert_encoding($escapedSubject, 'utf-8', LANG_CHARSET_CODE);
+            $eTitle = $eItem->addChild('title', $escapedSubject);
+
+            $escapedDesc = htmlspecialchars($article->getContent(),null,LANG_CHARSET_CODE);
+            $escapedDesc = mb_convert_encoding($escapedDesc, 'utf-8', LANG_CHARSET_CODE);
+            $eDesc = $eItem->addChild('description', $escapedDesc);
+
+            $link = $url->write('c=kb&a=article&id='.$article->id, true);
+            $eLink = $eItem->addChild('link', $link);
+	            
+            $eDate = $eItem->addChild('pubDate', gmdate('D, d M Y H:i:s T', $created));
+            
+            $eGuid = $eItem->addChild('guid', md5($escapedSubject . $link . $created));
+            $eGuid->addAttribute('isPermaLink', "false");
+        }
+
+        echo $xml->asXML();		
+	}
+	
+	private function _renderNewArticlesRss($portal) {
+		header("Content-Type: text/xml");
+
+        $xmlstr = <<<XML
+		<rss version='2.0' xmlns:atom='http://www.w3.org/2005/Atom'>
+		</rss>
+XML;
+
+        $xml = new SimpleXMLElement($xmlstr);
+        $translate = DevblocksPlatform::getTranslationService();
+		$url = DevblocksPlatform::getUrlService();
+
+		// Portal details
+		$portal_name = DAO_CommunityToolProperty::get($portal, UmScApp::PARAM_PAGE_TITLE, '');
+
+        // Channel
+        $channel = $xml->addChild('channel');
+		$channel->addChild('title', (!empty($portal_name) ? ('['.$portal_name.'] ') : '') . "Recently Added Articles");
+        $channel->addChild('link', $url->write(sprintf('c=rss&kb=kb&a=recent_changes', $portal),true));
+        $channel->addChild('description', '');
+        
+		// Limit topics to portal config
+		@$topics = unserialize(DAO_CommunityToolProperty::get($portal, UmScKbController::PARAM_KB_ROOTS, ''));
+		if(empty($topics))
+			return;
+		
+        // Search Results
+		list($results, $null) = DAO_KbArticle::search(
+			array(),
+			array(
+				SearchFields_KbArticle::TOP_CATEGORY_ID => new DevblocksSearchCriteria(SearchFields_KbArticle::TOP_CATEGORY_ID,'in',array_keys($topics)),
+			),
+			25,
+			0,
+			SearchFields_KbArticle::ID,
+			false,
+			false
+		);
+
+		if(empty($results)) {
+			echo $xml->asXML();
+			return;
+		}
+		
+		$articles = DAO_KbArticle::getWhere(
+			sprintf("%s IN (%s)",
+				DAO_KbArticle::ID,
+				implode(',', array_keys($results))
+			),
 			DAO_KbArticle::ID,
-			implode(',', array_keys($results))
-		));
+			false
+		);
 		
         foreach($articles as $article) { /* @var $article Model_KbArticle */
         	$created = intval($article->updated);
--- features/cerberusweb.kb/templates/support_center/kb/sidebar.tpl
+++ features/cerberusweb.kb/templates/support_center/kb/sidebar.tpl
@@ -15,7 +15,8 @@
 			</form>
 			
 			<div style="padding:2px;"><img src="{devblocks_url}c=resource&p=cerberusweb.support_center&f=images/feed-icon-16x16.gif{/devblocks_url}" align="top"> <a href="{devblocks_url}c=rss&m=kb&a=most_popular{/devblocks_url}">Most Popular</a></div>
-			<div style="padding:2px;"><img src="{devblocks_url}c=resource&p=cerberusweb.support_center&f=images/feed-icon-16x16.gif{/devblocks_url}" align="top"> <a href="{devblocks_url}c=rss&m=kb&a=recent_changes{/devblocks_url}">Recent Changes</a></div>
+			<div style="padding:2px;"><img src="{devblocks_url}c=resource&p=cerberusweb.support_center&f=images/feed-icon-16x16.gif{/devblocks_url}" align="top"> <a href="{devblocks_url}c=rss&m=kb&a=new_articles{/devblocks_url}">Recently Added</a></div>
+			<div style="padding:2px;"><img src="{devblocks_url}c=resource&p=cerberusweb.support_center&f=images/feed-icon-16x16.gif{/devblocks_url}" align="top"> <a href="{devblocks_url}c=rss&m=kb&a=recent_changes{/devblocks_url}">Recently Updated</a></div>
 		</td>
 	</tr>
 </table>
