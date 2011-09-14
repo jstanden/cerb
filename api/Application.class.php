4d2c7f9aadd1dcece00b95d01e1f71aee77f1303
Jeff Standen <jeff@webgroupmedia.com>
2011-09-13 17:53:02 -0700

    5.6-dev Push (2011091401)

--- api/Application.class.php
+++ api/Application.class.php
@@ -46,7 +46,7 @@
  * - Jeff Standen, Darren Sugita, Dan Hildebrandt, Scott Luther
  *	 WEBGROUP MEDIA LLC. - Developers of Cerberus Helpdesk
  */
-define("APP_BUILD", 2011083001);
+define("APP_BUILD", 2011091401);
 define("APP_VERSION", '5.6.0-dev');
 
 define("APP_MAIL_PATH", APP_STORAGE_PATH . '/mail/');
