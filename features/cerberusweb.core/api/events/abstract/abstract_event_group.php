7a6a63c4fb5a8d6ad2a6ae861690c2e54abaa243
Jeff Standen <jeff@webgroupmedia.com>
2012-04-13 02:24:36 -0700

    * [Virtual Attendants/Groups] Added the ability to create custom group behavior.  These macros can be run or scheduled from group profiles. For example, a group behavior could look for 'idle' tickets in the group's buckets (e.g. owned tickets needing a worker reply for more than 4 hours).  A custom ticket behavior can be scheduled on each idle ticket, which might notify their respective owners.

---
+++ features/cerberusweb.core/api/events/abstract/abstract_event_group.php
@@ -0,0 +1,338 @@
+<?php
+/***********************************************************************
+| Cerberus Helpdesk(tm) developed by WebGroup Media, LLC.
+|-----------------------------------------------------------------------
+| All source code & content (c) Copyright 2012, WebGroup Media LLC
+|   unless specifically noted otherwise.
+|
+| This source code is released under the Devblocks Public License.
+| The latest version of this license can be found here:
+| http://cerberusweb.com/license
+|
+| By using this software, you acknowledge having read this license
+| and agree to be bound thereby.
+| ______________________________________________________________________
+|	http://www.cerberusweb.com	  http://www.webgroupmedia.com/
+***********************************************************************/
+
+abstract class AbstractEvent_Group extends Extension_DevblocksEvent {
+	protected $_event_id = null; // override
+
+	/**
+	 * 
+	 * @param integer $group_id
+	 * @return Model_DevblocksEvent
+	 */
+	function generateSampleEventModel($group_id=null) {
+		
+		if(empty($group_id)) {
+			// Pull the latest record
+			list($results) = DAO_Group::search(
+				array(),
+				array(
+				),
+				10,
+				0,
+				SearchFields_Group::ID,
+				true,
+				false
+			);
+			
+			shuffle($results);
+			
+			$result = array_shift($results);
+			
+			$group_id = $result[SearchFields_Group::ID];
+		}
+		
+		return new Model_DevblocksEvent(
+			$this->_event_id,
+			array(
+				'group_id' => $group_id,
+			)
+		);
+	}	
+	
+	function setEvent(Model_DevblocksEvent $event_model=null) {
+		$labels = array();
+		$values = array();
+
+		/**
+		 * Group
+		 */
+		
+		@$group_id = $event_model->params['group_id']; 
+		$merge_labels = array();
+		$merge_values = array();
+		CerberusContexts::getContext(CerberusContexts::CONTEXT_GROUP, $group_id, $merge_labels, $merge_values, null, true);
+
+			// Merge
+			CerberusContexts::merge(
+				'group_',
+				'',
+				$merge_labels,
+				$merge_values,
+				$labels,
+				$values
+			);
+
+		/**
+		 * Return
+		 */
+
+		$this->setLabels($labels);
+		$this->setValues($values);		
+	}
+	
+	function getValuesContexts($trigger) {
+		$vals = array(
+			'group_id' => array(
+				'label' => 'Group',
+				'context' => CerberusContexts::CONTEXT_GROUP,
+			),
+		);
+		
+		$vars = parent::getValuesContexts($trigger);
+		
+		$vals_to_ctx = array_merge($vals, $vars);
+		asort($vals_to_ctx);
+		
+		return $vals_to_ctx;
+	}
+	
+	function getConditionExtensions() {
+		$labels = $this->getLabels();
+		
+		$types = array(
+			'group_name' => Model_CustomField::TYPE_SINGLE_LINE,
+		);
+
+		$conditions = $this->_importLabelsTypesAsConditions($labels, $types);
+
+		return $conditions;		
+	}
+	
+	function renderConditionExtension($token, $trigger, $params=array(), $seq=null) {
+		$tpl = DevblocksPlatform::getTemplateService();
+		$tpl->assign('params', $params);
+
+		if(!is_null($seq))
+			$tpl->assign('namePrefix','condition'.$seq);
+		
+		switch($token) {
+		}
+
+		$tpl->clearAssign('namePrefix');
+		$tpl->clearAssign('params');
+	}
+	
+	function runConditionExtension($token, $trigger, $params, DevblocksDictionaryDelegate $dict) {
+		$pass = true;
+		
+		switch($token) {
+			default:
+				$pass = false;
+				break;
+		}
+		
+		return $pass;
+	}
+	
+	function getActionExtensions() {
+		$actions = 
+			array(
+// 				'add_watchers' => array('label' =>'Add watchers'),
+				'create_comment' => array('label' =>'Create a comment'),
+				'create_notification' => array('label' =>'Create a notification'),
+				'create_task' => array('label' =>'Create a task'),
+				'create_ticket' => array('label' =>'Create a ticket'),
+				'schedule_behavior' => array('label' => 'Schedule behavior'),
+				'send_email' => array('label' => 'Send email'),
+				'unschedule_behavior' => array('label' => 'Unschedule behavior'),
+			)
+			+ DevblocksEventHelper::getActionCustomFields(CerberusContexts::CONTEXT_GROUP)
+			;
+			
+		return $actions;
+	}
+	
+	function renderActionExtension($token, $trigger, $params=array(), $seq=null) {
+		$tpl = DevblocksPlatform::getTemplateService();
+		$tpl->assign('params', $params);
+
+		if(!is_null($seq))
+			$tpl->assign('namePrefix','action'.$seq);
+
+		$labels = $this->getLabels($trigger);
+		$tpl->assign('token_labels', $labels);
+			
+		switch($token) {
+// 			case 'add_watchers':
+// 				DevblocksEventHelper::renderActionAddWatchers($trigger);
+// 				break;
+			
+			case 'create_comment':
+				DevblocksEventHelper::renderActionCreateComment($trigger);
+				break;
+				
+			case 'create_notification':
+				DevblocksEventHelper::renderActionCreateNotification($trigger);
+				break;
+				
+			case 'create_task':
+				DevblocksEventHelper::renderActionCreateTask($trigger);
+				break;
+				
+			case 'create_ticket':
+				DevblocksEventHelper::renderActionCreateTicket($trigger);
+				break;
+				
+			case 'schedule_behavior':
+				$dates = array();
+				$conditions = $this->getConditions($trigger);
+				foreach($conditions as $key => $data) {
+					if($data['type'] == Model_CustomField::TYPE_DATE)
+					$dates[$key] = $data['label'];
+				}
+				$tpl->assign('dates', $dates);
+			
+				DevblocksEventHelper::renderActionScheduleBehavior($trigger);
+				break;
+
+			case 'send_email':
+				DevblocksEventHelper::renderActionSendEmail($trigger);
+				break;
+
+			case 'unschedule_behavior':
+				DevblocksEventHelper::renderActionUnscheduleBehavior($trigger);
+				break;
+				
+			default:
+				if('set_cf_' == substr($token,0,7)) {
+					$field_id = substr($token,7);
+					$custom_field = DAO_CustomField::get($field_id);
+					DevblocksEventHelper::renderActionSetCustomField($custom_field);
+				}
+				break;
+		}
+		
+		$tpl->clearAssign('params');
+		$tpl->clearAssign('namePrefix');
+		$tpl->clearAssign('token_labels');		
+	}
+	
+	function simulateActionExtension($token, $trigger, $params, DevblocksDictionaryDelegate $dict) {
+		@$group_id = $dict->group_id;
+
+		if(empty($group_id))
+			return;
+		
+		switch($token) {
+// 			case 'add_watchers':
+// 				return DevblocksEventHelper::simulateActionAddWatchers($params, $dict, 'group_id');
+// 				break;
+			case 'create_comment':
+				return DevblocksEventHelper::simulateActionCreateComment($params, $dict, 'group_id');
+				break;
+			case 'create_notification':
+				return DevblocksEventHelper::simulateActionCreateNotification($params, $dict, 'group_id');
+				break;
+			case 'create_task':
+				return DevblocksEventHelper::simulateActionCreateTask($params, $dict, 'group_id');
+				break;
+			case 'create_ticket':
+				return DevblocksEventHelper::simulateActionCreateTicket($params, $dict);
+				break;
+			case 'schedule_behavior':
+				return DevblocksEventHelper::simulateActionScheduleBehavior($params, $dict);
+				break;
+			case 'send_email':
+				return DevblocksEventHelper::simulateActionSendEmail($params, $dict);
+				break;
+			case 'unschedule_behavior':
+				return DevblocksEventHelper::simulateActionUnscheduleBehavior($params, $dict);
+				break;
+			default:
+				if('set_cf_' == substr($token,0,7)) {
+					$field_id = substr($token,7);
+					$custom_field = DAO_CustomField::get($field_id);
+					$context = null;
+					$context_id = null;
+					
+					// If different types of custom fields, need to find the proper context_id
+					switch($custom_field->context) {
+						case CerberusContexts::CONTEXT_GROUP:
+							$context = $custom_field->context;
+							$context_id = $group_id;
+							break;
+					}
+					
+					if(!empty($context) && !empty($context_id))
+						return DevblocksEventHelper::simulateActionSetCustomField($custom_field, 'group_custom', $params, $dict, $context, $context_id);
+				}
+				break;
+		}
+	}
+	
+	function runActionExtension($token, $trigger, $params, DevblocksDictionaryDelegate $dict) {
+		@$group_id = $dict->group_id;
+
+		if(empty($group_id))
+			return;
+		
+		switch($token) {
+// 			case 'add_watchers':
+// 				DevblocksEventHelper::runActionAddWatchers($params, $dict, 'group_id');
+// 				break;
+			
+			case 'create_comment':
+				DevblocksEventHelper::runActionCreateComment($params, $dict, 'group_id');
+				break;
+				
+			case 'create_notification':
+				DevblocksEventHelper::runActionCreateNotification($params, $dict, 'group_id');
+				break;
+				
+			case 'create_task':
+				DevblocksEventHelper::runActionCreateTask($params, $dict, 'group_id');
+				break;
+
+			case 'create_ticket':
+				DevblocksEventHelper::runActionCreateTicket($params, $dict);
+				break;
+				
+			case 'schedule_behavior':
+				DevblocksEventHelper::runActionScheduleBehavior($params, $dict);
+				break;
+
+			case 'send_email':
+				DevblocksEventHelper::runActionSendEmail($params, $dict);
+				break;
+				
+			case 'unschedule_behavior':
+				DevblocksEventHelper::runActionUnscheduleBehavior($params, $dict);
+				break;
+				
+			default:
+				if('set_cf_' == substr($token,0,7)) {
+					$field_id = substr($token,7);
+					$custom_field = DAO_CustomField::get($field_id);
+					$context = null;
+					$context_id = null;
+					
+					// If different types of custom fields, need to find the proper context_id
+					switch($custom_field->context) {
+						case CerberusContexts::CONTEXT_GROUP:
+							$context = $custom_field->context;
+							$context_id = $group_id;
+							break;
+					}
+					
+					if(!empty($context) && !empty($context_id))
+						DevblocksEventHelper::runActionSetCustomField($custom_field, 'group_custom', $params, $dict, $context, $context_id);
+				}
+				break;	
+		}
+	}
+	
+};
