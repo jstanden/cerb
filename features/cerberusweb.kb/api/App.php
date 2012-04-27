d1f380fd2fef1c516cf8a8a0c5378ac5204e62f1
Jeff Standen <jeff@webgroupmedia.com>
2012-04-26 21:21:04 -0700

    * [Knowledgebase] Moved the Search tab from the Knowledgebase page to the new global Search page.
    [Code Cleanup] Removed the hardcoded quick search from KB.

--- features/cerberusweb.kb/api/App.php
+++ features/cerberusweb.kb/api/App.php
@@ -327,32 +327,6 @@ class ChKbBrowseTab extends Extension_KnowledgebaseTab {
 }
 endif;
 
-if (class_exists('Extension_KnowledgebaseTab')):
-class ChKbSearchTab extends Extension_KnowledgebaseTab {
-	const VIEW_ID = 'kb_search';
-	
-	function showTab() {
-		$visit = CerberusApplication::getVisit();
-		$translate = DevblocksPlatform::getTranslationService();
-		
-		$tpl = DevblocksPlatform::getTemplateService();
-
-		// [TODO] Convert to $defaults
-		
-		if(null == ($view = C4_AbstractViewLoader::getView(self::VIEW_ID))) {
-			$view = new View_KbArticle();
-			$view->id = self::VIEW_ID;
-			$view->name = $translate->_('common.search_results');
-			C4_AbstractViewLoader::setView($view->id, $view);
-		}
-		
-		$tpl->assign('view', $view);
-		
-		$tpl->display('devblocks:cerberusweb.kb::kb/tabs/search/index.tpl');
-	}
-}
-endif;
-
 if (class_exists('Extension_ReplyToolbarItem',true)):
 	class ChKbReplyToolbarButton extends Extension_ReplyToolbarItem {
 		function render(Model_Message $message) { 
@@ -612,40 +586,6 @@ class ChKbAjaxController extends DevblocksControllerExtension {
 		
 		// JSON
 		echo json_encode(array('id'=>$id));
-	}
-	
-	function doArticleQuickSearchAction() {
-        @$type = DevblocksPlatform::importGPC($_POST['type'],'string'); 
-        @$query = DevblocksPlatform::importGPC($_POST['query'],'string');
-
-        $visit = CerberusApplication::getVisit(); /* @var $visit CerberusVisit */
-        $translate = DevblocksPlatform::getTranslationService();
-		
-        if(null == ($searchView = C4_AbstractViewLoader::getView(ChKbSearchTab::VIEW_ID))) {
-        	$searchView = new View_KbArticle();
-        	$searchView->id = ChKbSearchTab::VIEW_ID;
-        	$searchView->name = $translate->_('common.search_results');
-        	C4_AbstractViewLoader::setView($searchView->id, $searchView);
-        }
-		
-        $params = array();
-        
-        switch($type) {
-            case "articles_all":
-				$params[SearchFields_KbArticle::FULLTEXT_ARTICLE_CONTENT] = new DevblocksSearchCriteria(SearchFields_KbArticle::FULLTEXT_ARTICLE_CONTENT,DevblocksSearchCriteria::OPER_FULLTEXT,array($query,'all'));
-                break;
-            case "articles_phrase":
-				$params[SearchFields_KbArticle::FULLTEXT_ARTICLE_CONTENT] = new DevblocksSearchCriteria(SearchFields_KbArticle::FULLTEXT_ARTICLE_CONTENT,DevblocksSearchCriteria::OPER_FULLTEXT,array($query,'phrase'));
-                break;
-        }
-        
-        $searchView->addParams($params, true);
-        $searchView->renderPage = 0;
-        $searchView->renderSortBy = null;
-        
-        C4_AbstractViewLoader::setView($searchView->id,$searchView);
-        
-        DevblocksPlatform::redirect(new DevblocksHttpResponse(array('kb','search')));
 	}
 	
 	function showKbCategoryEditPanelAction() {
--- features/cerberusweb.kb/templates/kb/tabs/articles/index.tpl
+++ features/cerberusweb.kb/templates/kb/tabs/articles/index.tpl
@@ -23,14 +23,7 @@
 	<td width="98%" valign="middle">
 	</td>
 	<td width="1%" nowrap="nowrap" valign="middle" align="right">
-		<form action="{devblocks_url}{/devblocks_url}" method="post">
-		<input type="hidden" name="c" value="kb.ajax">
-		<input type="hidden" name="a" value="doArticleQuickSearch">
-		<span><b>{$translate->_('common.search')|capitalize}:</b></span> <select name="type">
-			<option value="articles_all">Articles (all words)</option>
-			<option value="articles_phrase">Articles (phrase)</option>
-		</select><input type="text" name="query" class="input_search" size="24"><button type="submit">go!</button>
-		</form>
+		{* [TODO] Quick search *}
 	</td>
 </tr>
 </table>
