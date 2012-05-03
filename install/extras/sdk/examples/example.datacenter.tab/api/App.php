df2f0b5685f5f87548dd0836ad7fe0c51254da7e
Jeff Standen <jeff@webgroupmedia.com>
2012-05-02 19:05:30 -0700

    [Code Cleanup/SDK] Removed the datacenter tab SDK example since this page was removed.

--- install/extras/sdk/examples/example.datacenter.tab/api/App.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (class_exists('Extension_DatacenterTab')):
-class ExDatacenterTab extends Extension_DatacenterTab {
-	function showTab() {
-		$tpl = DevblocksPlatform::getTemplateService();		
-		$tpl->display('devblocks:example.datacenter.tab::index.tpl');		
-	}
-}
-endif;
