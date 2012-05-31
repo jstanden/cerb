8efffb094e26b518b1531d1ae3b8c540be276cc5
Jeff Standen <jeff@webgroupmedia.com>
2012-05-31 16:04:37 -0700

    [Code Cleanup/Opps/Links] Fixed an issue where 'Create & Link' for opportunities didn't actually link the issue to the target record.

--- features/cerberusweb.crm/api/App.php
+++ features/cerberusweb.crm/api/App.php
@@ -88,7 +88,7 @@ class CrmPage extends CerberusPageExtension {
 			// Context Link (if given)
 			@$link_context = DevblocksPlatform::importGPC($_REQUEST['link_context'],'string','');
 			@$link_context_id = DevblocksPlatform::importGPC($_REQUEST['link_context_id'],'integer','');
-			if(!empty($id) && !empty($link_context) && !empty($link_context_id)) {
+			if(!empty($opp_id) && !empty($link_context) && !empty($link_context_id)) {
 				DAO_ContextLink::setLink(CerberusContexts::CONTEXT_OPPORTUNITY, $opp_id, $link_context, $link_context_id);
 			}
 			
