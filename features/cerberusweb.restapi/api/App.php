bcc920951c5eb81f294de35d073edaa3241793f7
Jeff Standen <jeff@webgroupmedia.com>
2012-10-02 18:32:08 -0700

    * [Web-API/Performance] The Web-API will now lazy-load linked records on GET requests and search results when the 'expand' parameter is provided.  This is a comma-delimited list of extra fields to include.  If a field loads another object, all of those fields will be returned as well.  For example, if you want all the associated organization fields to be returned when retrieving a ticket, include 'ticket_org_' in the 'expand' parameter.  The reason this information is not returned by default is because there can be a dozen linked records per result.  It's inefficient to load all this information when it isn't needed, so the 'expand' parameter allows you to specify when you need more info, like custom fields, watchers, etc.

--- features/cerberusweb.restapi/api/App.php
+++ features/cerberusweb.restapi/api/App.php
@@ -66,11 +66,13 @@ class Plugin_RestAPI {
 			
 			header("Content-type: text/javascript; charset=utf-8");
 			echo json_encode($array);
+			
 		} elseif ('xml' == $format) {
 			header("Content-type: text/xml; charset=utf-8");
 			$xml = new SimpleXMLElement("<response/>");
 			self::xml_encode($array, $xml);
 			echo $xml->asXML();
+			
 		} else {
 			header("Content-type: text/plain; charset=utf-8");
 			echo "'" . $format . "' is not implemented.";
@@ -412,7 +414,35 @@ abstract class Extension_RestController extends DevblocksExtension {
 	protected function success($array) {
 		if(!is_array($array))
 			return false;
-			
+
+		@$expand = DevblocksPlatform::parseCsvString(DevblocksPlatform::importGPC($_REQUEST['expand'],'string',null));
+
+		// Do we need to lazy load some fields to be helpful?
+		if(is_array($expand) && !empty($expand)) {
+			if(isset($array['_context'])) {
+				$dict = new DevblocksDictionaryDelegate($array);
+				
+				foreach($expand as $expand_field)
+					$dict->$expand_field;
+				
+				$array = $dict->getDictionary();
+				
+			} elseif(isset($array['results'])) {
+				foreach($array['results'] as $k => $v) {
+					if(!isset($array['results'][$k]['_context']))
+						continue;
+					
+					$dict = new DevblocksDictionaryDelegate($array['results'][$k]);
+					
+					foreach($expand as $expand_field)
+						$dict->$expand_field;
+					
+					$array['results'][$k] = $dict->getDictionary();
+				}
+				
+			}
+		}
+		
 		$out = array(
 			'__status' => 'success',
 			'__version' => APP_VERSION,
