65f94fef95e13b70bdb42910874e251862fd575d
Jeff Standen <jeff@webgroupmedia.com>
2012-12-10 08:50:03 -0800

    * [Plugin Dev/Events] Added a new 'dao.contact_person.update' event for plugin listeners to react to Contact Person record changes.

--- features/cerberusweb.core/api/dao/contact_person.php
+++ features/cerberusweb.core/api/dao/contact_person.php
@@ -39,10 +39,45 @@ class DAO_ContactPerson extends C4_ORMHelper {
 	}
 	
 	static function update($ids, $fields) {
-		parent::_update($ids, 'contact_person', $fields);
+		if(!is_array($ids))
+			$ids = array($ids);
 		
-		// Log the context update
-		DevblocksPlatform::markContextChanged(CerberusContexts::CONTEXT_CONTACT_PERSON, $ids);
+		// Make a diff for the requested objects in batches
+		
+		$chunks = array_chunk($ids, 100, true);
+		while($batch_ids = array_shift($chunks)) {
+			if(empty($batch_ids))
+				continue;
+			
+			// Get state before changes
+			$object_changes = parent::_getUpdateDeltas($batch_ids, $fields, get_class());
+
+			if(empty($object_changes))
+				continue;
+			
+			// Make changes
+			parent::_update($batch_ids, 'contact_person', $fields);
+			
+			// Send events
+			if(!empty($object_changes)) {
+				// Local events
+				//self::_processUpdateEvents($object_changes);
+				
+				// Trigger an event about the changes
+				$eventMgr = DevblocksPlatform::getEventService();
+				$eventMgr->trigger(
+					new Model_DevblocksEvent(
+						'dao.contact_person.update',
+						array(
+							'objects' => $object_changes,
+						)
+					)
+				);
+				
+				// Log the context update
+				DevblocksPlatform::markContextChanged(CerberusContexts::CONTEXT_CONTACT_PERSON, $batch_ids);
+			}
+		}
 	}
 	
 	static function updateWhere($fields, $where) {
