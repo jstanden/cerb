a909e9e1b854300b82aaf1970422a57dea58453d
Jeff Standen <jeff@webgroupmedia.com>
2012-12-10 11:35:50 -0800

    [Code Cleanup] Cleaned up trailing whitespace in ./features/cerberusweb.translators/

--- features/cerberusweb.translators/api/App.php
+++ features/cerberusweb.translators/api/App.php
@@ -16,31 +16,31 @@
 ***********************************************************************/
 /*
  * IMPORTANT LICENSING NOTE from your friends on the Cerberus Helpdesk Team
- * 
- * Sure, it would be so easy to just cheat and edit this file to use the 
- * software without paying for it.  But we trust you anyway.  In fact, we're 
- * writing this software for you! 
- * 
- * Quality software backed by a dedicated team takes money to develop.  We 
- * don't want to be out of the office bagging groceries when you call up 
- * needing a helping hand.  We'd rather spend our free time coding your 
- * feature requests than mowing the neighbors' lawns for rent money. 
- * 
- * We've never believed in hiding our source code out of paranoia over not 
- * getting paid.  We want you to have the full source code and be able to 
- * make the tweaks your organization requires to get more done -- despite 
- * having less of everything than you might need (time, people, money, 
+ *
+ * Sure, it would be so easy to just cheat and edit this file to use the
+ * software without paying for it.  But we trust you anyway.  In fact, we're
+ * writing this software for you!
+ *
+ * Quality software backed by a dedicated team takes money to develop.  We
+ * don't want to be out of the office bagging groceries when you call up
+ * needing a helping hand.  We'd rather spend our free time coding your
+ * feature requests than mowing the neighbors' lawns for rent money.
+ *
+ * We've never believed in hiding our source code out of paranoia over not
+ * getting paid.  We want you to have the full source code and be able to
+ * make the tweaks your organization requires to get more done -- despite
+ * having less of everything than you might need (time, people, money,
  * energy).  We shouldn't be your bottleneck.
- * 
- * We've been building our expertise with this project since January 2002.  We 
- * promise spending a couple bucks [Euro, Yuan, Rupees, Galactic Credits] to 
- * let us take over your shared e-mail headache is a worthwhile investment.  
- * It will give you a sense of control over your inbox that you probably 
- * haven't had since spammers found you in a game of 'E-mail Battleship'. 
+ *
+ * We've been building our expertise with this project since January 2002.  We
+ * promise spending a couple bucks [Euro, Yuan, Rupees, Galactic Credits] to
+ * let us take over your shared e-mail headache is a worthwhile investment.
+ * It will give you a sense of control over your inbox that you probably
+ * haven't had since spammers found you in a game of 'E-mail Battleship'.
  * Miss. Miss. You sunk my inbox!
- * 
- * A legitimate license entitles you to support from the developers,  
- * and the warm fuzzy feeling of feeding a couple of obsessed developers 
+ *
+ * A legitimate license entitles you to support from the developers,
+ * and the warm fuzzy feeling of feeding a couple of obsessed developers
  * who want to help you get more done.
  *
  * - Jeff Standen, Darren Sugita, Dan Hildebrandt, Scott Luther
@@ -209,7 +209,7 @@ class ChTranslators_SetupPageSection extends Extension_PageSection {
 				// If we have a valid source, copy its override or its default (in that order)
 				@$copy_string = $copy_strings[$string_id];
 				if($copy_string instanceof Model_Translation)
-					$override = 
+					$override =
 						!empty($copy_string->string_override)
 						? $copy_string->string_override
 						: $copy_string->string_default
@@ -244,9 +244,9 @@ class ChTranslators_SetupPageSection extends Extension_PageSection {
 			), true);
 			
 			/*
-			 * If we didn't copy from another language, only show empty strings 
+			 * If we didn't copy from another language, only show empty strings
 			 * which makes it easier to translate in the GUI.
-			 */ 
+			 */
 			if(empty($copy_lang_code)) {
 				$view->addParam(new DevblocksSearchCriteria(SearchFields_Translation::STRING_OVERRIDE,DevblocksSearchCriteria::OPER_EQ,''));
 			}
@@ -361,7 +361,7 @@ class ChTranslators_SetupPageSection extends Extension_PageSection {
 		self::_clearCache();
 		
 		DevblocksPlatform::redirect(new DevblocksHttpResponse(array('config','translations')));
-	}	
+	}
 }
 endif;
 
@@ -467,7 +467,7 @@ class C4_TranslationView extends C4_AbstractView implements IAbstractView_Subtot
 		}
 		
 		return $counts;
-	}	
+	}
 	
 	function render() {
 		$this->_sanitize();
