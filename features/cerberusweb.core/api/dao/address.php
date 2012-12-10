9cc517b80c9183bf40df8822eb263070250747fc
Jeff Standen <jeff@webgroupmedia.com>
2012-12-10 08:49:17 -0800

    * [Plugin Dev/Events] Added a new 'dao.address.update' event for plugin listeners to react to Address record changes.

--- features/cerberusweb.core/api/dao/address.php
+++ features/cerberusweb.core/api/dao/address.php
@@ -98,10 +98,45 @@ class DAO_Address extends C4_ORMHelper {
 	}
 	
 	static function update($ids, $fields) {
-		parent::_update($ids, 'address', $fields);
+		if(!is_array($ids))
+			$ids = array($ids);
 		
-		// Log the context update
-		DevblocksPlatform::markContextChanged(CerberusContexts::CONTEXT_ADDRESS, $ids);
+		// Make a diff for the requested objects in batches
+		
+		$chunks = array_chunk($ids, 100, true);
+		while($batch_ids = array_shift($chunks)) {
+			if(empty($batch_ids))
+				continue;
+			
+			// Get state before changes
+			$object_changes = parent::_getUpdateDeltas($batch_ids, $fields, get_class());
+
+			if(empty($object_changes))
+				continue;
+			
+			// Make changes
+			parent::_update($batch_ids, 'address', $fields);
+			
+			// Send events
+			if(!empty($object_changes)) {
+				// Local events
+				//self::_processUpdateEvents($object_changes);
+				
+				// Trigger an event about the changes
+				$eventMgr = DevblocksPlatform::getEventService();
+				$eventMgr->trigger(
+					new Model_DevblocksEvent(
+						'dao.address.update',
+						array(
+							'objects' => $object_changes,
+						)
+					)
+				);
+				
+				// Log the context update
+				DevblocksPlatform::markContextChanged(CerberusContexts::CONTEXT_ADDRESS, $batch_ids);
+			}
+		}
 	}
 	
 	static function updateWhere($fields, $where) {
