321949668beef25eadba27015fd8a803544dcd63
Jeff Standen <jeff@webgroupmedia.com>
2012-12-12 23:05:09 -0800

    [Devblocks/Platform/jQuery] The .insertAtCursor() jQuery plugin now triggers a 'change' event on the affected fields.  This lets the .elastic() plugin change the form element size, etc.

--- libs/devblocks/resources/js/jquery/_development/jquery.insertatcursor.js
+++ libs/devblocks/resources/js/jquery/_development/jquery.insertatcursor.js
@@ -1,26 +1,30 @@
 // Reference: http://stackoverflow.com/questions/946534/insert-text-into-textarea-with-jquery/2819568#2819568
 $.fn.extend({
-insertAtCursor: function(myValue){
-  this.each(function(i) {
-    if (document.selection) {
-      this.focus();
-      sel = document.selection.createRange();
-      sel.text = myValue;
-      this.focus();
-    }
-    else if (this.selectionStart || this.selectionStart == '0') {
-      var startPos = this.selectionStart;
-      var endPos = this.selectionEnd;
-      var scrollTop = this.scrollTop;
-      this.value = this.value.substring(0, startPos)+myValue+this.value.substring(endPos,this.value.length);
-      this.focus();
-      this.selectionStart = startPos + myValue.length;
-      this.selectionEnd = startPos + myValue.length;
-      this.scrollTop = scrollTop;
-    } else {
-      this.value += myValue;
-      this.focus();
-    }
-  })
-}
+	insertAtCursor : function(myValue) {
+		this.each(function(i) {
+			if (document.selection) {
+				this.focus();
+				sel = document.selection.createRange();
+				sel.text = myValue;
+				this.focus();
+			} else if (this.selectionStart || this.selectionStart == '0') {
+				var startPos = this.selectionStart;
+				var endPos = this.selectionEnd;
+				var scrollTop = this.scrollTop;
+				this.value = this.value.substring(0, startPos) + myValue
+						+ this.value.substring(endPos, this.value.length);
+				this.focus();
+				this.selectionStart = startPos + myValue.length;
+				this.selectionEnd = startPos + myValue.length;
+				this.scrollTop = scrollTop;
+			} else {
+				this.value += myValue;
+				this.focus();
+			}
+			
+			$(this).trigger('change');
+		});
+		
+		return this;
+	}
 });
