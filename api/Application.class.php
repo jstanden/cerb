783333ce9b20ee3c3f0aa32b275cd9435f0af260
Jeff Standen <jeff@webgroupmedia.com>
2012-12-17 16:53:52 -0800

    6.2-release checkpoint

--- api/Application.class.php
+++ api/Application.class.php
@@ -46,8 +46,8 @@
  * - Jeff Standen, Darren Sugita, Dan Hildebrandt, Scott Luther
  *	 WEBGROUP MEDIA LLC. - Developers of Cerberus Helpdesk
  */
-define("APP_BUILD", 2012121501);
-define("APP_VERSION", '6.2.0-rc1');
+define("APP_BUILD", 2012121701);
+define("APP_VERSION", '6.2.0');
 
 define("APP_MAIL_PATH", APP_STORAGE_PATH . '/mail/');
 
