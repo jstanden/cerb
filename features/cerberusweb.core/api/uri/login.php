347fe32145e76f46f9b5680ed008a42d31e0941c
Jeff Standen <jeff@webgroupmedia.com>
2012-12-19 18:19:34 -0800

    * [CHD-3221] [Login] When logging in from a disabled worker account, a friendly error message is now properly displayed.

--- features/cerberusweb.core/api/uri/login.php
+++ features/cerberusweb.core/api/uri/login.php
@@ -166,6 +166,7 @@ class ChSignInPage extends CerberusPageExtension {
 			case 'failed':
 			case NULL:
 				@$url = DevblocksPlatform::importGPC($_REQUEST['url'], 'string', '');
+				@$error = DevblocksPlatform::importGPC($_REQUEST['error'], 'string', '');
 				
 				if(!empty($url))
 					$_SESSION['login_post_url'] = $url;
@@ -191,6 +192,9 @@ class ChSignInPage extends CerberusPageExtension {
 					$email = $remember_email;
 				
 				$tpl->assign('email', $email);
+				
+				if(!empty($error))
+					$tpl->assign('error', $error);
 				
 				switch($section) {
 					case 'failed':
