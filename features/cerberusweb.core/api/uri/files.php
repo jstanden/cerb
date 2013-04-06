fd8db78e8f5007ed803e7e0db6322f475075875c
Jeff Standen <jeff@webgroupmedia.com>
2013-04-06 14:55:54 -0700

    * [Mail/Attachments/Usability] When hovering over a file attachment with the mouse, a 'download' link is now revealed.  Clicking this link will prompt for an attachment to be downloaded rather than displayed in the browser.  Previously, clicking on an attachment always attempted to display it in the browser first (e.g. images, sound files, videos, PDFs, HTML, etc).  When displaying HTML content in the browser, Cerb repairs and purifies it to remove Javascript.  When downloading an HTML attachment, the raw file is provided without any sanitization.  This is useful behavior if you're sharing HTML files with safe contacts and you do not want the contents to be modified.  By downloading HTML, any potentially malicious Javascript no longer has access to the worker's Cerb session.

--- features/cerberusweb.core/api/uri/files.php
+++ features/cerberusweb.core/api/uri/files.php
@@ -55,13 +55,14 @@ class ChFilesController extends DevblocksControllerExtension {
 			
 		$file = $link->getAttachment();
 		
-		
 		if(false === ($fp = DevblocksPlatform::getTempFile()))
 			die("Could not open a temporary file.");
 		if(false === $file->getFileContents($fp))
 			die("Error reading resource.");
 			
 		$file_stats = fstat($fp);
+		
+		$is_download = isset($request->query['download']) ? true : false;
 
 		// Set headers
 		header("Expires: Mon, 26 Nov 1979 00:00:00 GMT");
@@ -70,10 +71,21 @@ class ChFilesController extends DevblocksControllerExtension {
 //		header("Keep-Alive: timeout=5, max=100");
 //		header("Connection: Keep-Alive");
 
+		if($is_download) {
+			header('Content-Disposition: attachment; filename=' . urlencode($file->display_name));
+		}
+		
 		header("Content-Type: " . $file->mime_type);
 		
 		switch(strtolower($file->mime_type)) {
 			case 'text/html':
+				// If we're downloading the HTML, just pass the raw bytes
+				if($is_download) {
+					header("Content-Length: " . $file_stats['size']);
+					fpassthru($fp);
+					
+				// If we're displaying the HTML inline, tidy and purify it first
+				} else {
 					// If the 'tidy' extension exists, and the file size is less than 5MB
 					if(extension_loaded('tidy') && $file_stats['size'] < 1024000 * 5) {
 						$tidy = new tidy();
@@ -86,6 +98,7 @@ class ChFilesController extends DevblocksControllerExtension {
 					$clean_html = DevblocksPlatform::purifyHTML($fp);
 					header("Content-Length: " . strlen($clean_html));
 					echo $clean_html;
+				}
 				break;
 				
 			default:
