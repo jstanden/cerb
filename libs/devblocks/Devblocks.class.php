6bdf537d64a78d533ca59113e30ad53b958ae880
Jeff Standen <jeff@webgroupmedia.com>
2013-04-19 13:51:25 -0700

    * [Platform/Devblocks/Plugins] Implemented DevblocksPlatform::strFormatJson($json) to format JSON in a human-friendly output.  Nested properties are indented, and one key/value pair or array element is displayed per line.

--- libs/devblocks/Devblocks.class.php
+++ libs/devblocks/Devblocks.class.php
@@ -890,6 +890,66 @@ class DevblocksPlatform extends DevblocksEngine {
 	}
 	
 	/**
+	 * Indents a flat JSON string to make it more human-readable.
+	 *
+	 * @author http://www.daveperrett.com/articles/2008/03/11/format-json-with-php/
+	 * @todo This won't be needed when we require PHP 5.4+ with JSON_PRETTY_PRINT
+	 *
+	 * @param string $json The original JSON string to process.
+	 *
+	 * @return string Indented version of the original JSON string.
+	 */
+	static function strFormatJson($json) {
+		$result = '';
+		$pos = 0;
+		$strLen  = strlen($json);
+		$indentStr = '  ';
+		$newLine = "\n";
+		$prevChar = '';
+		$outOfQuotes = true;
+
+		for ($i=0; $i<=$strLen; $i++) {
+
+			// Grab the next character in the string.
+			$char = substr($json, $i, 1);
+
+			// Are we inside a quoted string?
+			if ($char == '"' && $prevChar != '\\') {
+				$outOfQuotes = !$outOfQuotes;
+
+				// If this character is the end of an element,
+				// output a new line and indent the next line.
+			} else if(($char == '}' || $char == ']') && $outOfQuotes) {
+				$result .= $newLine;
+				$pos --;
+				for ($j=0; $j<$pos; $j++) {
+					$result .= $indentStr;
+				}
+			}
+
+			// Add the character to the result string.
+			$result .= $char;
+
+			// If the last character was the beginning of an element,
+			// output a new line and indent the next line.
+			if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
+				$result .= $newLine;
+				if ($char == '{' || $char == '[') {
+					$pos ++;
+				}
+
+				for ($j = 0; $j < $pos; $j++) {
+					$result .= $indentStr;
+				}
+			}
+
+			$prevChar = $char;
+		}
+
+		return $result;
+	}
+
+	/**
 	 * Clears any platform-level plugin caches.
 	 *
 	 */
