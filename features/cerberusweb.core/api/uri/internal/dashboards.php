b0895867ca03ee3b916f972ffd53db65e9622f82
Jeff Standen <jeff@webgroupmedia.com>
2013-05-21 17:18:06 -0700

    * [Dashboards/Countdown] A new countdown widget is available on dashboards. You can use these widgets as gentle reminders of upcoming events. For example, on a development dashboard you can display a countdown to the next milestone (an iteration or release). On a sales dashboard, you can display the time left in the current period (quarter, year).

--- features/cerberusweb.core/api/uri/internal/dashboards.php
+++ features/cerberusweb.core/api/uri/internal/dashboards.php
@@ -700,6 +700,113 @@ class WorkspaceWidget_Counter extends Extension_WorkspaceWidget implements ICerb
 	}
 };
 
+class WorkspaceWidget_Countdown extends Extension_WorkspaceWidget implements ICerbWorkspaceWidget_ExportData {
+	function render(Model_WorkspaceWidget $widget) {
+		$tpl = DevblocksPlatform::getTemplateService();
+
+		if(!isset($widget->params['target_timestamp'])) {
+			echo "This countdown doesn't have a target date. Configure it and set one.";
+			return;
+		}
+		
+		$tpl->assign('widget', $widget);
+		
+		$tpl->display('devblocks:cerberusweb.core::internal/workspaces/widgets/countdown/countdown.tpl');
+	}
+	
+	// Config
+	
+	function renderConfig(Model_WorkspaceWidget $widget) {
+		if(empty($widget))
+			return;
+		
+		$tpl = DevblocksPlatform::getTemplateService();
+		
+		// Widget
+		
+		$tpl->assign('widget', $widget);
+		
+		// Template
+		
+		$tpl->display('devblocks:cerberusweb.core::internal/workspaces/widgets/countdown/config.tpl');
+	}
+	
+	function saveConfig(Model_WorkspaceWidget $widget) {
+		@$params = DevblocksPlatform::importGPC($_REQUEST['params'], 'array', array());
+		
+		if(isset($params['target_timestamp'])) {
+			@$timestamp = intval(strtotime($params['target_timestamp']));
+			$params['target_timestamp'] = $timestamp;
+		}
+		
+		DAO_WorkspaceWidget::update($widget->id, array(
+			DAO_WorkspaceWidget::PARAMS_JSON => json_encode($params),
+		));
+	}
+	
+	// Export
+	
+	function exportData(Model_WorkspaceWidget $widget, $format=null) {
+		switch(strtolower($format)) {
+			case 'csv':
+				return $this->_exportDataAsCsv($widget);
+				break;
+				
+			default:
+			case 'json':
+				return $this->_exportDataAsJson($widget);
+				break;
+		}
+		
+		return false;
+	}
+	
+	private function _exportDataAsCsv(Model_WorkspaceWidget $widget) {
+		@$diff = max(0, intval($widget->params['target_timestamp']) - time());
+		
+		$results = array(
+			'Label' => $widget->label,
+			'Timestamp' => $widget->params['target_timestamp'],
+			'Output' => DevblocksPlatform::strSecsToString($diff, 2),
+		);
+
+		$fp = fopen("php://temp", 'r+');
+		
+		fputcsv($fp, array_keys($results));
+		fputcsv($fp, array_values($results));
+		
+		rewind($fp);
+
+		$output = "";
+		
+		while(!feof($fp)) {
+			$output .= fgets($fp);
+		}
+		
+		fclose($fp);
+		
+		return $output;
+	}
+	
+	private function _exportDataAsJson(Model_WorkspaceWidget $widget) {
+		@$diff = max(0, intval($widget->params['target_timestamp']) - time());
+		
+		$results = array(
+			'widget' => array(
+				'label' => $widget->label,
+				'type' => 'countdown',
+				'version' => 'Cerb ' . APP_VERSION,
+				'countdown' => array(
+					'output' => DevblocksPlatform::strSecsToString($diff, 2),
+					'timestamp' => $widget->params['target_timestamp'],
+				),
+			),
+		);
+		
+		return DevblocksPlatform::strFormatJson(json_encode($results));
+	}
+};
+
 class WorkspaceWidget_Chart extends Extension_WorkspaceWidget implements ICerbWorkspaceWidget_ExportData {
 	private function _loadData(Model_WorkspaceWidget &$widget) {
 		@$series = $widget->params['series'];
