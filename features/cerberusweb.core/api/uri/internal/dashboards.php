e371c02beae1bf64836575d6f260a3004d1edd8b
Jeff Standen <jeff@webgroupmedia.com>
2013-05-21 20:16:14 -0700

    * [Dashboards/Clock] A new world clock widget is available on dashboards.  The clocks support 12-hour or 24-hour display, and the closest timezone can be quickly selected from the list of major cities organized by continent and country.  These widgets are particularly useful for coordinating with international team members or sales prospects.

--- features/cerberusweb.core/api/uri/internal/dashboards.php
+++ features/cerberusweb.core/api/uri/internal/dashboards.php
@@ -560,6 +560,129 @@ class WorkspaceWidget_Gauge extends Extension_WorkspaceWidget implements ICerbWo
 	}
 };
 
+class WorkspaceWidget_Clock extends Extension_WorkspaceWidget implements ICerbWorkspaceWidget_ExportData {
+	function render(Model_WorkspaceWidget $widget) {
+		$tpl = DevblocksPlatform::getTemplateService();
+
+		@$timezone = $widget->params['timezone'];
+		
+		if(empty($timezone)) {
+			echo "This clock doesn't have a timezone. Configure it and set one.";
+			return;
+		}
+		
+		$datetimezone = new DateTimeZone($timezone);
+		$datetime = new DateTime('now', $datetimezone);
+
+		$offset = $datetimezone->getOffset($datetime);
+		$tpl->assign('offset', $offset);
+		
+		$tpl->assign('widget', $widget);
+		
+		$tpl->display('devblocks:cerberusweb.core::internal/workspaces/widgets/clock/clock.tpl');
+	}
+	
+	// Config
+	
+	function renderConfig(Model_WorkspaceWidget $widget) {
+		if(empty($widget))
+			return;
+		
+		$tpl = DevblocksPlatform::getTemplateService();
+		
+		// Widget
+		
+		$tpl->assign('widget', $widget);
+		
+		// Timezones
+		
+		$date = DevblocksPlatform::getDateService();
+		
+		$timezones = $date->getTimezones();
+		$tpl->assign('timezones', $timezones);
+		
+		// Template
+		
+		$tpl->display('devblocks:cerberusweb.core::internal/workspaces/widgets/clock/config.tpl');
+	}
+	
+	function saveConfig(Model_WorkspaceWidget $widget) {
+		@$params = DevblocksPlatform::importGPC($_REQUEST['params'], 'array', array());
+		
+		DAO_WorkspaceWidget::update($widget->id, array(
+			DAO_WorkspaceWidget::PARAMS_JSON => json_encode($params),
+		));
+	}
+	
+	// Export
+	
+	function exportData(Model_WorkspaceWidget $widget, $format=null) {
+		switch(strtolower($format)) {
+			case 'csv':
+				return $this->_exportDataAsCsv($widget);
+				break;
+				
+			default:
+			case 'json':
+				return $this->_exportDataAsJson($widget);
+				break;
+		}
+		
+		return false;
+	}
+	
+	private function _exportDataAsCsv(Model_WorkspaceWidget $widget) {
+		@$timezone = $widget->params['timezone'];
+		$datetimezone = new DateTimeZone($timezone);
+		$datetime = new DateTime('now', $datetimezone);
+		
+		$results = array(
+			'Label' => $widget->label,
+			'Timezone' => $widget->params['timezone'],
+			'Timestamp' => $datetime->getTimestamp(),
+			'Output' => $datetime->format('r'),
+		);
+
+		$fp = fopen("php://temp", 'r+');
+		
+		fputcsv($fp, array_keys($results));
+		fputcsv($fp, array_values($results));
+		
+		rewind($fp);
+
+		$output = "";
+		
+		while(!feof($fp)) {
+			$output .= fgets($fp);
+		}
+		
+		fclose($fp);
+		
+		return $output;
+	}
+	
+	private function _exportDataAsJson(Model_WorkspaceWidget $widget) {
+		@$timezone = $widget->params['timezone'];
+		$datetimezone = new DateTimeZone($timezone);
+		$datetime = new DateTime('now', $datetimezone);
+		
+		$results = array(
+			'widget' => array(
+				'label' => $widget->label,
+				'type' => 'clock',
+				'version' => 'Cerb ' . APP_VERSION,
+				'time' => array(
+					'timezone' => $widget->params['timezone'],
+					'timestamp' => $datetime->getTimestamp(),
+					'output' => $datetime->format('r'),
+				),
+			),
+		);
+		
+		return DevblocksPlatform::strFormatJson(json_encode($results));
+	}
+};
+
 class WorkspaceWidget_Counter extends Extension_WorkspaceWidget implements ICerbWorkspaceWidget_ExportData {
 	private function _loadData(Model_WorkspaceWidget &$widget) {
 		@$datasource_extid = $widget->params['datasource'];
