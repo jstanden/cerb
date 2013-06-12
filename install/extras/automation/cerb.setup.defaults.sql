7630cbf411e546013aef0898733ba50765df2cbb
Jeff Standen <jeff@webgroupmedia.com>
2013-06-11 20:55:58 -0700

    * [Platform/Automation] Updated the SQL automation scripts for 6.4 in the install/extras/automation/ directory.  These allow the creation of a default database without having to run the installer.

--- install/extras/automation/cerb.setup.defaults.sql
+++ install/extras/automation/cerb.setup.defaults.sql
@@ -1,103 +1,103 @@
-# Create the default reply-to address
-INSERT INTO address (id,email) VALUES (1,'do-not-reply@localhost');
-INSERT INTO address_outgoing (address_id,is_default,reply_personal) VALUES (1,1,'');
+--
+-- Dumping data for table `address`
+--
 
-# Set the helpdesk title
-#INSERT INTO devblocks_setting (plugin_id, setting, value) VALUES ('cerberusweb.core','helpdesk_title','');
+INSERT INTO `address` VALUES (1,'do-not-reply@localhost','','',0,0,0,0,0,0,1371005090);
+INSERT INTO `address` VALUES (2,'superuser@localhost','','',0,0,0,0,0,0,1371005108);
 
-# Configure SMTP
-INSERT INTO devblocks_setting (plugin_id, setting, value) VALUES ('cerberusweb.core','smtp_host','localhost');
-INSERT INTO devblocks_setting (plugin_id, setting, value) VALUES ('cerberusweb.core','smtp_port','25');
-INSERT INTO devblocks_setting (plugin_id, setting, value) VALUES ('cerberusweb.core','smtp_enc','None'); #TLS/SSL/None
-## SMTP Authentication
-INSERT INTO devblocks_setting (plugin_id, setting, value) VALUES ('cerberusweb.core','smtp_auth_enabled','0'); #0/1
-#INSERT INTO devblocks_setting (plugin_id, setting, value) VALUES ('cerberusweb.core','smtp_auth_user','');
-#INSERT INTO devblocks_setting (plugin_id, setting, value) VALUES ('cerberusweb.core','smtp_auth_pass','');
+--
+-- Dumping data for table `address_outgoing`
+--
 
-# Configure POP3/IMAP (pop3:110,pop3-ssl:995,imap:143,imap-ssl:993)
-#INSERT INTO pop3_account (id, enabled, nickname, protocol, host, username, password, port) VALUES (1, 1, 'Dropbox', 'pop3', 'mail.example.com', 'user', 'password', 110);
+INSERT INTO `address_outgoing` VALUES (1,1,'','');
 
-# Default Role
-INSERT INTO worker_role (id, name, params_json) VALUES (1, 'Default', '{"who":"all","what":"all"}');
+--
+-- Dumping data for table `address_to_worker`
+--
 
-# Default Groups
-INSERT INTO worker_group (id, name, is_default) VALUES (1, 'Support', 1);
-INSERT INTO worker_group (id, name, is_default) VALUES (2, 'Sales', 0);
+INSERT INTO `address_to_worker` VALUES ('superuser@localhost',1,1,'',0);
 
-# Default Buckets
-#INSERT INTO bucket (id, group_id, name, is_assignable) VALUES (1, 1, 'Spam', 1);
-#INSERT INTO bucket (id, group_id, name, is_assignable) VALUES (2, 2, 'Spam', 1);
-#INSERT INTO bucket (id, group_id, name, is_assignable) VALUES (3, 3, 'Spam', 1);
+--
+-- Dumping data for table `cerb_property_store`
+--
 
-# Add Superuser
-INSERT INTO worker (id, title, email, pass, is_superuser, first_name, last_name, auth_extension_id) VALUES (1, 'Administrator', 'superuser@localhost', MD5('password'), 1, 'Super', 'User', 'login.password');
-INSERT INTO address (id,email) VALUES(2,'superuser@localhost');
-INSERT INTO address_to_worker (address, worker_id, is_confirmed) VALUES ('superuser@localhost', 1, 1);
+INSERT INTO `cerb_property_store` VALUES ('cron.heartbeat','enabled','1');
+INSERT INTO `cerb_property_store` VALUES ('cron.heartbeat','duration','5');
+INSERT INTO `cerb_property_store` VALUES ('cron.heartbeat','term','m');
+INSERT INTO `cerb_property_store` VALUES ('cron.heartbeat','lastrun','1370847600');
+INSERT INTO `cerb_property_store` VALUES ('cron.import','enabled','');
+INSERT INTO `cerb_property_store` VALUES ('cron.import','duration','0');
+INSERT INTO `cerb_property_store` VALUES ('cron.import','term','m');
+INSERT INTO `cerb_property_store` VALUES ('cron.import','lastrun','1370847600');
+INSERT INTO `cerb_property_store` VALUES ('cron.storage','enabled','1');
+INSERT INTO `cerb_property_store` VALUES ('cron.storage','duration','1');
+INSERT INTO `cerb_property_store` VALUES ('cron.storage','term','h');
+INSERT INTO `cerb_property_store` VALUES ('cron.storage','lastrun','1370927700');
+INSERT INTO `cerb_property_store` VALUES ('cron.search','enabled','1');
+INSERT INTO `cerb_property_store` VALUES ('cron.search','duration','10');
+INSERT INTO `cerb_property_store` VALUES ('cron.search','term','m');
+INSERT INTO `cerb_property_store` VALUES ('cron.search','lastrun','1370927700');
+INSERT INTO `cerb_property_store` VALUES ('cron.mail_queue','enabled','1');
+INSERT INTO `cerb_property_store` VALUES ('cron.mail_queue','duration','1');
+INSERT INTO `cerb_property_store` VALUES ('cron.mail_queue','term','m');
+INSERT INTO `cerb_property_store` VALUES ('cron.mail_queue','lastrun','1370927700');
+INSERT INTO `cerb_property_store` VALUES ('cron.virtual_attendant.scheduled_behavior','enabled','1');
+INSERT INTO `cerb_property_store` VALUES ('cron.virtual_attendant.scheduled_behavior','duration','1');
+INSERT INTO `cerb_property_store` VALUES ('cron.virtual_attendant.scheduled_behavior','term','m');
+INSERT INTO `cerb_property_store` VALUES ('cron.virtual_attendant.scheduled_behavior','lastrun','1370930400');
+INSERT INTO `cerb_property_store` VALUES ('cron.pop3','enabled','1');
+INSERT INTO `cerb_property_store` VALUES ('cron.pop3','duration','5');
+INSERT INTO `cerb_property_store` VALUES ('cron.pop3','term','m');
+INSERT INTO `cerb_property_store` VALUES ('cron.pop3','lastrun','1370934000');
+INSERT INTO `cerb_property_store` VALUES ('cron.parser','enabled','1');
+INSERT INTO `cerb_property_store` VALUES ('cron.parser','duration','1');
+INSERT INTO `cerb_property_store` VALUES ('cron.parser','term','m');
+INSERT INTO `cerb_property_store` VALUES ('cron.parser','lastrun','1370934000');
+INSERT INTO `cerb_property_store` VALUES ('cron.maint','enabled','1');
+INSERT INTO `cerb_property_store` VALUES ('cron.maint','duration','24');
+INSERT INTO `cerb_property_store` VALUES ('cron.maint','term','h');
+INSERT INTO `cerb_property_store` VALUES ('cron.maint','lastrun','1370847600');
 
-## Preferences
-INSERT INTO worker_pref (worker_id, setting, value) VALUES (1, 'timezone', 'America/Los_Angeles');
-INSERT INTO worker_pref (worker_id, setting, value) VALUES (1, 'locale', 'en_US');
+--
+-- Dumping data for table `contact_org`
+--
 
-## Memberships
-INSERT INTO worker_to_group (worker_id, group_id, is_manager) VALUES (1, 1, 1); # Support manager
-INSERT INTO worker_to_group (worker_id, group_id, is_manager) VALUES (1, 2, 1); # Sales manager
+INSERT INTO `contact_org` VALUES (1, 'Webgroup Media, LLC.', 'PO BOX 1206', 'Brea', 'California', '92822', 'USA', '+1 714-671-9090', 'http://www.cerberusweb.com/', UNIX_TIMESTAMP());
 
-# Scheduler defaults
+--
+-- Dumping data for table `devblocks_setting`
+--
 
-## cron.pop3
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.pop3', 'enabled', '1');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.pop3', 'duration', '5');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.pop3', 'term', 'm');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.pop3', 'lastrun', '0');
-## cron.parser
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.parser', 'enabled', '1');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.parser', 'duration', '1');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.parser', 'term', 'm');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.parser', 'lastrun', '0');
-## cron.maint
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.maint', 'enabled', '1');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.maint', 'duration', '24');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.maint', 'term', 'h');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.maint', 'lastrun', '0');
-## cron.heartbeat
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.heartbeat', 'enabled', '1');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.heartbeat', 'duration', '5');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.heartbeat', 'term', 'm');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.heartbeat', 'lastrun', '0');
-## cron.storage
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.storage', 'enabled', '1');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.storage', 'duration', '1');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.storage', 'term', 'h');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.stoage', 'lastrun', '0');
-## cron.search
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.search', 'enabled', '1');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.search', 'duration', '15');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.search', 'term', 'm');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.search', 'lastrun', '0');
-## cron.mail_queue
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.mail_queue', 'enabled', '1');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.mail_queue', 'duration', '1');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.mail_queue', 'term', 'm');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.mail_queue', 'lastrun', '0');
-## cron.virtual_attendant.scheduled_behavior
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.virtual_attendant.scheduled_behavior', 'enabled', '1');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.virtual_attendant.scheduled_behavior', 'duration', '1');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.virtual_attendant.scheduled_behavior', 'term', 'm');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.virtual_attendant.scheduled_behavior', 'lastrun', '0');
-## cron.calendar_recurring
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.calendar_recurring', 'enabled', '1');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.calendar_recurring', 'duration', '1');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.calendar_recurring', 'term', 'd');
-REPLACE INTO cerb_property_store (extension_id, property, value) VALUES ('cron.calendar_recurring', 'lastrun', '0');
+INSERT INTO `devblocks_setting` VALUES ('cerberusweb.core','helpdesk_title','Cerb6 - a fast and flexible web-based platform for business collaboration and automation.');
+INSERT INTO `devblocks_setting` VALUES ('cerberusweb.core','smtp_host','localhost');
+INSERT INTO `devblocks_setting` VALUES ('cerberusweb.core','smtp_port','25');
+INSERT INTO `devblocks_setting` VALUES ('cerberusweb.core','smtp_auth_enabled','0');
+INSERT INTO `devblocks_setting` VALUES ('cerberusweb.core','smtp_enc','None');
 
-# Default Organization
-INSERT INTO contact_org (id, name, street, city, province, postal, country, phone, website, created) VALUES (1, 'WebGroup Media, LLC.', 'PO BOX 1206', 'Brea', 'California', '92822', 'USA', '+1 714-671-9090', 'http://www.cerberusweb.com/', UNIX_TIMESTAMP());
+--
+-- Dumping data for table `worker`
+--
 
-# Custom Fields
-# [TODO]
+INSERT INTO `worker` VALUES (1,'Super','User','Administrator','superuser@localhost',MD5('password'),1,NULL,NULL,0,0,'login.password');
 
-# Virtual Attendants
-# [TODO]
+--
+-- Dumping data for table `worker_group`
+--
 
-# Support Center portal
-# [TODO]
+INSERT INTO `worker_group` VALUES (1,'Dispatch',NULL,1,0,'');
+INSERT INTO `worker_group` VALUES (2,'Support',NULL,0,0,'');
+INSERT INTO `worker_group` VALUES (3,'Sales',NULL,0,0,'');
+
+--
+-- Dumping data for table `worker_role`
+--
+
+INSERT INTO `worker_role` VALUES (1,'Default','{\"who\":\"all\",\"what\":\"all\"}');
+
+--
+-- Dumping data for table `worker_to_group`
+--
+
+INSERT INTO `worker_to_group` VALUES (1,1,1);
+INSERT INTO `worker_to_group` VALUES (1,2,1);
+INSERT INTO `worker_to_group` VALUES (1,3,1);
