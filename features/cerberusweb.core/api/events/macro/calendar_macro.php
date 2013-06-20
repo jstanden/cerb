ff1143468095c08fb837dafe9bc50a0f47c5a998
Jeff Standen <jeff@webgroupmedia.com>
2013-06-20 15:06:00 -0700

    * [Calendars/Virtual Attendants] Added Virtual Attendant macros for Calendar records.  This allows the creation of behaviors that are aware of calendar-wide automatically generated events (e.g. worklist, recurring).

---
+++ features/cerberusweb.core/api/events/macro/calendar_macro.php
@@ -0,0 +1,41 @@
+<?php
+/***********************************************************************
+| Cerb(tm) developed by Webgroup Media, LLC.
+|-----------------------------------------------------------------------
+| All source code & content (c) Copyright 2013, Webgroup Media LLC
+|   unless specifically noted otherwise.
+|
+| This source code is released under the Devblocks Public License.
+| The latest version of this license can be found here:
+| http://cerberusweb.com/license
+|
+| By using this software, you acknowledge having read this license
+| and agree to be bound thereby.
+| ______________________________________________________________________
+|	http://www.cerberusweb.com	  http://www.webgroupmedia.com/
+***********************************************************************/
+
+class Event_CalendarMacro extends AbstractEvent_Calendar {
+	const ID = 'event.macro.calendar';
+	
+	function __construct($manifest) {
+		parent::__construct($manifest);
+		$this->_event_id = self::ID;
+	}
+	
+	static function trigger($trigger_id, $calendar_id, $variables=array()) {
+		$events = DevblocksPlatform::getEventService();
+		$events->trigger(
+			new Model_DevblocksEvent(
+				self::ID,
+				array(
+					'calendar_id' => $calendar_id,
+					'_variables' => $variables,
+					'_whisper' => array(
+						'_trigger_id' => array($trigger_id),
+					),
+				)
+			)
+		);
+	}
+};
