b553b05739f08f3a313f419d03c4e36dd9e10742
Jeff Standen <jeff@webgroupmedia.com>
2014-01-27 17:26:32 -0800

    * [Mail/HTML/Usability] The upgrade script for 6.6 now automatically creates a default HTML template and associates it with the default reply-to address.  This allows HTML replies to "just work" without any manual configuration.

--- features/cerberusweb.core/patches/6.x/6.6.0.php
+++ features/cerberusweb.core/patches/6.x/6.6.0.php
@@ -80,6 +80,15 @@ if(!isset($tables['mail_html_template'])) {
 	$db->Execute($sql);
 
 	$tables['mail_html_template'] = 'mail_html_template';
+
+	// Insert a default HTML template
+	$db->Execute(sprintf("INSERT INTO mail_html_template (name, updated_at, owner_context, owner_context_id, content) VALUES (%s, %d, %s, %d, %s)",
+		$db->qstr('Default'),
+		time(),
+		$db->qstr('cerberusweb.contexts.app'),
+		0,
+		$db->qstr("<div id=\"body\">\n{{message_body}}\n</div>\n\n<style type=\"text/css\">\n#body {\n  font-family: Arial, Verdana, sans-serif;\n  font-size: 10pt;\n}\n\na { \n  color: black;\n}\n\nblockquote {\n  color: rgb(0, 128, 255);\n  font-style: italic;\n  margin-left: 0px;\n  border-left: 1px solid rgb(0, 128, 255);\n  padding-left: 5px;\n}\n\nblockquote a {\n  color: rgb(0, 128, 255);\n}\n</style>")
+	));
 }
 
 // ===========================================================================
@@ -122,6 +131,11 @@ list($columns, $indexes) = $db->metaTable('address_outgoing');
 
 if(!isset($columns['reply_html_template_id'])) {
 	$db->Execute("ALTER TABLE address_outgoing ADD COLUMN reply_html_template_id INT UNSIGNED NOT NULL DEFAULT 0");
+	
+	// Add the default HTML template to the default reply-to addy
+	if(false != ($default_html_template_id = $db->GetOne("SELECT id FROM mail_html_template WHERE name = 'Default'"))) {
+		$db->Execute(sprintf("UPDATE address_outgoing SET reply_html_template_id = %d WHERE is_default = 1", $default_html_template_id));
+	}
 }
 
 // ===========================================================================
