39c292563490821b0de9d2a7c54d62c4ed56b10d
Jeff Standen <jeff@webgroupmedia.com>
2014-01-30 02:14:05 -0800

    [Code Cleanup] Updated copyright headers to 2014

--- features/cerberusweb.core/api/uri/config/acl.php
+++ features/cerberusweb.core/api/uri/config/acl.php
@@ -2,7 +2,7 @@
 /***********************************************************************
 | Cerb(tm) developed by Webgroup Media, LLC.
 |-----------------------------------------------------------------------
-| All source code & content (c) Copyright 2013, Webgroup Media LLC
+| All source code & content (c) Copyright 2002-2014, Webgroup Media LLC
 |   unless specifically noted otherwise.
 |
 | This source code is released under the Devblocks Public License.
--- libs/devblocks/resources/js/jquery/_development/jquery.devblocksCharts.js
+++ libs/devblocks/resources/js/jquery/_development/jquery.devblocksCharts.js
@@ -1,7 +1,7 @@
 /***********************************************************************
 | Devblocks(tm) developed by Webgroup Media, LLC.
 |-----------------------------------------------------------------------
-| All source code & content (c) Copyright 2013, Webgroup Media LLC
+| All source code & content (c) Copyright 2002-2014, Webgroup Media LLC
 |   unless specifically noted otherwise.
 |
 | This source code is released under the Devblocks Public License.
