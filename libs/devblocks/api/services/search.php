95e393ff47fe916b85928af7ae9b7a42ed296eeb
Jeff Standen <jeff@webgroupmedia.com>
2014-11-11 16:57:26 -0800

    * [Search/Usability] Fixed a PHP notice when performing a full-text search without a default 'max results' value.

--- libs/devblocks/api/services/search.php
+++ libs/devblocks/api/services/search.php
@@ -458,7 +458,7 @@ class DevblocksSearchEngineMysqlFulltext extends Extension_DevblocksSearchEngine
 		}
 
 		// The max desired results (blank for unlimited)
-		$max_results = intval($this->_config['max_results']);
+		$max_results = isset($this->_config['max_results']) ? intval($this->_config['max_results']) : 0;
 
 		// Our temp table name is consistently named because we may keep it around for the duration of the request
 		$temp_table = sprintf("_search_%s", sha1($ns.$query));
