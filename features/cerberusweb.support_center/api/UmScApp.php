8921b5bdc0d14073b8b3e242e0694c019bb8d811
Jeff Standen <jeff@webgroupmedia.com>
2014-12-11 22:43:54 -0800

    * [CHD-3984] [Support Center/Security] A banned email address can no longer be used to recover an account login in the Support Center.

--- features/cerberusweb.support_center/api/UmScApp.php
+++ features/cerberusweb.support_center/api/UmScApp.php
@@ -612,6 +612,9 @@ class UmScLoginAuthenticator extends Extension_ScLoginAuthenticator {
 				throw new Exception("The email address you provided is not registered.");
 			}
 			
+			if($address->is_banned)
+				throw new Exception("The email address you provided is not available.");
+			
 			if(empty($address->contact_person_id) || null == ($contact = DAO_ContactPerson::get($address->contact_person_id))) {
 				throw new Exception("The email address you provided is not registered.");
 			}
@@ -926,6 +929,9 @@ class ScOpenIDLoginAuthenticator extends Extension_ScLoginAuthenticator {
 			if(null == ($address = DAO_Address::lookupAddress($email, false))) {
 				throw new Exception("The email address you provided is not registered.");
 			}
+			
+			if($address->is_banned)
+				throw new Exception("The email address you provided is not available.");
 			
 			if(empty($address->contact_person_id) || null == ($contact = DAO_ContactPerson::get($address->contact_person_id))) {
 				throw new Exception("The email address you provided is not registered.");
