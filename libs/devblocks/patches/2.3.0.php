13ff97ca1422af04a3ed835626cb01d8c541fd1e
Jeff Standen <jeff@webgroupmedia.com>
2014-12-19 13:52:03 -0800

    * [Translations/Localization] Modified how the database stores translation text to prevent a distinction between 'NULL' and blank translation strings.  This makes it possible to cleanly filter a translation worklist with "Default Translation is blank; My Translation is blank" in order to create 'to do' list for translation work.  Previously, some text was considered blank, and others NULL, which resulted in inconsistrent filtering.

---
+++ libs/devblocks/patches/2.3.0.php
@@ -0,0 +1,20 @@
+<?php
+$db = DevblocksPlatform::getDatabaseService();
+$tables = $db->metaTables();
+$prefix = (APP_DB_PREFIX != '') ? APP_DB_PREFIX.'_' : '';
+
+// ===========================================================================
+// Convert the translation strings to 'text'
+
+list($columns, $indexes) = $db->metaTable('translation');
+
+if(!isset($columns['string_default']) || !isset($columns['string_override']))
+	return FALSE;
+
+if(strtolower($columns['string_default']['type']) == 'longtext')
+	$db->Execute("ALTER TABLE translation MODIFY COLUMN string_default TEXT NOT NULL");
+
+if(strtolower($columns['string_override']['type']) == 'longtext')
+	$db->Execute("ALTER TABLE translation MODIFY COLUMN string_override TEXT NOT NULL");
+
+return TRUE;
