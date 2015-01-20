98e075efed6ac8653e526e734b8ae7ab6919b70d
Jeff Standen <jeff@webgroupmedia.com>
2015-01-20 10:19:27 -0800

    6.8.8 release

--- api/Application.class.php
+++ api/Application.class.php
@@ -46,7 +46,7 @@
  \* - Jeff Standen, Darren Sugita, Dan Hildebrandt
  *	 Webgroup Media LLC - Developers of Cerb
  */
-define("APP_BUILD", 2015010401);
+define("APP_BUILD", 2015012001);
 define("APP_VERSION", '6.8.8');
 
 define("APP_MAIL_PATH", APP_STORAGE_PATH . '/mail/');
