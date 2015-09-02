9fe46f5e8c564a6d8913cae3da112c880bdd9282
Jeff Standen <jeff@webgroupmedia.com>
2015-09-01 19:57:27 -0700

    * [Reports/Workers] Improved the 'Average Response Time' worker report. Previously, the report attempted to calculate response times itself. Now it simply uses the 'response time' field that we already track and display on conversations (since 6.1 in Aug 2012).

--- features/cerberusweb.reports/api/reports/worker/WorkerAverageResponseTimeReport.php
+++ features/cerberusweb.reports/api/reports/worker/WorkerAverageResponseTimeReport.php
@@ -47,73 +47,66 @@ class ChReportAverageResponseTime extends Extension_Report {
 		}
 		
 		// reload variables in template
+		
 		$tpl->assign('start', $start);
 		$tpl->assign('end', $end);
 		
 		// set up necessary reference arrays
-	   	$groups = DAO_Group::getAll();
+		
+		$groups = DAO_Group::getAll();
 		$tpl->assign('groups', $groups);
+		
 		$group_buckets = DAO_Bucket::getGroups();
 		$tpl->assign('group_buckets', $group_buckets);
-	   	$workers = DAO_Worker::getAll();
-	   	$tpl->assign('workers',$workers);
 		
-	   	// pull data from db
-	   	$sql = sprintf("SELECT mm.id, mm.ticket_id, mm.created_date, mm.worker_id, mm.is_outgoing, t.group_id, t.bucket_id ".
+		$workers = DAO_Worker::getAll();
+		$tpl->assign('workers',$workers);
+		
+		// pull data from db
+		$sql = sprintf("SELECT m.id, m.ticket_id, m.created_date, m.worker_id, m.response_time, t.group_id, t.bucket_id ".
 			"FROM message m ".
-	   		"INNER JOIN ticket t ON (t.id=m.ticket_id) ".
-	   		"INNER JOIN message mm ON (mm.ticket_id=t.id) ".
-			"WHERE m.created_date > %d AND m.created_date <= %d AND m.is_outgoing = 1 ".
-	   		"ORDER BY ticket_id,id ",
+			"INNER JOIN ticket t ON (t.id=m.ticket_id) ".
+			"WHERE m.created_date > %d AND m.created_date <= %d AND m.is_outgoing = 1 AND m.is_broadcast = 0 AND m.response_time > 0 ",
 			$start_time,
 			$end_time
 		);
 		$rs = $db->ExecuteSlave($sql);
 		
 		// process and count results
-	   	$group_responses = array();
-	   	$worker_responses = array();
-	   	$prev = array();
+		$group_responses = array();
+		$worker_responses = array();
+		
 		while($row = mysqli_fetch_assoc($rs)) {
 			// load current data
 			$id = intval($row['id']);
 			$ticket_id = intval($row['ticket_id']);
 			$created_date = intval($row['created_date']);
 			$worker_id = intval($row['worker_id']);
-			$is_outgoing = intval($row['is_outgoing']);
+			$response_time = intval($row['response_time']);
 			$group_id = intval($row['group_id']);
 			$bucket_id = intval($row['bucket_id']);
 
+			if(empty($worker_id))
+				continue;
+			
 			if(!empty($worker_id) && !isset($workers[$worker_id]))
 				continue;
 
 			if(!empty($group_id) && !isset($groups[$group_id]))
 				continue;
 				
-			// we only add data if it's a worker reply to the same ticket as $prev
-			if ($is_outgoing==1 && !empty($prev) && $ticket_id==$prev['ticket_id']) {
-				// Initialize, if necessary
-				if (!isset($group_responses[$group_id])) $group_responses[$group_id] = array();
-				if (!isset($worker_responses[$worker_id])) $worker_responses[$worker_id] = array();
-				
-				// log reply and time
-				@$group_responses[$group_id]['replies'] += 1;
-				@$group_responses[$group_id]['time'] += $created_date - $prev['created_date'];
-				@$worker_responses[$worker_id]['replies'] += 1;
-				@$worker_responses[$worker_id]['time'] += $created_date - $prev['created_date'];
-			}
+			if(!isset($group_responses[$group_id]))
+				$group_responses[$group_id] = array();
 			
-			// Save this one as "previous" and move on
-			$prev = array(
-				'id'=>$id,
-				'ticket_id'=>$ticket_id,
-				'created_date'=>$created_date,
-				'worker_id'=>$worker_id,
-				'is_outgoing'=>$is_outgoing,
-				'group_id'=>$group_id,
-				'bucket_id'=>$bucket_id,
-			);
+			if(!isset($worker_responses[$worker_id]))
+				$worker_responses[$worker_id] = array();
+			
+			@$group_responses[$group_id]['replies'] += 1;
+			@$group_responses[$group_id]['time'] += $response_time;
+			@$worker_responses[$worker_id]['replies'] += 1;
+			@$worker_responses[$worker_id]['time'] += $response_time;
 		}
+		
 		$tpl->assign('group_responses', $group_responses);
 		$tpl->assign('worker_responses', $worker_responses);
 
