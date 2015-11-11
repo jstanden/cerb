81090892e66d0b617a7b17eff79ac6f4c2c44bbb
Jeff Standen <jeff@webgroupmedia.com>
2015-11-10 22:32:31 -0800

    * [Virtual Attendants/Variables] Fixed an issue in Virtual Attendants where the 'Remove these objects from the variable' option of the 'Set variable' action didn't properly remove the given objects from the variable.

--- libs/devblocks/api/services/event/event_helper.php
+++ libs/devblocks/api/services/event/event_helper.php
@@ -4494,6 +4494,16 @@ class DevblocksEventHelper {
 				break;
 		}
 		
+		// Remove any existing IDs in the token that aren't in the $new_ids (subtract/replace)
+		
+		@$array =& $dict->$token;
+		
+		if(is_array($array))
+		foreach($array as $id => $object) {
+			if(!in_array($id, $new_ids))
+				unset($array[$id]);
+		}
+		
 		$objects = array();
 		
 		// Preload these from DAO
