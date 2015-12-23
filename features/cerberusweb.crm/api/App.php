b2bd1d23534c8911d234ef531cf2964a3fc78c9b
Jeff Standen <jeff@webgroupmedia.com>
2015-12-23 00:14:57 -0800

    * [Virtual Attendants/Opportunities] Added a 'Create opportunity' action to Virtual Attendant behaviors. This allows opportunity records to be created automatically, along with custom fields and record links, and the new record can be appended to a variable to run additional behaviors on it.

--- features/cerberusweb.crm/api/App.php
+++ features/cerberusweb.crm/api/App.php
@@ -558,3 +558,176 @@ class CrmTicketOppTab extends Extension_ContextProfileTab {
 	}
 };
 endif;
+
+if(class_exists('Extension_DevblocksEventAction')):
+class VaAction_CreateOpportunity extends Extension_DevblocksEventAction {
+	function render(Extension_DevblocksEvent $event, Model_TriggerEvent $trigger, $params=array(), $seq=null) {
+		$tpl = DevblocksPlatform::getTemplateService();
+		$tpl->assign('params', $params);
+		
+		if(!is_null($seq))
+			$tpl->assign('namePrefix', 'action'.$seq);
+		
+		$event = $trigger->getEvent();
+		$values_to_contexts = $event->getValuesContexts($trigger);
+		$tpl->assign('values_to_contexts', $values_to_contexts);
+		
+		// Custom fields
+		DevblocksEventHelper::renderActionCreateRecordSetCustomFields(CerberusContexts::CONTEXT_OPPORTUNITY, $tpl);
+		
+		// Template
+		$tpl->display('devblocks:cerberusweb.crm::events/action_create_opportunity.tpl');
+	}
+	
+	function simulate($token, Model_TriggerEvent $trigger, $params, DevblocksDictionaryDelegate $dict) {
+		$tpl_builder = DevblocksPlatform::getTemplateBuilder();
+
+		$out = null;
+		
+		@$name = $tpl_builder->build($params['name'], $dict);
+		@$email = $tpl_builder->build($params['email'], $dict);
+		@$status = $params['status'];
+		@$amount = floatval($tpl_builder->build($params['amount'], $dict));
+		
+		@$notify_worker_ids = DevblocksPlatform::importVar($params['notify_worker_id'],'array',array());
+		$notify_worker_ids = DevblocksEventHelper::mergeWorkerVars($notify_worker_ids, $dict);
+		
+		if(empty($name))
+			return "[ERROR] Name is required.";
+		
+		if(empty($email))
+			return "[ERROR] Email is required.";
+		
+		if(false == ($email_model = DAO_Address::lookupAddress($email, true)))
+			return "[ERROR] The email address is invalid.";
+		
+		if(!in_array($status, array('open', 'closed_won', 'closed_lost')))
+			return "[ERROR] Status has an invalid value.";
+		
+		if(!is_float($amount))
+			return "[ERROR] Amount should be a numeric value.";
+		
+		$comment = $tpl_builder->build($params['comment'], $dict);
+		
+		$out = sprintf(">>> Creating opportunity: %s\nEmail: %s\nStatus: %s\nAmount: %0.2f\n",
+			$name,
+			$email,
+			$status,
+			$amount
+		);
+		
+		// Custom fields
+		$out .= DevblocksEventHelper::simulateActionCreateRecordSetCustomFields($params, $dict);
+		
+		$out .= "\n";
+		
+		// Comment content
+		if(!empty($comment)) {
+			$out .= sprintf(">>> Writing comment on opportunity\n\n".
+				"%s\n\n",
+				$comment
+			);
+			
+			if(!empty($notify_worker_ids) && is_array($notify_worker_ids)) {
+				$out .= ">>> Notifying\n";
+				foreach($notify_worker_ids as $worker_id) {
+					if(null != ($worker = DAO_Worker::get($worker_id))) {
+						$out .= ' * ' . $worker->getName() . "\n";
+					}
+				}
+				$out .= "\n";
+			}
+		}
+		
+		// Links
+		$out .= DevblocksEventHelper::simulateActionCreateRecordSetLinks($params, $dict);
+		
+		// Set object variable
+		$out .= DevblocksEventHelper::simulateActionCreateRecordSetVariable($params, $dict);
+		
+		// Run in simulator
+		@$run_in_simulator = !empty($params['run_in_simulator']);
+		if($run_in_simulator) {
+			$this->run($token, $trigger, $params, $dict);
+		}
+		
+		return $out;
+	}
+	
+	function run($token, Model_TriggerEvent $trigger, $params, DevblocksDictionaryDelegate $dict) {
+		$tpl_builder = DevblocksPlatform::getTemplateBuilder();
+		
+		@$name = $tpl_builder->build($params['name'], $dict);
+		@$email = $tpl_builder->build($params['email'], $dict);
+		@$status = $params['status'];
+		@$amount = floatval($tpl_builder->build($params['amount'], $dict));
+
+		@$notify_worker_ids = DevblocksPlatform::importVar($params['notify_worker_id'],'array',array());
+		$notify_worker_ids = DevblocksEventHelper::mergeWorkerVars($notify_worker_ids, $dict);
+		
+		$comment = $tpl_builder->build($params['comment'], $dict);
+		
+		if(empty($name))
+			return;
+		
+		if(empty($email))
+			return;
+		
+		if(false == ($email_model = DAO_Address::lookupAddress($email, true)))
+			return;
+		
+		if(!in_array($status, array('open', 'closed_won', 'closed_lost')))
+			return ;
+		
+		if(!is_float($amount))
+			return;
+		
+		$fields = array(
+			DAO_CrmOpportunity::NAME => $name,
+			DAO_CrmOpportunity::PRIMARY_EMAIL_ID => $email_model->id,
+			DAO_CrmOpportunity::AMOUNT => sprintf("%0.2f", $amount),
+		);
+		
+		switch($status) {
+			case 'open':
+				$fields[DAO_CrmOpportunity::IS_CLOSED] = 0;
+				$fields[DAO_CrmOpportunity::IS_WON] = 0;
+				break;
+			case 'closed_won':
+				$fields[DAO_CrmOpportunity::IS_CLOSED] = 1;
+				$fields[DAO_CrmOpportunity::IS_WON] = 1;
+				break;
+			case 'closed_lost':
+				$fields[DAO_CrmOpportunity::IS_CLOSED] = 1;
+				$fields[DAO_CrmOpportunity::IS_WON] = 0;
+				break;
+		}
+			
+		if(false == ($opp_id = DAO_CrmOpportunity::create($fields)))
+			return;
+		
+		// Custom fields
+		DevblocksEventHelper::runActionCreateRecordSetCustomFields(CerberusContexts::CONTEXT_OPPORTUNITY, $opp_id, $params, $dict);
+		
+		// Comment content
+		if(!empty($comment)) {
+			$fields = array(
+				DAO_Comment::OWNER_CONTEXT => CerberusContexts::CONTEXT_VIRTUAL_ATTENDANT,
+				DAO_Comment::OWNER_CONTEXT_ID => $trigger->virtual_attendant_id,
+				DAO_Comment::COMMENT => $comment,
+				DAO_Comment::CONTEXT => CerberusContexts::CONTEXT_OPPORTUNITY,
+				DAO_Comment::CONTEXT_ID => $opp_id,
+				DAO_Comment::CREATED => time(),
+			);
+			DAO_Comment::create($fields, $notify_worker_ids);
+		}
+
+		// Connection
+		DevblocksEventHelper::runActionCreateRecordSetLinks(CerberusContexts::CONTEXT_OPPORTUNITY, $opp_id, $params, $dict);
+		
+		// Set object variable
+		DevblocksEventHelper::runActionCreateRecordSetVariable(CerberusContexts::CONTEXT_OPPORTUNITY, $opp_id, $params, $dict);
+	}
+	
+};
+endif;
---
+++ features/cerberusweb.crm/templates/events/action_create_opportunity.tpl
@@ -0,0 +1,80 @@
+<b>{'common.name'|devblocks_translate|capitalize}:</b>
+<div style="margin-left:10px;margin-bottom:10px;">
+	<input type="text" name="{$namePrefix}[name]" value="{$params.name}" class="placeholders" spellcheck="false" size="45" style="width:100%;" placeholder="">
+</div>
+
+<b>{'common.email'|devblocks_translate|capitalize}:</b>
+<div style="margin-left:10px;margin-bottom:10px;">
+	<input type="text" name="{$namePrefix}[email]" value="{$params.email}" class="placeholders" spellcheck="false" size="45" style="width:100%;" placeholder="user@example.com">
+</div>
+
+<b>{'common.status'|devblocks_translate|capitalize}:</b>
+<div style="margin-left:10px;margin-bottom:10px;">
+	<select name="{$namePrefix}[status]">
+		<option value="open" {if $params.status=="open"}selected="selected"{/if}> {'crm.opp.status.open'|devblocks_translate|capitalize}</option>
+		<option value="closed_won" {if $params.status=="closed_won"}selected="selected"{/if}> {'crm.opp.status.closed.won'|devblocks_translate|capitalize}</option>
+		<option value="closed_lost" {if $params.status=="closed_lost"}selected="selected"{/if}> {'crm.opp.status.closed.lost'|devblocks_translate|capitalize}</option>
+	</select>
+</div>
+
+<b>{'crm.opportunity.amount'|devblocks_translate|capitalize}:</b>
+<div style="margin-left:10px;margin-bottom:10px;">
+	<input type="text" name="{$namePrefix}[amount]" value="{$params.amount}" class="placeholders" spellcheck="false" size="45" style="width:100%;" placeholder="e.g. 1500.00">
+</div>
+
+{if !empty($custom_fields)}
+<fieldset class="peek">
+	<legend>{'common.custom_fields'|devblocks_translate|capitalize}:</legend>
+	{include file="devblocks:cerberusweb.core::internal/custom_fields/bulk/form.tpl" bulk=false field_wrapper="{$namePrefix}"}
+</fieldset>
+{/if}
+
+{include file="devblocks:cerberusweb.core::internal/decisions/actions/_shared_add_custom_fieldsets.tpl" context=CerberusContexts::CONTEXT_OPPORTUNITY field_wrapper="{$namePrefix}"}
+
+<b>{'common.comment'|devblocks_translate|capitalize}:</b>
+<div style="margin-left:10px;margin-bottom:0.5em;">
+	<textarea name="{$namePrefix}[comment]" cols="45" rows="5" style="width:100%;" class="placeholders">{$params.comment}</textarea>
+</div>
+
+<b>{'common.notify_workers'|devblocks_translate|capitalize}:</b>
+<div style="margin-left:10px;margin-bottom:0.5em;">
+	{include file="devblocks:cerberusweb.core::internal/decisions/actions/_shared_var_worker_picker.tpl" param_name="notify_worker_id" values_to_contexts=$values_to_contexts}
+</div>
+
+{if !empty($values_to_contexts)}
+<b>Link to:</b>
+<div style="margin-left:10px;margin-bottom:0.5em;">
+{include file="devblocks:cerberusweb.core::internal/decisions/actions/_shared_var_picker.tpl" param_name="link_to" values_to_contexts=$values_to_contexts}
+</div>
+{/if}
+
+<b>Also create records in simulator mode:</b>
+<div style="margin-left:10px;margin-bottom:10px;">
+	<label><input type="radio" name="{$namePrefix}[run_in_simulator]" value="1" {if $params.run_in_simulator}checked="checked"{/if}> {'common.yes'|devblocks_translate|capitalize}</label>
+	<label><input type="radio" name="{$namePrefix}[run_in_simulator]" value="0" {if !$params.run_in_simulator}checked="checked"{/if}> {'common.no'|devblocks_translate|capitalize}</label>
+</div>
+
+{* Check for object variables *}
+{capture name="object_vars"}
+{foreach from=$trigger->variables item=var key=var_key}
+{if $var.type == "ctx_{CerberusContexts::CONTEXT_OPPORTUNITY}"}
+<option value="{$var_key}" {if $params.object_var==$var_key}selected="selected"{/if}>{$var.label}</option>
+{/if}
+{/foreach}
+{/capture}
+
+{if $smarty.capture.object_vars}
+<b>Add object to list variable:</b><br>
+<div style="margin-left:10px;margin-bottom:10px;">
+	<select name="{$namePrefix}[object_var]">
+		<option value=""></option>
+		{$smarty.capture.object_vars nofilter}
+	</select>
+</div>
+{/if}
+
+{*
+<script type="text/javascript">
+var $action = $('fieldset#{$namePrefix}');
+</script>
+*}
