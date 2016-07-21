0289e40a2636adaeff670e8e40e0651c8147aea7
Jeff Standen <jeff@webgroupmedia.com>
2016-07-20 19:24:15 -0700

    [Platform/Devblocks/Schema] In the 'devblocks_setting' table, increased the length of the 'setting' field to 128 chars from 32.

--- libs/devblocks/patches/2.3.3.php
+++ libs/devblocks/patches/2.3.3.php
@@ -26,6 +26,29 @@ if(isset($tables[$table])) {
 }
 
 // ===========================================================================
+// Increase devblocks_setting 'setting' length
+
+if(!isset($tables['devblocks_setting'])) {
+	$logger->error("The 'devblocks_setting' table does not exist.");
+	return FALSE;
+}
+
+list($columns, $indexes) = $db->metaTable('devblocks_setting');
+
+$changes = array();
+
+if(isset($columns['plugin_id']) && 'varchar(128)' != $columns['plugin_id']['type'])
+	$changes[] = "change column plugin_id plugin_id varchar(128) not null default ''";
+
+if(isset($columns['setting']) && 'varchar(128)' != $columns['setting']['type'])
+	$changes[] = "change column setting setting varchar(128) not null default ''";
+
+if(!empty($changes)) {
+	$sql = sprintf("ALTER TABLE devblocks_setting %s", implode(', ', $changes));
+	$db->ExecuteMaster($sql) or die("[MySQL Error] " . $db->ErrorMsgMaster());
+}
+
+// ===========================================================================
 // Finish
 
 return TRUE;
