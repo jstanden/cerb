b6b7c15d00930e98a199400c44fcc9c9002d3b13
Jeff Standen <jeff@webgroupmedia.com>
2016-10-28 13:45:41 -0700

    * [CHD-4466] [Setup/Snippets] Added back the Setup->Configure->Snippets page for administrators. This allows the management of all snippets regardless of owner (including private worker-owned snippets).

---
+++ features/cerberusweb.core/api/uri/config/snippets.php
@@ -0,0 +1,35 @@
+<?php
+/***********************************************************************
+| Cerb(tm) developed by Webgroup Media, LLC.
+|-----------------------------------------------------------------------
+| All source code & content (c) Copyright 2002-2016, Webgroup Media LLC
+|   unless specifically noted otherwise.
+|
+| This source code is released under the Devblocks Public License.
+| The latest version of this license can be found here:
+| http://cerb.io/license
+|
+| By using this software, you acknowledge having read this license
+| and agree to be bound thereby.
+| ______________________________________________________________________
+|	http://cerb.io	    http://webgroup.media
+***********************************************************************/
+
+class PageSection_SetupSnippets extends Extension_PageSection {
+	function render() {
+		$tpl = DevblocksPlatform::getTemplateService();
+		$visit = CerberusApplication::getVisit();
+		
+		$visit->set(ChConfigurationPage::ID, 'snippets');
+		
+		// View
+		
+		$defaults = C4_AbstractViewModel::loadFromClass('View_Snippet');
+		$defaults->id = 'setup_snippets';
+		
+		$view = C4_AbstractViewLoader::getView($defaults->id, $defaults);
+		$tpl->assign('view', $view);
+		
+		$tpl->display('devblocks:cerberusweb.core::configuration/section/snippets/index.tpl');
+	}
+}
