149c8abfdbb4142fe9102bcca984132a4e1af2ae
Jeff Standen <jeff@webgroupmedia.com>
2016-12-08 12:25:14 -0800

    * [Worklists/Search] Added a 'group.id:' quick search filter to message worklists.

--- features/cerberusweb.core/api/dao/message.php
+++ features/cerberusweb.core/api/dao/message.php
@@ -1614,6 +1614,11 @@ class View_Message extends C4_AbstractView implements IAbstractView_Subtotals, I
 					'type' => DevblocksSearchCriteria::TYPE_TEXT,
 					'options' => array('param_key' => SearchFields_Message::ADDRESS_EMAIL, 'match' => DevblocksSearchCriteria::OPTION_TEXT_PREFIX),
 				),
+			'group.id' => 
+				array(
+					'type' => DevblocksSearchCriteria::TYPE_NUMBER,
+					'options' => array('param_key' => SearchFields_Message::TICKET_GROUP_ID),
+				),
 			'group' => 
 				array(
 					'type' => DevblocksSearchCriteria::TYPE_VIRTUAL,
