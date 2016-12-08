c1de5dc34503fc6e21e8e0b52412b31d578ad368
Jeff Standen <jeff@webgroupmedia.com>
2016-12-08 12:37:07 -0800

    * [CHD-3742] [API/Activity Log] Added the ability to search activity log entries from the REST API. The results should be filtered using quick search syntax. See: http://cerbweb.com/book/latest/developer_guide/rest_api/activity_log.html

---
+++ features/cerberusweb.restapi/api/rest/activity_log.php
@@ -0,0 +1,149 @@
+<?php
+class Rest_ActivityLog extends Extension_RestController implements IExtensionRestController {
+	function getAction($stack) {
+		$this->error(self::ERRNO_NOT_IMPLEMENTED);
+	}
+	
+	function putAction($stack) {
+		$this->error(self::ERRNO_NOT_IMPLEMENTED);
+	}
+	
+	function postAction($stack) {
+		@$action = array_shift($stack);
+		
+		switch($action) {
+			case 'search':
+				$this->postSearch();
+				break;
+		}
+		
+		$this->error(self::ERRNO_NOT_IMPLEMENTED);
+	}
+	
+	function deleteAction($stack) {
+		$this->error(self::ERRNO_NOT_IMPLEMENTED);
+	}
+	
+	function translateToken($token, $type='dao') {
+		if('custom_' == substr($token, 0, 7) && in_array($type, array('search', 'subtotal'))) {
+			return 'cf_' . intval(substr($token, 7));
+		}
+		
+		$tokens = array();
+		
+		if('dao'==$type) {
+			$tokens = array(
+				//'created' => DAO_ContactActivityLog::CREATED,
+			);
+			
+		} elseif ('subtotal'==$type) {
+			$tokens = array(
+				//'fieldsets' => SearchFields_ContextActivityLog::VIRTUAL_HAS_FIELDSET,
+			);
+			
+			$tokens_cfields = $this->_handleSearchTokensCustomFields(CerberusContexts::CONTEXT_ACTIVITY_LOG);
+			
+			if(is_array($tokens_cfields))
+				$tokens = array_merge($tokens, $tokens_cfields);
+			
+		} else {
+			$tokens = array(
+				'id' => SearchFields_ContextActivityLog::ID,
+			);
+		}
+		
+		if(isset($tokens[$token]))
+			return $tokens[$token];
+		
+		return NULL;
+	}
+	
+	function getContext($model) {
+		$labels = array();
+		$values = array();
+		$context = CerberusContexts::getContext(CerberusContexts::CONTEXT_ACTIVITY_LOG, $model, $labels, $values, null, true);
+
+		return $values;
+	}
+	
+	function search($filters=array(), $sortToken='email', $sortAsc=1, $page=1, $limit=10, $options=array()) {
+		@$query = DevblocksPlatform::importVar($options['query'], 'string', null);
+		@$show_results = DevblocksPlatform::importVar($options['show_results'], 'boolean', true);
+		@$subtotals = DevblocksPlatform::importVar($options['subtotals'], 'array', array());
+		
+		$params = array();
+		
+		// Sort
+		$sortBy = $this->translateToken($sortToken, 'search');
+		$sortAsc = !empty($sortAsc) ? true : false;
+
+		// Search
+		
+		$view = $this->_getSearchView(
+			CerberusContexts::CONTEXT_ACTIVITY_LOG,
+			$params,
+			$limit,
+			$page,
+			$sortBy,
+			$sortAsc
+		);
+		
+		if(!empty($query) && $view instanceof IAbstractView_QuickSearch)
+			$view->addParamsWithQuickSearch($query, true);
+		
+		// If we're given explicit filters, merge them in to our quick search
+		if(!empty($filters)) {
+			if(!empty($query))
+				$params = $view->getParams(false);
+
+			$custom_field_params = $this->_handleSearchBuildParamsCustomFields($filters, CerberusContexts::CONTEXT_ACTIVITY_LOG);
+			$new_params = $this->_handleSearchBuildParams($filters);
+			$params = array_merge($params, $new_params, $custom_field_params);
+			
+			$view->addParams($params, true);
+		}
+		
+		if($show_results)
+			list($results, $total) = $view->getData();
+		
+		// Get subtotal data, if provided
+		if(!empty($subtotals))
+			$subtotal_data = $this->_handleSearchSubtotals($view, $subtotals);
+		
+		if($show_results) {
+			$objects = array();
+			
+			$models = CerberusContexts::getModels(CerberusContexts::CONTEXT_ACTIVITY_LOG, array_keys($results));
+			
+			unset($results);
+			
+			foreach($models as $id => $model) {
+				$values = $this->getContext($model);
+				$objects[$id] = $values;
+			}
+		}
+		
+		$container = array();
+		
+		if($show_results) {
+			$container['results'] = $objects;
+			$container['total'] = $total;
+			$container['count'] = count($objects);
+			$container['page'] = $page;
+		}
+		
+		if(!empty($subtotals)) {
+			$container['subtotals'] = $subtotal_data;
+		}
+		
+		return $container;
+	}
+	
+	function postSearch() {
+		$worker = CerberusApplication::getActiveWorker();
+		
+		$container = $this->_handlePostSearch();
+		
+		$this->success($container);
+	}
+};
