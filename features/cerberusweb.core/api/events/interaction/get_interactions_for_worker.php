771a127a518db34c715efd1893a44ccda0d3dc03
Jeff Standen <jeff@webgroupmedia.com>
2017-05-25 13:57:22 -0700

    [Bots/Code Cleanup] Fixed an issue with avatar images in bot interaction menus.

--- features/cerberusweb.core/api/events/interaction/get_interactions_for_worker.php
+++ features/cerberusweb.core/api/events/interaction/get_interactions_for_worker.php
@@ -112,7 +112,7 @@ class Event_GetInteractionsForWorker extends Extension_DevblocksEvent {
 		foreach($bots as $bot) { /* @var $bot Model_Bot */
 			$bot_menu = new DevblocksMenuItemPlaceholder();
 			$bot_menu->label = $bot->name;
-			$bot_menu->image = $url_writer->write(sprintf('c=avatars&context=bot&context_id=%d{/devblocks_url}?v=%s', $bot->id, $bot->updated_at));
+			$bot_menu->image = $url_writer->write(sprintf('c=avatars&context=bot&context_id=%d', $bot->id)) . '?v=' . $bot->updated_at;
 			$bot_menu->children = [];
 			
 			$interactions_menu[$bot->id] = $bot_menu;
