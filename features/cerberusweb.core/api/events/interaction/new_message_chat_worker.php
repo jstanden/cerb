a733581062114de6df1441a43e19b40cb11f73dd
Jeff Standen <jeff@webgroupmedia.com>
2017-06-01 17:07:42 -0700

    [Bots] Added actions to conversational behaviors: create comment, notification, task, and ticket.

--- features/cerberusweb.core/api/events/interaction/new_message_chat_worker.php
+++ features/cerberusweb.core/api/events/interaction/new_message_chat_worker.php
@@ -262,6 +262,11 @@ class Event_NewMessageChatWorker extends Extension_DevblocksEvent {
 	function getActionExtensions(Model_TriggerEvent $trigger) {
 		$actions =
 			array(
+				'create_comment' => array('label' =>'Create comment'),
+				'create_notification' => array('label' =>'Create notification'),
+				'create_task' => array('label' =>'Create task'),
+				'create_ticket' => array('label' =>'Create ticket'),
+
 				'prompt_buttons' => array('label' => 'Prompt with buttons'),
 				'prompt_text' => array('label' => 'Prompt with text input'),
 				'prompt_wait' => array('label' => 'Prompt with wait'),
@@ -287,6 +292,22 @@ class Event_NewMessageChatWorker extends Extension_DevblocksEvent {
 		$tpl->assign('token_labels', $labels);
 			
 		switch($token) {
+			case 'create_comment':
+				DevblocksEventHelper::renderActionCreateComment($trigger);
+				break;
+				
+			case 'create_notification':
+				DevblocksEventHelper::renderActionCreateNotification($trigger);
+				break;
+				
+			case 'create_task':
+				DevblocksEventHelper::renderActionCreateTask($trigger);
+				break;
+				
+			case 'create_ticket':
+				DevblocksEventHelper::renderActionCreateTicket($trigger);
+				break;
+			
 			case 'prompt_buttons':
 				$tpl->display('devblocks:cerberusweb.core::events/pm/action_prompt_buttons.tpl');
 				break;
@@ -329,6 +350,22 @@ class Event_NewMessageChatWorker extends Extension_DevblocksEvent {
 	
 	function simulateActionExtension($token, $trigger, $params, DevblocksDictionaryDelegate $dict) {
 		switch($token) {
+			case 'create_comment':
+				return DevblocksEventHelper::simulateActionCreateComment($params, $dict, 'worker_id');
+				break;
+				
+			case 'create_notification':
+				return DevblocksEventHelper::simulateActionCreateNotification($params, $dict, 'worker_id');
+				break;
+				
+			case 'create_task':
+				return DevblocksEventHelper::simulateActionCreateTask($params, $dict, 'worker_id');
+				break;
+				
+			case 'create_ticket':
+				return DevblocksEventHelper::simulateActionCreateTicket($params, $dict, 'worker_id');
+				break;
+			
 			case 'prompt_buttons':
 				$tpl_builder = DevblocksPlatform::getTemplateBuilder();
 				$options = $tpl_builder->build($params['options'], $dict);
@@ -405,6 +442,22 @@ class Event_NewMessageChatWorker extends Extension_DevblocksEvent {
 	
 	function runActionExtension($token, $trigger, $params, DevblocksDictionaryDelegate $dict) {
 		switch($token) {
+			case 'create_comment':
+				DevblocksEventHelper::runActionCreateComment($params, $dict, 'worker_id');
+				break;
+				
+			case 'create_notification':
+				DevblocksEventHelper::runActionCreateNotification($params, $dict, 'worker_id');
+				break;
+				
+			case 'create_task':
+				DevblocksEventHelper::runActionCreateTask($params, $dict, 'worker_id');
+				break;
+
+			case 'create_ticket':
+				DevblocksEventHelper::runActionCreateTicket($params, $dict, 'worker_id');
+				break;
+			
 			case 'prompt_buttons':
 				$actions =& $dict->_actions;
 				
