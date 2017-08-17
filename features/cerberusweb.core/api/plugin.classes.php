4edf11ee59af5500c85e8680d9aaff83b06467fe
Jeff Standen <jeff@webgroupmedia.com>
2017-08-17 16:40:07 -0700

    * [Bots/Simulator/Usability] When simulating 'Execute HTTP Request' actions in bot behaviors, a reminder is shown if the action is configured to not run in the simulator. This helps preserve the sanity of bot developers.
    Fixes #355

--- features/cerberusweb.core/api/plugin.classes.php
+++ features/cerberusweb.core/api/plugin.classes.php
@@ -552,6 +552,8 @@ class VaAction_HttpRequest extends Extension_DevblocksEventAction {
 				if(isset($response['body']))
 					$out .= sprintf(">>> Body:\n%s\n", $response['body']);
 			}
+		} else {
+			$out .= ">>> NOTE: This HTTP request is not configured to run in the simulator.\n";
 		}
 		
 		return $out;
