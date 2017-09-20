eef5fa8194880c4a0b7f72a129f3825e3f4b759b
Jeff Standen <jeff@webgroupmedia.com>
2017-09-19 17:04:20 -0700

    * [Records/API] When creating workspace widget records, the 'params' field can be provided. This is a JSON-formatted key/value object.

--- features/cerberusweb.core/api/dao/workspace_widget.php
+++ features/cerberusweb.core/api/dao/workspace_widget.php
@@ -556,6 +556,27 @@ class Context_WorkspaceWidget extends Extension_DevblocksContext {
 		];
 	}
 	
+	function getDaoFieldsFromKeyAndValue($key, $value, &$out_fields, &$error) {
+		$dict_key = DevblocksPlatform::strLower($key);
+		switch($dict_key) {
+			case 'params':
+				if(!is_array($value)) {
+					$error = 'must be an object.';
+					return false;
+				}
+				
+				if(false == ($json = json_encode($value))) {
+					$error = 'could not be JSON encoded.';
+					return false;
+				}
+				
+				$out_fields[DAO_WorkspaceWidget::PARAMS_JSON] = $json;
+				break;
+		}
+		
+		return true;
+	}
+	
 	function lazyLoadContextValues($token, $dictionary) {
 		if(!isset($dictionary['id']))
 			return;
