3b71e544abde9755854084adb2fceddd12b5ffa4
Jeff Standen <jeff@webgroupmedia.com>
2017-09-19 17:01:29 -0700

    * [Records/API] When creating calendar records, the 'params' field can be provided. This is a JSON-formatted key/value object.

--- features/cerberusweb.core/api/dao/calendar.php
+++ features/cerberusweb.core/api/dao/calendar.php
@@ -1431,6 +1431,27 @@ class Context_Calendar extends Extension_DevblocksContext implements IDevblocksC
 		];
 	}
 	
+	function getDaoFieldsFromKeyAndValue($key, $value, &$out_fields, &$error) {
+		$dict_key = DevblocksPlatform::strLower($key);
+		switch($dict_key) {
+			case 'params':
+				if(!is_array($value)) {
+					$error = 'must be an object.';
+					return false;
+				}
+				
+				if(false == ($json = json_encode($value))) {
+					$error = 'could not be JSON encoded.';
+					return false;
+				}
+				
+				$out_fields[DAO_Calendar::PARAMS_JSON] = $json;
+				break;
+		}
+		
+		return true;
+	}
+	
 	function lazyLoadContextValues($token, $dictionary) {
 		if(!isset($dictionary['id']))
 			return;
