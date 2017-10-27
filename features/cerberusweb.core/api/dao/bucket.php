1d7bf775a35e97a79c85e0b4c29eca4cd754afe9
Jeff Standen <jeff@webgroupmedia.com>
2017-10-26 18:11:15 -0700

    [Code Cleanup] Changed the order of the default bucket worklist columns.

--- features/cerberusweb.core/api/dao/bucket.php
+++ features/cerberusweb.core/api/dao/bucket.php
@@ -1341,9 +1341,9 @@ class View_Bucket extends C4_AbstractView implements IAbstractView_Subtotals, IA
 		$this->renderSortAsc = true;
 
 		$this->view_columns = array(
-			SearchFields_Bucket::IS_DEFAULT,
 			SearchFields_Bucket::GROUP_ID,
 			SearchFields_Bucket::NAME,
+			SearchFields_Bucket::IS_DEFAULT,
 			SearchFields_Bucket::REPLY_ADDRESS_ID,
 			SearchFields_Bucket::REPLY_PERSONAL,
 			SearchFields_Bucket::REPLY_SIGNATURE_ID,
