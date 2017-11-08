c2a51719275ed087bdbcb65aef9493037250a19e
Jeff Standen <jeff@webgroupmedia.com>
2017-11-08 15:35:11 -0800

    * [Records/Validation] Fixed an issue with record when validating 'language' fields. Only enabled language packs were being permitted.

--- libs/devblocks/api/services/validation.php
+++ libs/devblocks/api/services/validation.php
@@ -302,10 +302,11 @@ class _DevblocksValidators {
 	
 	function language() {
 		return function($value, &$error) {
-			$languages = DAO_Translation::getDefinedLangCodes();
 			if(empty($value))
 				return true;
 			
+			$translate = DevblocksPlatform::getTranslationService();
+			$locales = $translate->getLocaleStrings();
 			
 			if(!$value || !isset($locales[$value])) {
 				$error = sprintf("(%s) is not a valid language. Format like: en_US",
