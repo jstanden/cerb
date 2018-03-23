ac853568c4a637512e10d6e9fd8aba9cf5687e63
Jeff Standen <jeff@webgroupmedia.com>
2018-03-23 12:52:45 -0700

    * [Bots/Scripting] In bot scripting the `xml_decode()` function now accepts an option third argument for `mode`. When set to `html`, an HTML document is converted into XML while ignoring the quirks of its markup. This allows HTML (like content_html) to be parsed using XPath.

--- libs/devblocks/api/services/template_builder.php
+++ libs/devblocks/api/services/template_builder.php
@@ -826,8 +826,18 @@ class _DevblocksTwigExtensions extends Twig_Extension {
 		return $xml->asXML();
 	}
 	
-	function function_xml_decode($str, $namespaces=array()) {
-		$xml = simplexml_load_string($str);
+	function function_xml_decode($str, $namespaces=[], $mode=null) {
+		switch(DevblocksPlatform::strLower($mode)) {
+			case 'html':
+				$doc = new DOMDocument();
+				$doc->loadHTML($str);
+				$xml = simplexml_import_dom($doc);
+				break;
+				
+			default:
+				$xml = simplexml_load_string($str);
+				break;
+		}
 		
 		if(!($xml instanceof SimpleXMLElement))
 			return false;
