cfcff9a6ade8c7a1ceb21aac8874853eb6547428
Jeff Standen <jeff@webgroupmedia.com>
2018-04-19 09:57:18 -0700

    * [Records/Validation] In record validation (create/update), the configured alias for a field is now used when returning errors about missing required values. This provides more useful errors in the API when the dictionary key and database schema differ on the naming of a field.

--- libs/devblocks/api/DAO.php
+++ libs/devblocks/api/DAO.php
@@ -87,13 +87,13 @@ abstract class DevblocksORMHelper {
 		if(is_array($valid_fields) && !$id)
 		foreach($valid_fields as $field_key => $field) {
 			if($field->_type->isRequired()) {
-				if(!$field->_type->canBeEmpty() && 0 == strlen($fields[$field_key])) {
-					$error = sprintf("'%s' is required.", $field_key);
+				if(!$field->_type->canBeEmpty() && 0 == strlen(@$fields[$field_key])) {
+					$error = sprintf("'%s' is required.", $field->_label);
 					return false;
 				}
 				
 				if(!isset($fields[$field_key])) {
-					$error = sprintf("'%s' is required.", $field_key);
+					$error = sprintf("'%s' is required.", $field->_label);
 					return false;
 				}
 			}
