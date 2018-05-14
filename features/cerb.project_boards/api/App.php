d77200d2773bb20fdaffcc0ba94983681ad85f30
Jeff Standen <jeff@webgroupmedia.com>
2018-05-14 15:58:57 -0700

    * [Profiles/Widgets/Projects] Added a 'Project Board' widget to profile dashboards. This displays any project board and is configurable with placeholders.

--- features/cerb.project_boards/api/App.php
+++ features/cerb.project_boards/api/App.php
@@ -1,5 +1,4 @@
 <?php
-if(class_exists('Extension_WorkspaceTab')):
 class WorkspaceTab_Board extends Extension_WorkspaceTab {
 	public function renderTab(Model_WorkspacePage $page, Model_WorkspaceTab $tab) {
 		$tpl = DevblocksPlatform::services()->template();
@@ -68,4 +67,79 @@ class WorkspaceTab_Board extends Extension_WorkspaceTab {
 		return true;
 	}
 }
-endif;
+
+class ProfileWidget_ProjectBoard extends Extension_ProfileWidget {
+	const ID = 'cerb.profile.tab.widget.project_board';
+
+	function __construct($manifest=null) {
+		parent::__construct($manifest);
+	}
+	
+	function render(Model_ProfileWidget $model, $context, $context_id) {
+		$tpl = DevblocksPlatform::services()->template();
+		$tpl_builder = DevblocksPlatform::services()->templateBuilder();
+		
+		@$target_context_id = $model->extension_params['context_id'];
+		
+		if(false == ($context_ext = Extension_DevblocksContext::get($context)))
+			return;
+		
+		$dao_class = $context_ext->getDaoClass();
+		
+		if(false == ($record = $dao_class::get($context_id)))
+			return;
+		
+		// Are we showing fields for a different record?
+		
+		if($target_context_id) {
+			$labels = $values = $merge_token_labels = $merge_token_values = [];
+			
+			CerberusContexts::getContext($context, $record, $merge_token_labels, $merge_token_values, null, true, true);
+			
+			CerberusContexts::merge(
+				'record_',
+				'Record:',
+				$merge_token_labels,
+				$merge_token_values,
+				$labels,
+				$values
+			);
+			
+			CerberusContexts::getContext(CerberusContexts::CONTEXT_PROFILE_WIDGET, $model, $merge_token_labels, $merge_token_values, null, true, true);
+			
+			CerberusContexts::merge(
+				'widget_',
+				'Widget:',
+				$merge_token_labels,
+				$merge_token_values,
+				$labels,
+				$values
+			);
+			
+			$values['widget__context'] = CerberusContexts::CONTEXT_PROFILE_WIDGET;
+			$values['widget_id'] = $model->id;
+			$dict = DevblocksDictionaryDelegate::instance($values);
+			
+			$context_id = $tpl_builder->build($target_context_id, $dict);
+			
+			if(false == ($board = DAO_ProjectBoard::get($context_id))) {
+				$tpl->display('devblocks:cerb.project_boards::boards/board/empty_tip.tpl');
+				return;
+			}
+		}
+		
+		$tpl->assign('board', $board);
+		
+		$contexts = Extension_DevblocksContext::getAll(false, 'links');
+		$tpl->assign('contexts', $contexts);
+		
+		$tpl->display('devblocks:cerb.project_boards::boards/board/board.tpl');
+	}
+	
+	function renderConfig(Model_ProfileWidget $model) {
+		$tpl = DevblocksPlatform::services()->template();
+		$tpl->assign('widget', $model);
+		
+		$tpl->display('devblocks:cerb.project_boards::widgets/project_board/config.tpl');
+	}
+}
