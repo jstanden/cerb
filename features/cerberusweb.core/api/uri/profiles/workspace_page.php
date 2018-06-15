b9f6a5d7365cc5d7a2b55f5a0b491a3b7df1b135
Jeff Standen <jeff@webgroupmedia.com>
2018-06-15 10:44:00 -0700

    [Profiles] Added profile pages for workspace pages.

---
+++ features/cerberusweb.core/api/uri/profiles/workspace_page.php
@@ -0,0 +1,108 @@
+<?php
+/***********************************************************************
+| Cerb(tm) developed by Webgroup Media, LLC.
+|-----------------------------------------------------------------------
+| All source code & content (c) Copyright 2014, Webgroup Media LLC
+|   unless specifically noted otherwise.
+|
+| This source code is released under the Devblocks Public License.
+| The latest version of this license can be found here:
+| http://cerb.ai/license
+|
+| By using this software, you acknowledge having read this license
+| and agree to be bound thereby.
+| ______________________________________________________________________
+|	http://cerb.ai	    http://webgroup.media
+***********************************************************************/
+
+class PageSection_ProfilesWorkspacePage extends Extension_PageSection {
+	function render() {
+		$response = DevblocksPlatform::getHttpResponse();
+		$stack = $response->path;
+		@array_shift($stack); // profiles
+		@array_shift($stack); // workspace_page
+		@$context_id = intval(array_shift($stack)); // 123
+		
+		$context = CerberusContexts::CONTEXT_WORKSPACE_PAGE;
+		
+		$tabs = DAO_WorkspaceTab::getByPage($context_id);
+		
+		Page_Profiles::renderProfile($context, $context_id, $stack);
+	}
+	
+	function savePeekJsonAction() {
+	}
+	
+	function viewExploreAction() {
+		@$view_id = DevblocksPlatform::importGPC($_REQUEST['view_id'],'string');
+		
+		$active_worker = CerberusApplication::getActiveWorker();
+		$url_writer = DevblocksPlatform::services()->url();
+		
+		// Generate hash
+		$hash = md5($view_id.$active_worker->id.time());
+		
+		// Loop through view and get IDs
+		$view = C4_AbstractViewLoader::getView($view_id);
+		$view->setAutoPersist(false);
+
+		// Page start
+		@$explore_from = DevblocksPlatform::importGPC($_REQUEST['explore_from'],'integer',0);
+		if(empty($explore_from)) {
+			$orig_pos = 1+($view->renderPage * $view->renderLimit);
+		} else {
+			$orig_pos = 1;
+		}
+
+		$view->renderPage = 0;
+		$view->renderLimit = 250;
+		$pos = 0;
+		
+		do {
+			$models = [];
+			list($results, $total) = $view->getData();
+
+			// Summary row
+			if(0==$view->renderPage) {
+				$model = new Model_ExplorerSet();
+				$model->hash = $hash;
+				$model->pos = $pos++;
+				$model->params = array(
+					'title' => $view->name,
+					'created' => time(),
+//					'worker_id' => $active_worker->id,
+					'total' => $total,
+					'return_url' => isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : $url_writer->writeNoProxy('c=search&type=workspace_page', true),
+					'toolbar_extension_id' => 'cerberusweb.contexts.workspace.tab.explore.toolbar',
+				);
+				$models[] = $model;
+				
+				$view->renderTotal = false; // speed up subsequent pages
+			}
+			
+			if(is_array($results))
+			foreach($results as $opp_id => $row) {
+				if($opp_id==$explore_from)
+					$orig_pos = $pos;
+				
+				$url = $url_writer->writeNoProxy(sprintf("c=profiles&type=workspace_page&id=%d-%s", $row[SearchFields_WorkspacePage::ID], DevblocksPlatform::strToPermalink($row[SearchFields_WorkspacePage::NAME])), true);
+				
+				$model = new Model_ExplorerSet();
+				$model->hash = $hash;
+				$model->pos = $pos++;
+				$model->params = array(
+					'id' => $row[SearchFields_WorkspacePage::ID],
+					'url' => $url,
+				);
+				$models[] = $model;
+			}
+			
+			DAO_ExplorerSet::createFromModels($models);
+			
+			$view->renderPage++;
+			
+		} while(!empty($results));
+		
+		DevblocksPlatform::redirect(new DevblocksHttpResponse(array('explore',$hash,$orig_pos)));
+	}
+};
