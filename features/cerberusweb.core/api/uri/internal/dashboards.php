b0d187d38a6047d8bc326a723bd4df1053671586
Jeff Standen <jeff@webgroupmedia.com>
2018-07-05 12:47:26 -0700

    * [Workspaces/Dashboards] Added 'Behavior Tree' widgets to workspace dashboards. This allows the decision trees of multiple behaviors to be displayed and edited on the same page.

--- features/cerberusweb.core/api/uri/internal/dashboards.php
+++ features/cerberusweb.core/api/uri/internal/dashboards.php
@@ -508,10 +508,83 @@ class WorkspaceWidget_Gauge extends Extension_WorkspaceWidget implements ICerbWo
 	}
 };
 
-class WorkspaceWidget_BotBehavior extends Extension_WorkspaceWidget {
+class WorkspaceWidget_BehaviorTree extends Extension_WorkspaceWidget {
+	const ID = 'cerb.workspace.widget.behavior.tree';
+	
+	function render(Model_WorkspaceWidget $widget) {
+		$tpl = DevblocksPlatform::services()->template();
+		$tpl_builder = DevblocksPlatform::services()->templateBuilder();
+		
+		$active_worker = CerberusApplication::getActiveWorker();
+		
+		@$behavior_id_template = $widget->params['behavior'];
+		
+		$labels = $values = $merge_token_labels = $merge_token_values = [];
+		
+		CerberusContexts::getContext(CerberusContexts::CONTEXT_WORKER, $active_worker, $merge_token_labels, $merge_token_values, null, true, true);
+		
+		CerberusContexts::merge(
+			'current_worker_',
+			'Current Worker:',
+			$merge_token_labels,
+			$merge_token_values,
+			$labels,
+			$values
+		);
+		
+		CerberusContexts::getContext(CerberusContexts::CONTEXT_WORKSPACE_WIDGET, $widget, $merge_token_labels, $merge_token_values, null, true, true);
+		
+		CerberusContexts::merge(
+			'widget_',
+			'Widget:',
+			$merge_token_labels,
+			$merge_token_values,
+			$labels,
+			$values
+		);
+		
+		$dict = DevblocksDictionaryDelegate::instance($values);
+		
+		$behavior_id = $tpl_builder->build($behavior_id_template, $dict);
+		
+		if(false == ($behavior = DAO_TriggerEvent::get($behavior_id)))
+			return;
+		
+		if(false == ($event = $behavior->getEvent()))
+			return;
+		
+		if(false == ($bot = $behavior->getBot()))
+			return;
+		
+		$tpl->assign('behavior', $behavior);
+		$tpl->assign('event', $event->manifest);
+		
+		$tpl->display('devblocks:cerberusweb.core::internal/bot/behavior/tab.tpl');
+	}
+	
+	function renderConfig(Model_WorkspaceWidget $widget) {
+		if(empty($widget))
+			return;
+		
+		$tpl = DevblocksPlatform::services()->template();
+		
+		$tpl->assign('widget', $widget);
+		$tpl->display('devblocks:cerberusweb.core::internal/workspaces/widgets/behavior_tree/config.tpl');
+	}
+	
+	function saveConfig(Model_WorkspaceWidget $widget) {
+		@$params = DevblocksPlatform::importGPC($_REQUEST['params'], 'array', []);
+		
+		DAO_WorkspaceWidget::update($widget->id, array(
+			DAO_WorkspaceWidget::PARAMS_JSON => json_encode($params),
+		));
+	}
+};
+
 	function render(Model_WorkspaceWidget $widget) {
 		$tpl = DevblocksPlatform::services()->template();
 
+class WorkspaceWidget_BotBehavior extends Extension_WorkspaceWidget {
 		@$behavior_id = $widget->params['behavior_id'];
 		@$behavior_vars = DevblocksPlatform::importVar(@$widget->params['behavior_vars'], 'array', []);
 		
