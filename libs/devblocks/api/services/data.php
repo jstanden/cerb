5500a9472461822a696c67e23e147c1df9b38812
Jeff Standen <jeff@webgroupmedia.com>
2018-07-26 12:49:54 -0700

    * [Data Queries/Usage/Snippets] Added a `usage.snippets` type to data queries. This returns historical usage data for snippets (e.g. uses by worker over time). It currently supports the following `format:` options: `table` (default) returns tabular data (columns and rows) suitable for the "Chart: Table" visualization widget; `timeseries` returns series-based data suitable for the "Chart: Time Series" visualization widget (the x-axis values are timestamps).

--- libs/devblocks/api/services/data.php
+++ libs/devblocks/api/services/data.php
@@ -1501,6 +1501,192 @@ class _DevblocksDataProviderUsageBotBehaviors extends _DevblocksDataProvider {
 		return $output;
 	}
 }
+
+class _DevblocksDataProviderUsageSnippets extends _DevblocksDataProvider {
+	function getData($query, $chart_fields, array $options=[]) {
+		$format = 'table';
+		
+		foreach($chart_fields as $field) {
+			if(!($field instanceof DevblocksSearchCriteria))
+				continue;
+			
+			if($field->key == 'format') {
+				CerbQuickSearchLexer::getOperStringFromTokens($field->tokens, $oper, $format);
+			}
+		}
+		
+		switch($format) {
+			case 'timeseries':
+				return $this->_formatDataAsTimeSeries($chart_fields);
+				break;
+				
+			case 'table':
+			default:
+				return $this->_formatDataAsTable($chart_fields);
+				break;
+		}
+	}
+	
+	private function _formatDataAsTable(array $chart_fields=[]) {
+		$db = DevblocksPlatform::services()->database();
+		
+		$chart_model = [
+			'type' => 'usage.snippets',
+			'format' => 'table',
+		];
+		
+		// Filter: Start + End
+		
+		$start = 'first day of this month -1 year';
+		@$start_time = strtotime($start);
+		$start_time -= $start_time % 86400;
+		
+		$end = 'now';
+		@$end_time = strtotime($end);
+		$end_time -= $end_time % 86400;
+
+		// Filter: Limit
+		$limit = 0;
+		
+		// Filter: Workers
+		
+		$filter_worker_ids = [];
+		$filter_worker_ids = DevblocksPlatform::sanitizeArray($filter_worker_ids, 'integer', ['unique','nonzero']);
+		
+		$workers = DAO_Worker::getAll();
+		
+		$sql = sprintf("SELECT snippet.id AS snippet_id, snippet.title AS snippet_title, SUM(snippet_use_history.uses) AS snippet_uses ".
+			"FROM snippet_use_history ".
+			"INNER JOIN snippet ON (snippet_use_history.snippet_id=snippet.id) ".
+			"WHERE snippet_use_history.ts_day BETWEEN %d AND %d ".
+			"%s ".
+			"GROUP BY snippet.id, snippet.title ".
+			"ORDER BY snippet_uses %%s ".
+			"%s",
+			$start_time,
+			$end_time,
+			(!empty($filter_worker_ids) && is_array($filter_worker_ids) ? sprintf("AND snippet_use_history.worker_id IN (%s)", implode(',', $filter_worker_ids)) : ''),
+			(!empty($limit) ? sprintf("LIMIT %d", $limit) : '')
+		);
+		
+		if(true) {
+			$results = $db->GetArraySlave(sprintf($sql, 'DESC'));
+		} else {
+			$results = $db->GetArraySlave(sprintf($sql, 'ASC'));
+		}
+		
+		return [
+			'data' => [
+				'columns' => [
+					'snippet_title' => [
+						'label' => 'Name',
+						'type' => 'context',
+						'type_options' => [
+							'context' => CerberusContexts::CONTEXT_SNIPPET,
+							'context_id_key' => 'snippet_id',
+						]
+					],
+					'snippet_uses' => [
+						'label' => '# Uses',
+						'type' => Model_CustomField::TYPE_NUMBER,
+					]
+				],
+				'rows' => $results,
+			],
+			'_' => [
+				'type' => 'usage.snippets',
+				'format' => 'table'
+			]
+		];
+	}
+	
+	private function _formatDataAsTimeSeries(array $chart_fields=[]) {
+		$db = DevblocksPlatform::services()->database();
+		
+		$chart_model = [
+			'type' => 'usage.snippets',
+			'format' => 'timeseries',
+			'ids' => [],
+		];
+		
+		foreach($chart_fields as $field) {
+			if(!($field instanceof DevblocksSearchCriteria))
+				continue;
+			
+			if($field->key == 'ids') {
+				CerbQuickSearchLexer::getOperArrayFromTokens($field->tokens, $oper, $ids);
+				$chart_model['ids'] = $ids;
+			}
+		}
+		
+		// Default IDs
+		
+		if(!$chart_model['ids']) {
+			$sql = sprintf("SELECT snippet_id ".
+				"FROM snippet_use_history ".
+				"WHERE ts_day BETWEEN %d AND %d ".
+				"GROUP BY snippet_id ".
+				"ORDER BY COUNT(*) DESC ".
+				"LIMIT 10",
+				strtotime('first day of this month -1 year 00:00:00'),
+				strtotime('today 00:00:00')
+			);
+			$results = $db->GetArraySlave($sql);
+			$chart_model['ids'] = array_column($results, 'snippet_id');
+		}
+		
+		$sql = sprintf("SELECT SUM(uses) AS metric, snippet_id, DATE_FORMAT(FROM_UNIXTIME(ts_day),'%%Y-%%m') AS label ".
+			"FROM snippet_use_history ".
+			"WHERE ts_day BETWEEN %d AND %d ".
+			"%s".
+			"GROUP BY label, snippet_id ".
+			"ORDER BY label ",
+			strtotime('first day of this month -1 year 00:00:00'),
+			strtotime('today 00:00:00'),
+			$chart_model['ids'] 
+				? sprintf("AND snippet_id IN (%s) ", implode(',', $chart_model['ids']))
+				: ''
+		);
+		
+		$results = $db->GetArraySlave($sql);
+		
+		$output = [
+			'data' => [
+				'ts' => [],
+			],
+			'_' => [
+				'type' => 'usage.snippets',
+				'format' => 'timeseries'
+			]
+		];
+		
+		$x_values = array_values(array_unique(array_column($results, 'label')));
+		
+		$output['data']['ts'] = $x_values;
+		
+		$series = [];
+		
+		foreach($results as $result) {
+			$series_key = $result['snippet_id'];
+			
+			if(!array_key_exists($series_key, $series)) {
+				$series[$series_key] = array_fill_keys($x_values, 0);
+			}
+			
+			$series[$series_key][$result['label']] = floatval($result['metric']);
+		}
+		
+		$snippets = DAO_Snippet::getIds(array_keys($series));
+		
+		foreach($series as $snippet_id => $y_values) {
+			$snippet_name = @$snippets[$snippet_id]->title ?: '(no name)';
+			$output['data'][$snippet_name] = array_values($y_values);
+		}
+		
+		return $output;
+	}
+}
+
 class _DevblocksDataService {
 	static $instance = null;
 	
@@ -1538,6 +1724,11 @@ class _DevblocksDataService {
 		switch($chart_type) {
 			case 'usage.behaviors':
 				$provider = new _DevblocksDataProviderUsageBotBehaviors();
+				$results = $provider->getData($query, $chart_fields);
+				break;
+				
+			case 'usage.snippets':
+				$provider = new _DevblocksDataProviderUsageSnippets();
 				$results = $provider->getData($query, $chart_fields);
 				break;
 				
