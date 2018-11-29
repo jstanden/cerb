68b976d462ff34755d9d3ecf445f43d9251efa35
Jeff Standen <jeff@webgroupmedia.com>
2018-11-29 11:45:51 -0800

    * [Connected Services/Providers] Implemented a 'Token Bearer' provider for connected services. This allows connected accounts to authenticate against any API that uses bearer tokens (e.g.HipChat).

---
+++ features/cerberusweb.core/api/service_providers/token.php
@@ -0,0 +1,81 @@
+<?php
+class ServiceProvider_TokenBearer extends Extension_ConnectedServiceProvider {
+	const ID = 'cerb.service.provider.token.bearer';
+	
+	function renderConfigForm(Model_ConnectedService $service) {
+		$tpl = DevblocksPlatform::services()->template();
+		$active_worker = CerberusApplication::getActiveWorker();
+		
+		$params = $service->decryptParams($active_worker);
+		$tpl->assign('params', $params);
+		
+		$tpl->display('devblocks:cerberusweb.core::internal/connected_service/providers/token_bearer/config_service.tpl');
+	}
+	
+	function saveConfigForm(Model_ConnectedService $service, array &$params, &$error=null) {
+		@$edit_params = DevblocksPlatform::importGPC($_POST['params'], 'array', []);
+		
+		$validation = DevblocksPlatform::services()->validation();
+	
+		$validation
+			->addField('token_name','Token Name')
+			->string()
+			->setRequired(true)
+			;
+		
+		if(false == $validation->validateAll($edit_params, $error))
+			return false;
+		
+		foreach($edit_params as $k => $v)
+			$params[$k] = $v;
+		
+		return true;
+	}
+	
+	public function renderAccountConfigForm(Model_ConnectedService $service, Model_ConnectedAccount $account) {
+		$tpl = DevblocksPlatform::services()->template();
+		$active_worker = CerberusApplication::getActiveWorker();
+		
+		$params = $account->decryptParams($active_worker);
+		$tpl->assign('params', $params);
+		
+		$tpl->display('devblocks:cerberusweb.core::internal/connected_service/providers/token_bearer/config_account.tpl');
+	}
+
+	public function saveAccountConfigForm(Model_ConnectedService $service, Model_ConnectedAccount $account, array &$params, &$error = null) {
+		@$edit_params = DevblocksPlatform::importGPC($_POST['params'], 'array', []);
+		
+		$validation = DevblocksPlatform::services()->validation();
+	
+		$validation
+			->addField('token','Token')
+			->string()
+			->setRequired(true)
+			;
+		
+		if(false == $validation->validateAll($edit_params, $error))
+			return false;
+		
+		foreach($edit_params as $k => $v)
+			$params[$k] = $v;
+		
+		return true;
+	}
+	
+	function authenticateHttpRequest(Model_ConnectedAccount $account, Psr\Http\Message\RequestInterface &$request, array &$options = []) : bool {
+		if(false == ($service = $account->getService()))
+			return false;
+		
+		$service_params = $service->decryptParams();
+		$account_params = $account->decryptParams();
+		
+		$request = $request
+			->withHeader('Authorization', sprintf("%s %s",
+				@$service_params['token_name'] ?: 'Bearer',
+				@$account_params['token'] ?: ''
+			))
+			;
+		
+		return true;
+	}
+}
---
+++ features/cerberusweb.core/templates/internal/connected_service/providers/token_bearer/config_service.tpl
@@ -0,0 +1,5 @@
+<fieldset class="peek black">
+	<b>Token Name:</b><br>
+	<input type="text" name="params[token_name]" value="{$params.token_name|default:'Bearer'}" size="50" style="width:100%;" placeholder="Bearer" spellcheck="false"><br>
+	<br>
+</fieldset>
