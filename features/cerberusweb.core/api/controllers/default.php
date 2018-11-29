25a5698e1dfaa0bd1f3529171aa351a96a43064c
Jeff Standen <jeff@webgroupmedia.com>
2018-11-29 11:12:48 -0800

    [Code Cleanup] Moved controllers from `plugin.classes.php` to `api/controllers/`.

---
+++ features/cerberusweb.core/api/controllers/default.php
@@ -0,0 +1,235 @@
+<?php 
+class Controller_Default extends DevblocksControllerExtension {
+	const ID = 'core.controller.page';
+	
+	// [TODO] We probably need a CerberusApplication scope for getting content that has ACL applied
+	private function _getAllowedPages() {
+		$active_worker = CerberusApplication::getActiveWorker();
+		$page_manifests = DevblocksPlatform::getExtensions('cerberusweb.page', false);
+
+		// [TODO] This may cause problems on other pages where an active worker isn't required
+		
+		// Check worker level ACL (if set by manifest)
+		foreach($page_manifests as $idx => $page_manifest) {
+			// If ACL policy defined
+			if(isset($page_manifest->params['acl'])) {
+				if($active_worker && !$active_worker->hasPriv($page_manifest->params['acl'])) {
+					unset($page_manifests[$idx]);
+				}
+			}
+		}
+		
+		return $page_manifests;
+	}
+	
+	public function handleRequest(DevblocksHttpRequest $request) {
+		$path = $request->path;
+		$controller = array_shift($path);
+
+		$page = null;
+		if(null != ($page_manifest = CerberusApplication::getPageManifestByUri($controller))) {
+			$page = $page_manifest->createInstance(); /* @var $page CerberusPageExtension */
+		}
+		
+		if(empty($page)) {
+			switch($controller) {
+				case "portal":
+					DevblocksPlatform::dieWithHttpError(null, 404);
+					break;
+					
+				default:
+					return; // default page
+					break;
+			}
+		}
+
+		@$action = DevblocksPlatform::strAlphaNum(array_shift($path), '\_') . 'Action';
+
+		switch($action) {
+			case NULL:
+				// [TODO] Index/page render
+				break;
+				
+			default:
+				// Default action, call arg as a method suffixed with Action
+				
+				if($page->isVisible()) {
+					if(method_exists($page,$action)) {
+						call_user_func(array($page, $action)); // [TODO] Pass HttpRequest as arg?
+					}
+				} else {
+					// if Ajax [TODO] percolate isAjax from platform to handleRequest
+					// DevblocksPlatform::dieWithHttpError("Access denied.  Session expired?", 403);
+				}
+
+				break;
+		}
+	}
+	
+	public function writeResponse(DevblocksHttpResponse $response) {
+		$path = $response->path;
+
+		$tpl = DevblocksPlatform::services()->template();
+		$session = DevblocksPlatform::services()->session();
+		$settings = DevblocksPlatform::services()->pluginSettings();
+		$translate = DevblocksPlatform::getTranslationService();
+		$active_worker = CerberusApplication::getActiveWorker();
+		
+		$visit = $session->getVisit();
+		$page_manifests = $this->_getAllowedPages();
+
+		$controller = array_shift($path);
+		
+		// Default page
+		if(empty($controller)) {
+			if(is_a($active_worker, 'Model_Worker')) {
+				$controller = 'pages';
+				$path = array('pages');
+				
+				// Find the worker's first page
+				
+				if(null != ($menu_json = DAO_WorkerPref::get($active_worker->id, 'menu_json', null))) {
+					@$menu = json_decode($menu_json);
+
+					if(is_array($menu) && !empty($menu)) {
+						$page_id = current($menu);
+						$path[] = $page_id;
+					}
+				}
+
+				$response = new DevblocksHttpResponse($path);
+				
+				DevblocksPlatform::setHttpResponse($response);
+			}
+		}
+		
+		// [JAS]: Require us to always be logged in for Cerberus pages
+		if(empty($visit) && 0 != strcasecmp($controller,'login')) {
+			$query = array();
+			// Must be a valid page controller
+			if(!empty($response->path)) {
+				if(is_array($response->path) && !empty($response->path) && CerberusApplication::getPageManifestByUri(current($response->path)))
+					$query = ['url'=> implode('/',$response->path)];
+			}
+			DevblocksPlatform::redirect(new DevblocksHttpRequest(array('login'),$query));
+		}
+		
+		$page = null;
+		if(null != ($page_manifest = CerberusApplication::getPageManifestByUri($controller))) {
+			@$page = $page_manifest->createInstance(); /* @var $page CerberusPageExtension */
+		}
+		
+		if(empty($page)) {
+			$tpl->assign('settings', $settings);
+			$tpl->assign('session', $_SESSION);
+			$tpl->assign('translate', $translate);
+			$tpl->assign('visit', $visit);
+			$message = $tpl->fetch('devblocks:cerberusweb.core::404.tpl');
+			
+			DevblocksPlatform::dieWithHttpError($message, 404);
+			return;
+		}
+		
+		// [JAS]: Listeners (Step-by-step guided tour, etc.)
+		$listenerManifests = DevblocksPlatform::getExtensions('devblocks.listener.http');
+		foreach($listenerManifests as $listenerManifest) { /* @var $listenerManifest DevblocksExtensionManifest */
+			$inst = $listenerManifest->createInstance(); /* @var $inst DevblocksHttpResponseListenerExtension */
+			$inst->run($response, $tpl);
+		}
+
+		$tpl->assign('active_worker', $active_worker);
+		$tour_enabled = false;
+		
+		if(!empty($visit) && !is_null($active_worker)) {
+			$tour_enabled = intval(DAO_WorkerPref::get($active_worker->id, 'assist_mode', 1));
+
+			$keyboard_shortcuts = intval(DAO_WorkerPref::get($active_worker->id,'keyboard_shortcuts',1));
+			$tpl->assign('pref_keyboard_shortcuts', $keyboard_shortcuts);
+			
+			$active_worker_memberships = $active_worker->getMemberships();
+			$tpl->assign('active_worker_memberships', $active_worker_memberships);
+		}
+		$tpl->assign('tour_enabled', $tour_enabled);
+		
+		// [JAS]: Variables provided to all page templates
+		$tpl->assign('settings', $settings);
+		$tpl->assign('session', $_SESSION);
+		$tpl->assign('translate', $translate);
+		$tpl->assign('visit', $visit);
+		
+		$tpl->assign('page_manifests',$page_manifests);
+		$tpl->assign('page',$page);
+
+		$tpl->assign('response_path', $response->path);
+		$tpl->assign('response_uri', implode('/', $response->path));
+		
+		// Prebody Renderers
+		$preBodyRenderers = DevblocksPlatform::getExtensions('cerberusweb.renderer.prebody', true);
+		if(!empty($preBodyRenderers))
+			$tpl->assign('prebody_renderers', $preBodyRenderers);
+
+		// Postbody Renderers
+		$postBodyRenderers = DevblocksPlatform::getExtensions('cerberusweb.renderer.postbody', true);
+		if(!empty($postBodyRenderers))
+			$tpl->assign('postbody_renderers', $postBodyRenderers);
+		
+		// Timings
+		$tpl->assign('render_time', (microtime(true) - DevblocksPlatform::getStartTime()));
+		if(function_exists('memory_get_usage') && function_exists('memory_get_peak_usage')) {
+			$tpl->assign('render_memory', memory_get_usage() - DevblocksPlatform::getStartMemory());
+			$tpl->assign('render_peak_memory', memory_get_peak_usage() - DevblocksPlatform::getStartPeakMemory());
+		}
+
+		// Contexts
+		
+		$search_favorites = [];
+		
+		if($active_worker) {
+			$search_favorites = DAO_WorkerPref::getAsJson($active_worker->id, 'search_favorites_json', '[]');
+			$search_favorites = array_flip($search_favorites);
+		}
+		
+		$contexts = Extension_DevblocksContext::getAll(false);
+		$search_menu = [];
+		
+		foreach($contexts as $context_id => $context) {
+			if($context->hasOption('search')) {
+				$label = $context->name;
+
+				if(false != ($aliases = Extension_DevblocksContext::getAliasesForContext($context)))
+					$label = @$aliases['plural'] ?: $aliases['singular'];
+				
+				$is_visible = !$search_favorites || isset($search_favorites[$context_id]);
+				
+				$search_menu[$context_id] = [
+					'context' => $context_id,
+					'label' => $label,
+					'visible' => $is_visible,
+				];
+			}
+		}
+		
+		DevblocksPlatform::sortObjects($search_menu, '[label]');
+		
+		$tpl->assign('search_menu', $search_menu);
+		
+		// Conversational interactions
+		$interactions = Event_GetInteractionsForWorker::getByPoint('global');
+		$tpl->assign('global_interactions_show', !empty($interactions));
+		
+		// Proactive interactions
+		if(!empty($active_worker)) {
+			$proactive_interactions_count = DAO_BotInteractionProactive::getCountByWorker($active_worker->id);
+			$tpl->assign('proactive_interactions_count', $proactive_interactions_count);
+		}
+		
+		// Template
+		$tpl->display('devblocks:cerberusweb.core::border.tpl');
+		
+		if(!empty($active_worker)) {
+			$unread_notifications = DAO_Notification::getUnreadCountByWorker($active_worker->id);
+			$tpl->assign('active_worker_notify_count', $unread_notifications);
+			$tpl->display('devblocks:cerberusweb.core::badge_notifications_script.tpl');
+		}
+	}
+};
