204795bb79e2263315a4642c88e036caf34c4f82
Jeff Standen <jeff@webgroupmedia.com>
2019-03-07 08:46:50 -0800

    [Roles] The role cache is cleared when worker records are edited. This updates any roles when queries use changing worker properties (e.g. memberships, mention name).

--- features/cerberusweb.core/api/dao/worker.php
+++ features/cerberusweb.core/api/dao/worker.php
@@ -250,6 +250,7 @@ class DAO_Worker extends Cerb_ORMHelper {
 	static function clearCache() {
 		$cache = DevblocksPlatform::services()->cache();
 		$cache->remove(self::CACHE_ALL);
+		DAO_WorkerRole::clearWorkerCache();
 	}
 	
 	/**
