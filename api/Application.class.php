af6696ef5bd2c08a0feca9fed57be9863a1c9deb
Jeff Standen <jeff@webgroupmedia.com>
2019-03-11 07:31:01 -0700

    9.2-checkpoint

--- api/Application.class.php
+++ api/Application.class.php
@@ -39,7 +39,7 @@
  * - Jeff Standen and Dan Hildebrandt
  *	 Founders at Webgroup Media LLC; Developers of Cerb
  */
-define("APP_BUILD", 2019030801);
+define("APP_BUILD", 2019031101);
 define("APP_VERSION", '9.2.0');
 
 define("APP_MAIL_PATH", APP_STORAGE_PATH . '/mail/');
