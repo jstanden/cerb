917304a5506dd390dc2f6ad9fe52a809599838f5
Jeff Standen <jeff@webgroupmedia.com>
2019-05-01 16:37:16 -0700

    * [Profiles/Widgets/Sheets] Implemented sheet widgets for profiles. This uses placeholders from the current record to run a data query to populate the sheet. The following column types are currently supported: `card`, `custom`, `date`, `link`, `search_button`, `slider`, and `text`.

---
+++ features/cerberusweb.core/api/profiles/widgets/sheet.php
@@ -0,0 +1,115 @@
+<?php
+class ProfileWidget_Sheet extends Extension_ProfileWidget {
+	function getData(Model_ProfileWidget $widget, $page=null, $context, $context_id, &$error=null) {
+		$data = DevblocksPlatform::services()->data();
+		$tpl_builder = DevblocksPlatform::services()->templateBuilder();
+		$active_worker= CerberusApplication::getActiveWorker();
+		
+		@$data_query = DevblocksPlatform::importGPC($widget->extension_params['data_query'], 'string', null);
+		@$cache_secs = DevblocksPlatform::importGPC($widget->extension_params['cache_secs'], 'integer', 0);
+		
+		if($page) {
+			$data_query .= sprintf(' page:%d', $page);
+		}
+		
+		$dict = DevblocksDictionaryDelegate::instance([
+			'current_worker__context' => CerberusContexts::CONTEXT_WORKER,
+			'current_worker_id' => $active_worker->id,
+			'widget__context' => CerberusContexts::CONTEXT_PROFILE_WIDGET,
+			'widget_id' => $widget->id,
+			'record__context' => $context,
+			'record_id' => $context_id,
+		]);
+		
+		$query = $tpl_builder->build($data_query, $dict);
+		
+		if(!$query) {
+			$error = "Invalid data query.";
+			return false;
+		}
+		
+		if(false === ($results = $data->executeQuery($query, $error, $cache_secs)))
+			return false;
+		
+		return $results;
+	}
+	
+	function render(Model_ProfileWidget $model, $context, $context_id) {
+		$tpl = DevblocksPlatform::services()->template();
+		
+		@$page = DevblocksPlatform::importGPC($_REQUEST['page'], 'integer', 0);
+		
+		$error = null;
+		
+		if(false == ($results = $this->getData($model, $page, $context, $context_id, $error))) {
+			echo DevblocksPlatform::strEscapeHtml($error);
+			return;
+		}
+		
+		if(empty($results)) {
+			echo "(no data)";
+			return;
+		}
+		
+		$format = DevblocksPlatform::strLower(@$results['_']['format']);
+		
+		if(!in_array($format, ['dictionaries'])) {
+			echo DevblocksPlatform::strEscapeHtml("The data should be in one of the following formats: dictionaries.");
+			return;
+		}
+		
+		switch($format) {
+			case 'dictionaries':
+				$sheets = DevblocksPlatform::services()->sheet();
+				
+				@$sheet_yaml = DevblocksPlatform::importGPC($model->extension_params['sheet_yaml'], 'string', null);
+				$sheet = $sheets->parseYaml($sheet_yaml, $error);
+				
+				$sheets->addType('card', $sheets->types()->card());
+				$sheets->addType('custom', $sheets->types()->custom());
+				$sheets->addType('date', $sheets->types()->date());
+				$sheets->addType('link', $sheets->types()->link());
+				$sheets->addType('search_button', $sheets->types()->searchButton());
+				$sheets->addType('slider', $sheets->types()->slider());
+				$sheets->addType('text', $sheets->types()->text());
+				$sheets->setDefaultType('text');
+				
+				$sheet_dicts = $results['data'];
+				
+				$rows = $sheets->getRows($sheet, $sheet_dicts);
+				$tpl->assign('rows', $rows);
+				
+				$columns = $sheet['columns'];
+				$tpl->assign('columns', $columns);
+				
+				$tpl->assign('show_headings', (!array_key_exists('show_headings', $sheet) || $sheet['show_headings']) ? true : false);
+				
+				@$paging = $results['_']['paging'];
+				
+				if($paging) {
+					$tpl->assign('paging', $paging);
+				}
+				
+				$tpl->assign('widget_ext', $this);
+				$tpl->assign('widget', $model);
+				$tpl->display('devblocks:cerberusweb.core::internal/profiles/widgets/sheet/render.tpl');
+				break;
+		}
+	}
+	
+	function renderConfig(Model_ProfileWidget $widget) {
+		$tpl = DevblocksPlatform::services()->template();
+		$tpl->assign('widget', $widget);
+		$tpl->display('devblocks:cerberusweb.core::internal/profiles/widgets/sheet/config.tpl');
+	}
+	
+	/*
+	function saveConfig(array $fields, $id, &$error=null) {
+		@$params = DevblocksPlatform::importGPC($_REQUEST['params'], 'array', []);
+		
+		DAO_ProfileWidget::update($id, array(
+			DAO_ProfileWidget::PARAMS_JSON => json_encode($params),
+		));
+	}
+	*/
+};
---
+++ features/cerberusweb.core/templates/internal/profiles/widgets/sheet/config.tpl
@@ -0,0 +1,33 @@
+<div id="widget{$widget->id}Config" style="margin-top:10px;">
+	<fieldset id="widget{$widget->id}QueryEditor" class="peek">
+		<legend>
+			Run this data query: 
+			{include file="devblocks:cerberusweb.core::help/docs_button.tpl" url="https://cerb.ai/docs/data-queries/"}
+		</legend>
+		
+		<textarea name="params[data_query]" class="cerb-code-editor" data-editor-mode="ace/mode/twig" class="placeholders" style="width:95%;height:50px;">{$widget->extension_params.data_query}</textarea>
+		
+		<div>
+			<b>Cache</b> query results for 
+			<input type="text" size="5" maxlength="6" name="params[cache_secs]" placeholder="e.g. 300" value="{$widget->extension_params.cache_secs}"> seconds
+		</div>
+	</fieldset>
+	
+	<fieldset id="widget{$widget->id}Columns" class="peek">
+		<legend>
+			Display these columns: <small>(YAML)</small>
+		</legend>
+		
+		<textarea name="params[sheet_yaml]" class="cerb-code-editor" data-editor-mode="ace/mode/yaml" style="width:95%;height:50px;">{$widget->extension_params.sheet_yaml}</textarea>
+	</fieldset>
+</div>
+
+<script type="text/javascript">
+$(function() {
+	var $config = $('#widget{$widget->id}Config');
+	
+	$config.find('textarea.cerb-code-editor')
+		.cerbCodeEditor()
+		;
+});
+</script>
