4c2497d5b9a65f33d3a1ab5a0e9b3943b32ab058
Jeff Standen <jeff@webgroupmedia.com>
2019-06-14 14:49:30 -0700

    * [Code Editors/UX] Fixed an issue with code editors on search and data queries. When typing `*` the current autocompletion suggestion would stop matching.

--- features/cerberusweb.core/resources/js/cerberus.js
+++ features/cerberusweb.core/resources/js/cerberus.js
@@ -1455,7 +1455,7 @@ var ajax = new cAjaxCalls();
 			});
 			
 			var completer = {
-				identifierRegexps: [/[a-zA-Z_0-9\#\@\.\$\-\u00A2-\uFFFF]/],
+				identifierRegexps: [/[a-zA-Z_0-9\*\#\@\.\$\-\u00A2-\uFFFF]/],
 				formatData: function(scope_key) {
 					return editor.completer.autocomplete_suggestions[scope_key].map(function(data) {
 						if('object' == typeof data) {
@@ -1805,7 +1805,7 @@ var ajax = new cAjaxCalls();
 			editor.completer.exactMatch = true;
 			
 			var completer = {
-				identifierRegexps: [/[a-zA-Z_0-9\#\@\.\$\-\u00A2-\uFFFF]/],
+				identifierRegexps: [/[a-zA-Z_0-9\*\#\@\.\$\-\u00A2-\uFFFF]/],
 				formatData: function(scope_key) {
 					if(!autocomplete_suggestions.hasOwnProperty(scope_key) 
 						|| undefined == autocomplete_suggestions.hasOwnProperty(scope_key))
