a527604edbdb5cd9137d4bd101bc1cb1249ca55f
Jeff Standen <jeff@webgroupmedia.com>
2019-12-10 12:04:04 -0800

    * [Records/Activity Log] In activity log dictionaries, the new `params` key is an array of key/value metadata associated with the log entry. This enables custom rendering and processing of log entries beyond what is displayed by default.

--- features/cerberusweb.core/api/dao/context_activity_log.php
+++ features/cerberusweb.core/api/dao/context_activity_log.php
@@ -1247,6 +1247,8 @@ class Context_ContextActivityLog extends Extension_DevblocksContext implements I
 			
 			$token_values['target__context'] = $entry->target_context;
 			$token_values['target_id'] = $entry->target_context_id;
+			
+			$token_values['params'] = json_decode($entry->entry_json, true);
 		}
 		
 		return true;
