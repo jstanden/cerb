073ddbf4425f7517fa30f0203839f6b74f748021
Jeff Standen <jeff@webgroupmedia.com>
2020-02-28 15:05:24 -0800

    [Code Cleanup] URI routing in the legacy printing plugin

--- plugins/cerb.legacy.print/api/App.php
+++ plugins/cerb.legacy.print/api/App.php
@@ -51,14 +51,17 @@ class Controller_Print extends DevblocksControllerExtension {
 			case 'ticket':
 				@$id = array_shift($stack);
 				@$ticket = is_numeric($id) ? DAO_Ticket::get($id) : DAO_Ticket::getTicketByMask($id);
+				
+				if(false == ($ticket))
+					DevblocksPlatform::dieWithHttpError(null, 404);
 
 				if(false == ($ticket->getGroup()))
-					break;
+					DevblocksPlatform::dieWithHttpError(null, 404);
 				
 				// Make sure we're allowed to view this ticket or message
 				if(!Context_Ticket::isReadableByActor($ticket, $active_worker)) {
 					echo "<H1>" . $translate->_('common.access_denied') . "</H1>";
-					return;
+					DevblocksPlatform::dieWithHttpError(null, 403);
 				}
 				
 				$convo_timeline = array();
@@ -106,16 +109,20 @@ class Controller_Print extends DevblocksControllerExtension {
 				
 			case 'message':
 				@$id = array_shift($stack);
-				@$message = DAO_Message::get($id);
-				@$ticket = DAO_Ticket::get($message->ticket_id);
+				
+				if(false == ($message = DAO_Message::get($id)))
+					DevblocksPlatform::dieWithHttpError(null, 404);
+				
+				if(false == ($ticket = DAO_Ticket::get($message->ticket_id)))
+					DevblocksPlatform::dieWithHttpError(null, 404);
 				
 				if(false == ($ticket->getGroup()))
-					break;
+					DevblocksPlatform::dieWithHttpError(null, 404);
 				
 				// Make sure we're allowed to view this ticket or message
 				if(!Context_Ticket::isReadableByActor($ticket, $active_worker)) {
 					echo "<H1>" . $translate->_('common.access_denied') . "</H1>";
-					return;
+					DevblocksPlatform::dieWithHttpError(null, 403);
 				}
 				
 				// Message Notes
