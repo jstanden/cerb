46c04799ea4ab8883642783ff5cc924f82ba1454
Jeff Standen <jeff@webgroupmedia.com>
2020-08-03 17:08:03 -0700

    [Docs] Fixed a bad link in the generated docs for profile widgets.

--- features/cerberusweb.core/api/dao/profile_widget.php
+++ features/cerberusweb.core/api/dao/profile_widget.php
@@ -1212,7 +1212,7 @@ class Context_ProfileWidget extends Extension_DevblocksContext implements IDevbl
 			'type' => 'object',
 		];
 		
-		$keys['extension_id']['notes'] = "[Profile Widget Type](/docs/plugins/extensions/points/cerb.profile.widget/)";
+		$keys['extension_id']['notes'] = "[Profile Widget Type](/docs/plugins/extensions/points/cerb.profile.tab.widget/)";
 		$keys['pos']['notes'] = "The order of the widget on the profile; `0` is first (top-left) proceeding in rows then columns";
 		$keys['profile_tab_id']['notes'] = "The ID of the [profile tab](/docs/records/types/profile_tab/) dashboard containing this widget";
 		$keys['width_units']['notes'] = "`1` (25%), `2` (50%), `3` (75%), `4` (100%)";
