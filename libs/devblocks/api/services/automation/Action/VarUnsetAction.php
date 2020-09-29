e8666440bfa354d2836135dc735204284795eb7d
Jeff Standen <jeff@webgroupmedia.com>
2020-09-28 18:40:15 -0700

    * [Automations/Actions] Implemented the `var:` action in automations. This has actions for `set`, `push`, and `unset`.

---
+++ libs/devblocks/api/services/automation/Action/VarUnsetAction.php
@@ -0,0 +1,73 @@
+<?php
+namespace Cerb\AutomationBuilder\Action;
+
+use DevblocksPlatform;
+use Exception_DevblocksAutomationError;
+
+class VarUnsetAction extends AbstractAction {
+	const ID = 'var.unset';
+	
+	function activate(\DevblocksDictionaryDelegate $dict, array &$node_memory, \CerbAutomationPolicy $policy, string &$error=null) {
+		$validation = DevblocksPlatform::services()->validation();
+		
+		$params = $this->node->getParams($dict);
+		
+		$inputs = $params['inputs'] ?? [];
+		$output = $params['output'] ?? null;
+		
+		try {
+			// Params validation
+			
+			$validation->addField('inputs', 'inputs:')
+				->array()
+				->setRequired(true)
+			;
+			
+			$validation->addField('output', 'output:')
+				->string()
+			;
+			
+			if(false === ($validation->validateAll($params, $error)))
+				throw new Exception_DevblocksAutomationError($error);
+			
+			// Validate input
+			
+			$validation->reset();
+			
+			$validation->addField('key', 'inputs:key:')
+				->string()
+				->setRequired(true)
+			;
+			
+			if(false === ($validation->validateAll($inputs, $error)))
+				throw new Exception_DevblocksAutomationError($error);
+			
+			$result = $dict->unsetKeyPath($inputs['key'], ':');
+			
+			if($output) {
+				$dict->set($output, $result);
+			}
+		
+		} catch (\Exception_DevblocksAutomationError $e) {
+			$error = $e->getMessage();
+			
+			if(null != ($event_error = $this->node->getChildBySuffix(':on_error'))) {
+				if($output) {
+					$dict->set($output, [
+						'error' => $error,
+					]);
+				}
+				
+				return $event_error->getId();
+			}
+			
+			return false;
+		}
+		
+		if(null != ($event_success = $this->node->getChildBySuffix(':on_success'))) {
+			return $event_success->getId();
+		}
+		
+		return $this->node->getParent()->getId();
+	}
+}
