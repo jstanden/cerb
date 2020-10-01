fe0043b557e803d5a5c009663bdc3404db7c4a71
Jeff Standen <jeff@webgroupmedia.com>
2020-09-30 18:47:45 -0700

    * [Automations/Triggers] Implemented the `projectBoard.renderCard` trigger for automations. This is triggered when a card is displayed on a project board. The input is dictionaries for the `board_*`, `card_*`, and current `worker_*`. This uses event handler KATA, so the first matching automation is used. For instance, the match can be based on the record type of the card. The output is sheet KATA describing the card's layout.

---
+++ features/cerb.project_boards/api/automations/triggers/projectBoard.renderCard.php
@@ -0,0 +1,51 @@
+<?php
+class AutomationTrigger_ProjectBoardRenderCard extends Extension_AutomationTrigger {
+	const ID = 'cerb.trigger.projectBoard.renderCard';
+	
+	function renderConfig(Model_Automation $model) {
+		$tpl = DevblocksPlatform::services()->template();
+		$tpl->assign('inputs', $this->getInputsMeta());
+		$tpl->assign('outputs', $this->getOutputsMeta());
+		$tpl->display('devblocks:cerberusweb.core::automations/triggers/config_inputs_outputs.tpl');
+	}
+	
+	function validateConfig(array &$params, &$error=null) {
+		return true;
+	}
+	
+	function getInputsMeta() {
+		return [
+			[
+				'key' => 'board_*',
+				'notes' => 'The project board record. Supports key expansion.',
+			],
+			[
+				'key' => 'card_*',
+				'notes' => 'The card record. Supports key expansion.',
+			],
+			[
+				'key' => 'worker_*',
+				'notes' => 'The active worker record. Supports key expansion.',
+			],
+		];
+	}
+	
+	function getOutputsMeta() {
+		return [
+			'return' => [
+				[
+					'key' => 'sheet',
+					'notes' => 'The sheet for displaying the card.',
+				],
+			]
+		];
+	}
+	
+	public function getEditorToolbarItems(array $toolbar): array {
+		return $toolbar;
+	}
+	
+	public function getAutocompleteSuggestions() : array {
+		return [];
+	}
+}
