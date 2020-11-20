c847cfdac4330d6804e2e6bea281e02ec718ca72
Jeff Standen <jeff@webgroupmedia.com>
2020-11-19 19:02:44 -0800

    [Resources] Refactored the `cerb.resource.map` (Map) resource type.

---
+++ features/cerberusweb.core/api/resources/types/cerb.resource.map.php
@@ -0,0 +1,20 @@
+<?php
+class ResourceType_Map extends Extension_ResourceType {
+	const ID = 'cerb.resource.map';
+	
+	/**
+	 * @param Model_Resource $resource
+	 * @return Model_Resource_ContentData
+	 */
+	function getContentData(Model_Resource $resource) {
+		$content_data = new Model_Resource_ContentData();
+		
+		$content_data->headers = [
+			'Content-Type: application/json; charset=utf-8',
+		];
+		
+		$this->getContentResource($resource, $content_data);
+		
+		return $content_data;
+	}
+}
--- features/cerberusweb.core/api/resources/types/cerb.resource.ui.map.geojson.php
+++
@@ -1,4 +0,0 @@
-<?php
-class ResourceType_MapGeoJson extends Extension_ResourceType {
-	const ID = 'cerb.resource.ui.map.geojson';
-}
