a9c79638ac46bec6927fb00227f069823975f2c8
Jeff Standen <jeff@webgroupmedia.com>
2020-11-20 18:27:06 -0800

    * [Maps/Resources] Included a built-in `mapPoints.worldCapitalCities` resource for the capital cities of the world.

--- features/cerberusweb.core/patches/10.x/10.0.0.php
+++ features/cerberusweb.core/patches/10.x/10.0.0.php
@@ -317,6 +317,7 @@ if(!isset($tables['resource'])) {
 		'map.world.countries.json',
 		'map.country.usa.states.json',
 		'map.country.usa.counties.json',
+		'mapPoints.worldCapitalCities.json',
 	];
 	
 	foreach($resource_files as $resource_file) {
