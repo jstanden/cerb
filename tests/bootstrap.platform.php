126c3031ca01ad48d13dd2ce061cd9b2a0be8c8c
Jeff Standen <jeff@webgroupmedia.com>
2020-12-21 18:56:34 -0800

    [Tests] Updated the tests bootloader with classloading paths for automations. This allows tests to run without a completed installation or database.

--- tests/bootstrap.platform.php
+++ tests/bootstrap.platform.php
@@ -5,3 +5,14 @@ require(APP_PATH . '/api/Application.class.php');
 require(APP_PATH . '/api/app/Mail.php');
 
 DevblocksPlatform::init();
+
+$classloader = DevblocksPlatform::services()->classloader();
+
+$classloader->registerPsr4Path(DEVBLOCKS_PATH . 'api/services/automation/', 'Cerb\\AutomationBuilder\\');
+
+$classloader->registerClasses(APP_PATH . '/features/cerberusweb.core/api/dao/abstract_view.php', [
+	'C4_AbstractView',
+]);
+$classloader->registerClasses(APP_PATH . '/features/cerberusweb.core/api/dao/automation.php', [
+	'Model_Automation',
+]);
