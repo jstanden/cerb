2fb975e8acde4efd08b34358bd643815e1a8b221
Jeff Standen <jeff@webgroupmedia.com>
2020-12-24 17:10:43 -0800

    [Updater] Fixed an issue with the 10.0 update script where built-in automations weren't syncing.

--- features/cerberusweb.core/patches/10.x/10.0.0.php
+++ features/cerberusweb.core/patches/10.x/10.0.0.php
@@ -111,7 +111,7 @@ if(!isset($tables['automation'])) {
 	];
 	
 	foreach($automation_files as $automation_file) {
-		$path = realpath(APP_PATH . '/features/cerberusweb.core/assets/resources/') . '/' . $automation_file;
+		$path = realpath(APP_PATH . '/features/cerberusweb.core/assets/automations/') . '/' . $automation_file;
 		
 		if(!file_exists($path) || false === ($automation_data = json_decode(file_get_contents($path), true)))
 			continue;
