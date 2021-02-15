38649806cde95a1b8531b10a0a380cb2d8e43d44
Jeff Standen <jeff@webgroupmedia.com>
2021-02-15 14:42:34 -0800

    [Platform/OpenID] Refactored our OpenID Connect implementation to be compatible with `lcobucci/jwt` 4.x changes.

--- features/cerberusweb.core/api/service_providers/oidc.php
+++ features/cerberusweb.core/api/service_providers/oidc.php
@@ -1,41 +1,47 @@
 <?php
 use GuzzleHttp\Psr7\Request;
+use Lcobucci\Clock\SystemClock;
+use Lcobucci\JWT\Configuration;
+use Lcobucci\JWT\Signer\Key\InMemory;
+use Lcobucci\JWT\Signer\Rsa\Sha256;
+use Lcobucci\JWT\Token;
+use Lcobucci\JWT\Validation\Validator;
 use League\OAuth2\Client\Grant\AbstractGrant;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use League\OAuth2\Client\Provider\GenericProvider;
 use phpseclib\Crypt\RSA;
 use phpseclib\Math\BigInteger;
-use Lcobucci\JWT\Token;
-use Lcobucci\JWT\ValidationData;
-use Lcobucci\JWT\Parser;
 
 class InvalidTokenException extends \Exception {
 }
 
 class AccessToken extends \League\OAuth2\Client\Token\AccessToken {
-	protected $idToken;
+	protected Token $idToken;
 
 	public function __construct($options = []) {
 		parent::__construct($options);
 
-		if (!empty($this->values['id_token'])) {
-			$this->idToken = (new Parser())->parse($this->values['id_token']);
+		$config = Configuration::forUnsecuredSigner();
+		
+		if(array_key_exists('id_token', $this->values)) {
+			$this->idToken = $config->parser()->parse($this->values['id_token']);
 			unset($this->values['id_token']);
 		}
 	}
 	
-	public function getIdToken() {
+	public function getIdToken() : Token {
 		return $this->idToken;
 	}
 	
-	public function jsonSerialize() {
+	public function jsonSerialize(): array {
 		$parameters = parent::jsonSerialize();
 		
 		if ($this->idToken) {
-			$parameters['id_token'] = (string) $this->idToken;
+			$parameters['id_token'] = $this->idToken->toString();
 		}
 		
 		return $parameters;
-	}	
+	}
 }
 
 class GenericOpenIDConnectProvider extends GenericProvider {
@@ -141,38 +147,38 @@ class GenericOpenIDConnectProvider extends GenericProvider {
 	 * @param  mixed $grant
 	 * @param  array $options
 	 * @return AccessToken
+	 * @throws InvalidTokenException|IdentityProviderException
 	 */
 	public function getAccessToken($grant, array $options = []) {
-		$accessToken = parent::getAccessToken($grant, $options);
+		$accessToken = parent::getAccessToken($grant, $options); /* @var $accessToken AccessToken */
 		$token = $accessToken->getIdToken(); /* @var $token Token */
 		
 		// id_token is empty.
-		if(null === $token) {
+		if(!($token instanceof Token)) {
 			throw new InvalidTokenException('Expected an id_token but did not receive one from the authorization server.');
 		}
 		
-		$signer = new Lcobucci\JWT\Signer\Rsa\Sha256();
-		
 		// Not all ID tokens provide a 'kid' (Key ID) header
-		if($token->hasHeader('kid')) {
-			$kid = $token->getHeader('kid');
+		if($token->headers()->has('kid')) {
+			$kid = $token->headers()->get('kid');
 			
 			if(false == ($public_key = $this->getPublicKeyByJwkId($kid)))
 				throw new InvalidTokenException('Received an invalid key ID (kid) header from authorization server.');
+			
 		} else {
 			// Use the first key if one wasn't specified
 			$public_key = $this->getPublicKeyByDefault();
 		}
+
+		$validation = new Validator();
 		
-		if (false === $token->verify($signer, $public_key)) {
-			throw new InvalidTokenException('Received an invalid id_token from authorization server.');
-		}
-		
-		$data = new ValidationData();
-		$data->setIssuer($this->getIdTokenIssuer());
-		$data->setAudience($this->clientId);
-		
-		if(false === $token->validate($data)) {
+		if(!$validation->validate(
+			$token,
+			new Lcobucci\JWT\Validation\Constraint\SignedWith(new Sha256(), InMemory::plainText($public_key)),
+			new Lcobucci\JWT\Validation\Constraint\StrictValidAt(new SystemClock(new DateTimeZone(\date_default_timezone_get()))),
+			new Lcobucci\JWT\Validation\Constraint\IssuedBy($this->getIdTokenIssuer()),
+			new Lcobucci\JWT\Validation\Constraint\PermittedFor($this->clientId)
+			)) {
 			throw new InvalidTokenException('The id_token did not pass validation.');
 		}
 		
@@ -346,7 +352,7 @@ class ServiceProvider_OpenIdConnect extends Extension_ConnectedServiceProvider {
 		
 		$url_writer = DevblocksPlatform::services()->url();
 		
-		$provider = new GenericOpenIDConnectProvider([
+		return new GenericOpenIDConnectProvider([
 			'clientId' => $service_params['client_id'],
 			'clientSecret' => $service_params['client_secret'],
 			'idTokenIssuer' => $service_params['issuer'],
@@ -356,8 +362,6 @@ class ServiceProvider_OpenIdConnect extends Extension_ConnectedServiceProvider {
 			'urlResourceOwnerDetails' => $service_params['userinfo_url'],
 			'urlJwks' => $service_params['jwks_url'],
 		]);
-		
-		return $provider;
 	}
 	
 	public function sso(Model_ConnectedService $service, array $path) {
@@ -370,7 +374,7 @@ class ServiceProvider_OpenIdConnect extends Extension_ConnectedServiceProvider {
 		if(!array_key_exists('code', $_GET)) {
 			// Send to the authentication URL
 			$redirectUrl = $provider->getAuthorizationUrl();
-			header(sprintf("Location: %s", $redirectUrl, true, 302));
+			header(sprintf("Location: %s", $redirectUrl), true, 302);
 			return;
 		}
 		
@@ -379,19 +383,13 @@ class ServiceProvider_OpenIdConnect extends Extension_ConnectedServiceProvider {
 				'code' => $_GET['code']
 			]);
 			
-		} catch (InvalidTokenException $e) {
-			$query = ['error' => 'auth.failed'];
-			DevblocksPlatform::redirect(new DevblocksHttpResponse(['login'], $query), 0);
-		}
-		
-		$id_token = $token->getIdToken();
-		
-		try {
-			if(false == ($email = $id_token->getClaim('email')))
-				throw new Exception_DevblocksValidationError();
+			$id_token = $token->getIdToken();
+			
+			if(false == ($email = $id_token->claims()->get('email')))
+				throw new Exception_DevblocksValidationError("The ID token does not have an 'email' claim.");
 			
 			if(false == ($worker = DAO_Worker::getByEmail($email)))
-				throw new Exception_DevblocksValidationError();
+				throw new Exception_DevblocksValidationError("The ID token 'email' claim does not match a worker account.");
 			
 			$login_state
 				->clearAuthState()
