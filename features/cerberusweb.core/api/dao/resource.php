96d44b90780f1d86706cfd849fa05b3ddfcdf9d1
Jeff Standen <jeff@webgroupmedia.com>
2021-05-26 17:39:21 -0700

    * [Resources/Storage] Fixed an issue when deleting 'Resource' records where the underlying storage object wasn't removed.

--- features/cerberusweb.core/api/dao/resource.php
+++ features/cerberusweb.core/api/dao/resource.php
@@ -276,6 +276,8 @@ class DAO_Resource extends Cerb_ORMHelper {
 		if(empty($ids))
 			return;
 		
+		Storage_Resource::delete($ids);
+		
 		$ids_list = implode(',', $ids);
 		
 		$db->ExecuteMaster(sprintf("DELETE FROM resource WHERE id IN (%s)", $ids_list));
