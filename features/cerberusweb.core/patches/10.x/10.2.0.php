e88f78355c1838b73b4ed59f88a6b20fbbd51872
Jeff Standen <jeff@webgroupmedia.com>
2021-05-28 14:41:58 -0700

    * [Automations] The `cerb.data.records` (sheet.data) automation now accepts a `query:` input (rather than `required_query:`) and a `query_params:` input for untrusted query placeholder substitution.

--- features/cerberusweb.core/patches/10.x/10.2.0.php
+++ features/cerberusweb.core/patches/10.x/10.2.0.php
@@ -4,6 +4,33 @@ $logger = DevblocksPlatform::services()->log();
 $tables = $db->metaTables();
 
 // ===========================================================================
+// Update built-in automations
+
+$automation_files = [
+	'ai.cerb.automationBuilder.action.function.json',
+	'ai.cerb.automationBuilder.action.httpRequest.json',
+	'ai.cerb.automationBuilder.interaction.worker.await.promptSheet.json',
+	'ai.cerb.editor.mapBuilder.json',
+	'ai.cerb.eventHandler.automation.json',
+	'ai.cerb.toolbarBuilder.interaction.json',
+	'cerb.data.records.json',
+	'cerb.projectBoard.toolbar.task.find.json',
+	'cerb.ticket.move.json',
+	'cerb.ticket.participants.manage.json',
+];
+
+foreach($automation_files as $automation_file) {
+	$path = realpath(APP_PATH . '/features/cerberusweb.core/assets/automations/') . '/' . $automation_file;
+	
+	if(!file_exists($path) || false === ($automation_data = json_decode(file_get_contents($path), true)))
+		continue;
+	
+	DAO_Automation::importFromJson($automation_data);
+	
+	unset($automation_data);
+}
+
+// ===========================================================================
 // Finish up
 
 return TRUE;
