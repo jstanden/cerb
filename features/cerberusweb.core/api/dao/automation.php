8da0475522d5aa35ca53fefa24ef6d04d2d3a5a9
Jeff Standen <jeff@webgroupmedia.com>
2021-08-19 18:55:10 -0700

    10.1: * [Automations/Validation] Before saving an automation record, the script syntax is now validated to catch common errors.
    Fixes #1507

--- features/cerberusweb.core/api/dao/automation.php
+++ features/cerberusweb.core/api/dao/automation.php
@@ -165,6 +165,14 @@ class DAO_Automation extends Cerb_ORMHelper {
 			return false;
 		}
 		
+		if(array_key_exists(DAO_Automation::SCRIPT, $fields)) {
+			$kata = DevblocksPlatform::services()->kata();
+			if(false === $kata->validate($fields[DAO_Automation::SCRIPT], CerberusApplication::kataSchemas()->automation(), $error)) {
+				$error = 'Automation: ' . $error;
+				return false;
+			}
+		}
+
 		if(array_key_exists(DAO_Automation::POLICY_KATA, $fields)) {
 			$kata = DevblocksPlatform::services()->kata();
 			if(false === $kata->validate($fields[DAO_Automation::POLICY_KATA], CerberusApplication::kataSchemas()->automationPolicy(), $error)) {
