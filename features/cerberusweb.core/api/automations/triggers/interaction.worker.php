295240f53639fdb6932f3d2250e1362ac2b62039
Beat Stieger <beat.stieger@vonbesmer.ch>
2022-01-11 17:13:35 +0100

    elements:fileupload
    added missing "required"

--- features/cerberusweb.core/api/automations/triggers/interaction.worker.php
+++ features/cerberusweb.core/api/automations/triggers/interaction.worker.php
@@ -202,6 +202,7 @@ class AutomationTrigger_InteractionWorker extends Extension_AutomationTrigger {
 						'score' => 2000,
 					],
 					'validation@raw:',
+					'required@bool:',
 				],
 			
 				// [TODO] Maps KATA	
