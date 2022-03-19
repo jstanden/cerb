3d1de8cdddd280c6955a6f67d7dc6d582e42b603
Jeff Standen <jeff@webgroupmedia.com>
2022-03-18 18:26:50 -0700

    10.2.3: * [Automations/Scripting] In automation scripting, added a new `|repeat(times)` filter that repeats a string the given number of times. For instance, this is useful to render a 1-5 star rating in a sheet.

--- libs/devblocks/api/services/template_builder.php
+++ libs/devblocks/api/services/template_builder.php
@@ -187,6 +187,7 @@ class _DevblocksTemplateBuilder {
 				'permalink',
 				'quote',
 				'regexp',
+				'repeat',
 				'secs_pretty',
 				'sha1',
 				'spaceless',
@@ -1695,6 +1696,7 @@ class _DevblocksTwigExtensions extends \Twig\Extension\AbstractExtension {
 			new \Twig\TwigFilter('permalink', [$this, 'filter_permalink']),
 			new \Twig\TwigFilter('quote', [$this, 'filter_quote']),
 			new \Twig\TwigFilter('regexp', [$this, 'filter_regexp']),
+			new \Twig\TwigFilter('repeat', [$this, 'filter_repeat']),
 			new \Twig\TwigFilter('secs_pretty', [$this, 'filter_secs_pretty']),
 			new \Twig\TwigFilter('sha1', [$this, 'filter_sha1']),
 			new \Twig\TwigFilter('split_crlf', [$this, 'filter_split_crlf']),
@@ -2045,6 +2047,16 @@ class _DevblocksTwigExtensions extends \Twig\Extension\AbstractExtension {
 		}
 		
 		return $string;
+	}
+	
+	function filter_repeat($string, $times) : string {
+		if($string instanceof Twig\Markup)
+			$string = strval($string);
+		
+		if(!is_string($string))
+			return '';
+		
+		return str_repeat($string, $times);
 	}
 	
 	function filter_secs_pretty($string, $precision=0) {
