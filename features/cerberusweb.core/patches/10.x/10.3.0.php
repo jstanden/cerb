dbd14914e8961e3f66bb4169e78b060d32b64950
Jeff Standen <jeff@webgroupmedia.com>
2022-04-28 18:50:04 -0700

    10.3: * [Scheduler/Storage] In the `cron.storage` scheduler job, optimized the query used to determine when objects can be archived. Previously, this could log slow queries in large databases due to table scans.

--- features/cerberusweb.core/patches/10.x/10.3.0.php
+++ features/cerberusweb.core/patches/10.x/10.3.0.php
@@ -4,6 +4,22 @@ $logger = DevblocksPlatform::services()->log();
 $tables = $db->metaTables();
 
 // ===========================================================================
+// Index attachment storage profiles for cron.storage
+
+if(!isset($tables['attachment']))
+	return FALSE;
+
+list(,$indexes) = $db->metaTable('attachment');
+
+if(!array_key_exists('profile_updated', $indexes)) {
+	$db->ExecuteMaster('ALTER TABLE attachment ADD INDEX `profile_updated` (`storage_extension`,`storage_profile_id`,`updated`)');
+}
+
+if(array_key_exists('storage_profile_id', $indexes)) {
+	$db->ExecuteMaster('ALTER TABLE attachment DROP INDEX storage_profile_id');
+}
+
+// ===========================================================================
 // Add `available_at` to `queue_message`
 
 if(!isset($tables['queue_message']))
