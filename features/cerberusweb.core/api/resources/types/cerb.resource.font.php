58d919d4d9945fea5530bd94114577ce30a1524f
Jeff Standen <jeff@webgroupmedia.com>
2022-10-06 18:10:18 -0700

    10.3.1: * [Resources/Fonts] On 'Font' resource records, loaded TTF fonts are now served from a cache. This makes them much more efficient to use from frequently invoked automations.

--- features/cerberusweb.core/api/resources/types/cerb.resource.font.php
+++ features/cerberusweb.core/api/resources/types/cerb.resource.font.php
@@ -4,6 +4,37 @@ class ResourceType_Font extends Extension_ResourceType {
 	
 	const PARAM_MIME_TYPE = 'mime_type';
 	
+	public static function getTempPathByName(string $resource_key) {
+		// Look up the TTF font
+		if(!($font = \DAO_Resource::getByNameAndType($resource_key, self::ID)))
+			return null;
+		
+		// Use a versioned cache key
+		$font_cache_path = sprintf("%s/font_cache/%d-%d.ttf",
+			APP_TEMP_PATH,
+			$font->id,
+			$font->updated_at
+		);
+		
+		// Check the temp path first
+		if(file_exists($font_cache_path))
+			return $font_cache_path;
+		
+		// Create the font cache if it doesn't exist
+		if(!file_exists(APP_TEMP_PATH . '/font_cache/'))
+			if(!mkdir(APP_TEMP_PATH . '/font_cache/', 0770))
+				return null;
+		
+		if(!($font_data = $font->getExtension()->getContentData($font)))
+			return null;
+		
+		// Write the bytes to the temp path
+		if(!(file_put_contents($font_cache_path, fread($font_data->data, 1_048_576))))
+			return null;
+		
+		return $font_cache_path;
+	}
+	
 	function validateContentData($fp, &$extension_params=[], &$error=null) : bool {
 		if($fp) {
 			$bytes = null;
