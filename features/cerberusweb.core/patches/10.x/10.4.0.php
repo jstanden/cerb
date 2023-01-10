693f33b39b3bf5bf55256489ee2f527a19f19165
Jeff Standen <jeff@webgroupmedia.com>
2023-01-10 15:41:54 -0800

    10.4: [Update] Added the 10.4 database patch.

---
+++ features/cerberusweb.core/patches/10.x/10.4.0.php
@@ -0,0 +1,10 @@
+<?php
+$db = DevblocksPlatform::services()->database();
+$logger = DevblocksPlatform::services()->log();
+$tables = $db->metaTables();
+
+// ===========================================================================
+// Finish up
+
+return TRUE;
+
