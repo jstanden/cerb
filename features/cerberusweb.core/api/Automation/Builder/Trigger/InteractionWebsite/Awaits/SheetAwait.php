4cecc645d3ca8e417380cc904f4229be9b76e5af
Jeff Standen <jeff@webgroupmedia.com>
2025-02-05 15:59:44 -0800

    11.0.4: * [Portals/Website] Fixed an issue with `interaction.website` automations. Paging a sheet always reset its style back to `table`.

--- features/cerberusweb.core/api/Automation/Builder/Trigger/InteractionWebsite/Awaits/SheetAwait.php
+++ features/cerberusweb.core/api/Automation/Builder/Trigger/InteractionWebsite/Awaits/SheetAwait.php
@@ -203,11 +203,15 @@ class SheetAwait extends AbstractAwait {
 		
 		$this->_render($prompt_key, $continuation);
 		
-		$tpl->assign('layout_style', $layout_style);
-		
 		$tpl->assign('sheet_selection_key', uniqid('selection_'));
 		
-		$tpl->display('devblocks:cerberusweb.core::automations/triggers/interaction.website/await/sheet/render.tpl');
+		if('buttons' == $layout_style) {
+			$tpl->display('devblocks:cerberusweb.core::automations/triggers/interaction.website/await/sheet/render_buttons.tpl');
+		} else if('scale' == $layout_style) {
+			$tpl->display('devblocks:cerberusweb.core::automations/triggers/interaction.website/await/sheet/render_scale.tpl');
+		} else {
+			$tpl->display('devblocks:cerberusweb.core::automations/triggers/interaction.website/await/sheet/render.tpl');
+		}
 		
 		if($is_dirty) {
 			DAO_AutomationContinuation::update($continuation->token, [
