4854a0c57a1fc9313b49df733b4f70b87786868a
Jeff Standen <jeff@webgroupmedia.com>
2025-02-21 15:10:32 -0800

    11.1: [Update] Added the 11.1 database patch.

---
+++ features/cerberusweb.core/patches/11.x/11.1.0.php
@@ -0,0 +1,13 @@
+<?php
+/** @noinspection SqlResolve */
+$db = DevblocksPlatform::services()->database();
+$logger = DevblocksPlatform::services()->log();
+$tables = $db->metaTables();
+$revision = $db->GetOneMaster("SELECT revision FROM cerb_patch_history WHERE plugin_id = 'cerberusweb.core'");
+
+
+
+// ===========================================================================
+// Finish up
+
+return TRUE;
