f9e7f887e9144493695a679f2a3f4ab97327f50c
Jeff Standen <jeff@webgroupmedia.com>
2025-02-21 16:34:39 -0800

    11.1: * [Platform/LLM/Groq] Added a `groq` provider to the LLM service. This connects to fast, hosted, open-source large language models through the Groq API.

---
+++ libs/devblocks/api/services/llm/Providers/Groq.php
@@ -0,0 +1,134 @@
+<?php
+namespace Cerb\LLM\Providers;
+
+use DevblocksLlmChatResponse;
+use DevblocksLlmChatResponse_Tool;
+use DevblocksPlatform;
+use Exception_DevblocksAutomationError;
+use Extension_DevblocksLlmMemoryStore;
+use Extension_DevblocksLlmProvider;
+use GuzzleHttp\Psr7\Request;
+
+class Groq extends Extension_DevblocksLlmProvider {
+	const ID = 'groq';
+	
+	/**
+	 * @throws Exception_DevblocksAutomationError
+	 */
+	function __construct(array $params) {
+		parent::__construct($params);
+		
+		if(!$this->getParam('api_endpoint_url'))
+			$this->setParam('api_endpoint_url', 'https://api.groq.com/openai');
+		
+		if(!$this->getParam('model'))
+			throw new Exception_DevblocksAutomationError('llm:inputs:llm:groq:model: is required.');
+	}
+	
+	/**
+	 * @throws Exception_DevblocksAutomationError
+	 */
+	function chatCompletion(array $messages, string $system_prompt, array $tools, Extension_DevblocksLlmMemoryStore $memory) : DevblocksLlmChatResponse {
+		$http = DevblocksPlatform::services()->http();
+		
+		$base_url = rtrim($this->getParam('api_endpoint_url'), '/');
+		$authentication_uri = $this->getParam('authentication', null);
+		
+		$model_messages = $messages;
+		
+		// Always start with the system prompt
+		if($system_prompt) {
+			array_unshift($model_messages,
+				[
+					'role' => 'system',
+					'content' => $system_prompt,
+				]
+			);
+		}
+		
+		$verb = 'POST';
+		$url = $base_url . '/v1/chat/completions';
+		$headers = [
+			'Content-Type' => 'application/json',
+		];
+		$body = json_encode([
+			'model' => $this->getParam('model', ''),
+			'stream' => false,
+			'messages' => $model_messages,
+			'tools' => $tools,
+		]);
+		
+		$request = new Request($verb, $url, $headers, $body);
+		$request_options = [
+			'http_errors' => false,
+		];
+		$error = null;
+		
+		// Authenticate the request if required
+		if($authentication_uri) {
+			if(!$this->_authenticateRequest($authentication_uri, $request, $request_options, $error))
+				throw new Exception_DevblocksAutomationError($error);
+		}
+		
+		if(false === ($response = $http->sendRequest($request, $request_options, $error)))
+			throw new Exception_DevblocksAutomationError($error);
+		
+		if(false === ($response_json = $http->getResponseAsJson($response, $error)))
+			throw new Exception_DevblocksAutomationError($error);
+		
+		if(200 != $response->getStatusCode()) {
+			if($response_json['error']['message'] ?? null)
+				throw new Exception_DevblocksAutomationError($response_json['error']['message']);
+			
+			throw new Exception_DevblocksAutomationError('HTTP status code: ' . $response->getStatusCode());
+		}
+		
+		$message = $response_json['choices'][0]['message'] ?? null;
+		
+		// Add to the memory
+		if($message)
+			$messages = array_merge($messages, [$message]);
+		
+		$chat_response = new DevblocksLlmChatResponse();
+		
+		if($message['content'] ?? null)
+			$chat_response->pushMessage($message['content']);
+		
+		if($message['tool_calls'] ?? null) {
+			foreach($message['tool_calls'] as $tool_call) {
+				if(
+					!($tool_call['function']['name'] ?? null)
+					|| is_null($tool_call['id'] ?? null)
+				) continue;
+				
+				$tool_args = $tool_call['function']['arguments'] ?? [];
+				
+				$tool = new DevblocksLlmChatResponse_Tool(
+					$tool_call['function']['name'] ?? '',
+					is_string($tool_args) ? json_decode($tool_args, true) : $tool_args,
+					$tool_call['id'] ?? null,
+				);
+				
+				$chat_response->pushTool($tool);
+			}
+		}
+		
+		$memory->setMessages($messages);
+		
+		return $chat_response;
+	}
+	
+	function returnTool(DevblocksLlmChatResponse_Tool $tool, string $content, Extension_DevblocksLlmMemoryStore $memory): void {
+		$memory_messages = $memory->getMessages();
+		
+		$tool_message = [
+			'role' => 'tool',
+			'tool_call_id' => $tool->getId(),
+			'content' => $content,
+		];
+		
+		$memory_messages[] = $tool_message;
+		
+		$memory->setMessages($memory_messages);
+	}
+}
