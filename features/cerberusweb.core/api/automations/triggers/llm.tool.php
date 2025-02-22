80e049a4ae0affd78298544ef5ad54e91719c123
Jeff Standen <jeff@webgroupmedia.com>
2025-02-21 16:07:00 -0800

    11.1: * [Automations/LLM] Added an `llm.tool` automation trigger. A collection of these tools can be provided to an `llm.agent:` using a large language model (LLM) that supports function calling (tool use). The inputs of an `llm.tool` automation are used to automatically generate the JSON Schema expected by common models. For instance, tools can provide access to Cerb data or integrate with third party APIs.

---
+++ features/cerberusweb.core/api/automations/triggers/llm.tool.php
@@ -0,0 +1,59 @@
+<?php
+class AutomationTrigger_LlmTool extends Extension_AutomationTrigger {
+	const ID = 'cerb.trigger.llm.tool';
+	
+	function renderConfig(Model_Automation $model) {
+		$tpl = DevblocksPlatform::services()->template();
+		$tpl->assign('inputs', $this->getInputsMeta());
+		$tpl->assign('outputs', $this->getOutputsMeta());
+		$tpl->display('devblocks:cerberusweb.core::automations/triggers/config_inputs_outputs.tpl');
+	}
+	
+	function validateConfig(array &$params, &$error=null) {
+		return true;
+	}
+	
+	function getInputsMeta() {
+		return [];
+	}
+	
+	function getOutputsMeta() {
+		return [
+			'return' => [
+				[
+					'key' => 'content',
+					'notes' => 'The results of the tool execution for the given inputs.',
+					'required' => true,
+				],
+			],
+		];
+	}
+	
+	function getUsageMeta(string $automation_name): array {
+		$results = [];
+		
+		// Automations
+		if(($linked_automations = DAO_Automation::getWhere(sprintf("%s LIKE %s",
+			Cerb_ORMHelper::escape(DAO_Automation::SCRIPT),
+			Cerb_ORMHelper::qstr('%' . $automation_name . '%')
+		)))) {
+			$linked_automations = array_filter($linked_automations, function($w) use ($automation_name) {
+				$tokens = DevblocksPlatform::services()->string()->tokenize($w->script, false);
+				return in_array($automation_name, $tokens);
+			});
+			
+			if($linked_automations)
+				$results['automation'] = array_column($linked_automations, 'id');
+		}
+		
+		return $results;
+	}
+	
+	public function getEditorToolbarItems(array $toolbar): array {
+		return $toolbar;
+	}
+	
+	public function getAutocompleteSuggestions() : array {
+		return [];
+	}
+}
