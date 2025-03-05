4c87a3b850ddc20bd076bb5f31c810ab0dce723a
Jeff Standen <jeff@webgroupmedia.com>
2025-03-05 15:24:36 -0800

    11.0.6: * [Connected Services/Library] Added a 'Together.ai' connected service to the library. This provides access to open source large language model APIs.

--- features/cerberusweb.core/patches/11.x/11.0.0.php
+++ features/cerberusweb.core/patches/11.x/11.0.0.php
@@ -211,6 +211,7 @@ $packages = [
 	'cerb_connected_service_pinecone.json',
 	'cerb_connected_service_slack.json',
 	'cerb_connected_service_tavily.json',
+	'cerb_connected_service_togetherai.json',
 	'cerb_profile_tab_ticket_overview.json',
 	'cerb_profile_widget_ticket_status.json',
 	'cerb_profile_widget_ticket_participants.json',
