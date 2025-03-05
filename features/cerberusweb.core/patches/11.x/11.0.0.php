cadd7f9640975a9ed9ee1067fa075881af49e322
Jeff Standen <jeff@webgroupmedia.com>
2025-03-05 15:23:15 -0800

    11.0.6: * [Connected Services/Library] Added a 'Pinecone' connected service to the library. This provides access to vector database and text embedding APIs.

--- features/cerberusweb.core/patches/11.x/11.0.0.php
+++ features/cerberusweb.core/patches/11.x/11.0.0.php
@@ -208,6 +208,7 @@ $packages = [
 	'cerb_connected_service_ipstack.json',
 	'cerb_connected_service_notion.json',
 	'cerb_connected_service_openweather.json',
+	'cerb_connected_service_pinecone.json',
 	'cerb_connected_service_slack.json',
 	'cerb_profile_tab_ticket_overview.json',
 	'cerb_profile_widget_ticket_status.json',
