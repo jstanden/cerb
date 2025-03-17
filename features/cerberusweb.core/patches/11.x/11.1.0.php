e487289b90e8187cf938b1cb16e9df327ca39d96
Jeff Standen <jeff@webgroupmedia.com>
2025-03-17 15:36:11 -0700

    11.1: [Database/Schema] Reduced the max row size for `worker_view_model` records (8126 bytes).

--- features/cerberusweb.core/patches/11.x/11.1.0.php
+++ features/cerberusweb.core/patches/11.x/11.1.0.php
@@ -52,6 +52,39 @@ if(!isset($tables['llm_agent_message'])) {
 }
 
 // ===========================================================================
+// Resize `worker_view_model`
+
+list($columns, ) = $db->metaTable('worker_view_model');
+
+$changes = [];
+
+if(array_key_exists('class_name', $columns) && 'varchar(128)' != $columns['class_name']['type']) {
+	$changes[] = "MODIFY COLUMN class_name varchar(128) NOT NULL DEFAULT ''";
+}
+
+if(array_key_exists('render_sort_json', $columns) && 'varchar(128)' != $columns['render_sort_json']['type']) {
+	$changes[] = "MODIFY COLUMN render_sort_json varchar(128) NOT NULL DEFAULT ''";
+}
+
+if(array_key_exists('render_subtotals', $columns) && 'varchar(128)' != $columns['render_subtotals']['type']) {
+	$changes[] = "MODIFY COLUMN render_subtotals varchar(128) NOT NULL DEFAULT ''";
+}
+
+if(array_key_exists('render_template', $columns) && 'varchar(64)' != $columns['render_template']['type']) {
+	$changes[] = "MODIFY COLUMN render_template varchar(64) NOT NULL DEFAULT ''";
+}
+
+if(array_key_exists('params_timezone', $columns) && 'varchar(128)' != $columns['params_timezone']['type']) {
+	$changes[] = "MODIFY COLUMN params_timezone varchar(128) NOT NULL DEFAULT ''";
+}
+
+if($changes) {
+	$db->ExecuteMaster("ALTER TABLE worker_view_model ".
+		implode(', ', $changes)
+	);
+}
+
+// ===========================================================================
 // Finish up
 
 return TRUE;
