7d547503773570121a13c625a78c427b99171b72
Jeff Standen <jeff@webgroupmedia.com>
2025-03-26 19:52:54 -0700

    11.1: * [Automations/LLM] In automations, the `llm.agent:` command now returns a `session_id` to its output key. This can be used with the `llmTranscript` form element to render the chat history.

--- libs/devblocks/api/services/automation/Node/LlmAgentNode.php
+++ libs/devblocks/api/services/automation/Node/LlmAgentNode.php
@@ -391,6 +391,7 @@ class LlmAgentNode extends AbstractNode {
 		
 		// Return an abstract list of messages
 		$this->_dict->set($this->_output, [
+			'session_id' => $session_id,
 			'messages' => $llm_response->getMessages(),
 		]);
 		
