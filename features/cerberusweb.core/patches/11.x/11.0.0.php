1db654159214b81dd062bce93d9465e6a3e35832
Jeff Standen <jeff@webgroupmedia.com>
2025-03-26 19:41:00 -0700

    11.0.7: * [Connected Services] Updated the 'Stripe' connected service provider to use bearer tokens rather than legacy http-basic auth. This now only requires the secret key and not the publishable one.

--- features/cerberusweb.core/patches/11.x/11.0.0.php
+++ features/cerberusweb.core/patches/11.x/11.0.0.php
@@ -214,6 +214,7 @@ $packages = [
 	'cerb_connected_service_pinecone.json',
 	'cerb_connected_service_sambanova.json',
 	'cerb_connected_service_slack.json',
+	'cerb_connected_service_stripe.json',
 	'cerb_connected_service_tavily.json',
 	'cerb_connected_service_telegram.json',
 	'cerb_connected_service_togetherai.json',
