68871ae1315b9f9ae0e8f6dd91c84f346e31b52a
Jeff Standen <jeff@webgroupmedia.com>
2026-01-09 18:06:56 -0800

    11.1.6: * [LLM/Agents] In the Anthropic LLM provider, updated the list of available models.

--- api/Extension.class.php
+++ api/Extension.class.php
@@ -1450,12 +1450,15 @@ abstract class Extension_AutomationTrigger extends DevblocksExtension {
 					]
 				],
 				'(.*):llm.agent:inputs:llm:anthropic:model:' => [
-					'claude-3-5-haiku-20241022',
-					'claude-3-5-sonnet-20241022',
-					'claude-3-7-sonnet-20250219',
-					'claude-3-opus-20240229',
-					'claude-sonnet-4-20250514',
+					'claude-opus-4-5-20251101',
+					'claude-haiku-4-5-20251001',
+					'claude-sonnet-4-5-20250929',
+					'claude-opus-4-1-20250805',
 					'claude-opus-4-20250514',
+					'claude-sonnet-4-20250514',
+					'claude-3-7-sonnet-20250219',
+					'claude-3-5-haiku-20241022',
+					'claude-3-haiku-20240307',
 				],
 				'(.*):llm.agent:inputs:llm:aws_bedrock:' => [
 					'anthropic_version: bedrock-2023-05-31',
@@ -1820,12 +1823,15 @@ abstract class Extension_AutomationTrigger extends DevblocksExtension {
 					]
 				],
 				'(.*):llm.chat:inputs:llm:anthropic:model:' => [
-					'claude-3-5-haiku-20241022',
-					'claude-3-5-sonnet-20241022',
-					'claude-3-7-sonnet-20250219',
-					'claude-3-opus-20240229',
-					'claude-sonnet-4-20250514',
+					'claude-opus-4-5-20251101',
+					'claude-haiku-4-5-20251001',
+					'claude-sonnet-4-5-20250929',
+					'claude-opus-4-1-20250805',
 					'claude-opus-4-20250514',
+					'claude-sonnet-4-20250514',
+					'claude-3-7-sonnet-20250219',
+					'claude-3-5-haiku-20241022',
+					'claude-3-haiku-20240307',
 				],
 				'(.*):llm.chat:inputs:llm:aws_bedrock:' => [
 					'anthropic_version: bedrock-2023-05-31',
