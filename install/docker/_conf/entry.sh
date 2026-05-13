e30faec3468ea2fe3e99b56e23789328aa4f8f41
Jeff Standen <jeff@webgroupmedia.com>
2026-05-13 16:04:59 -0700

    11.2: * [Docker] Updated the Dockerfile to Ubuntu 26.04 LTS and PHP 8.5.

--- install/docker/_conf/entry.sh
+++ install/docker/_conf/entry.sh
@@ -3,4 +3,4 @@ chown www-data:www-data /var/www/html/storage \
 && chmod 0770 /var/www/html/storage \
 && envsubst "\$CERB_DB_HOST \$CERB_DB_NAME \$CERB_DB_USER \$CERB_DB_PASS \$CERB_SERVICE_TOKEN \$CERB_SERVICE_TOKEN_SCOPE" \
   < /tmp/cerb.framework.config.template > /var/www/html/framework.config.php \
-&& php-fpm8.3 -F
+&& php-fpm8.5 -F
