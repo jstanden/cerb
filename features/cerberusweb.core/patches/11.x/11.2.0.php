fad1f82ed1deaaca782c0d6caacaa4e3e4a7d98b
Jeff Standen <jeff@webgroupmedia.com>
2026-05-20 00:08:22 -0700

    11.2: * [Installer/UX] Added default card and profile widgets for queue records.
    11.2: * [Installer/UX] Added default card and profile widgets for queue job records.

--- features/cerberusweb.core/patches/11.x/11.2.0.php
+++ features/cerberusweb.core/patches/11.x/11.2.0.php
@@ -1384,6 +1384,128 @@ if(!$db->GetOneMaster("SELECT id FROM profile_tab WHERE context = 'cerb.contexts
 }
 
 // ===========================================================================
+// Default `Properties` card widget for `cerb.contexts.queue`
+
+if(!$db->GetOneMaster("SELECT id FROM card_widget WHERE record_type='cerb.contexts.queue' AND extension_id='cerb.card.widget.fields'")) {
+	$db->ExecuteMaster(sprintf(
+		"INSERT INTO card_widget (name, record_type, extension_id, extension_params_json, created_at, updated_at, pos, width_units, zone) ".
+		"VALUES (%s, %s, %s, %s, %d, %d, %d, %d, %s)",
+		$db->qstr('Properties'),
+		$db->qstr('cerb.contexts.queue'),
+		$db->qstr('cerb.card.widget.fields'),
+		$db->qstr(json_encode([
+			"context" => "cerb.contexts.queue",
+			"context_id" => "{{record_id}}",
+			"properties" => [
+				[
+					"name",
+					"extension_id",
+					"created",
+					"updated",
+					"id",
+				],
+			],
+			"search" => [
+				"context" => ["cerb.contexts.queue.job"],
+				"query" => ["queue:{{record_name}}"],
+				"label_singular" => ["Job"],
+				"label_plural" => ["Jobs"],
+			],
+			"toolbar_kata" => "",
+		])),
+		time(), time(),
+		1, 12,
+		$db->qstr('content')
+	));
+}
+
+// ===========================================================================
+// Default `Overview` profile tab for `cerb.contexts.queue`
+
+if(!$db->GetOneMaster("SELECT id FROM profile_tab WHERE context = 'cerb.contexts.queue'")) {
+	$db->ExecuteMaster(sprintf("INSERT INTO profile_tab (name, context, extension_id, updated_at, extension_params_json, pos) ".
+		"VALUES (%s, %s, %s, %d, %s, %d)",
+		$db->qstr('Overview'),
+		$db->qstr('cerb.contexts.queue'),
+		$db->qstr('cerb.profile.tab.dashboard'),
+		time(),
+		$db->qstr(json_encode([
+			"layout" => "sidebar_left",
+		])),
+		1
+	));
+
+	$new_profile_tab_id = $db->LastInsertId();
+
+	$db->ExecuteMaster(sprintf("INSERT INTO profile_widget (name, profile_tab_id, extension_id, extension_params_json, updated_at, pos, width_units, zone, options_kata) ".
+		"VALUES (%s, %d, %s, %s, %d, %d, %d, %s, %s)",
+		$db->qstr('Queue'),
+		$new_profile_tab_id,
+		$db->qstr('cerb.profile.tab.widget.fields'),
+		$db->qstr(json_encode([
+			"context" => "cerb.contexts.queue",
+			"context_id" => "{{record_id}}",
+			"properties" => [
+				[
+					"name",
+					"extension_id",
+					"created",
+					"updated",
+					"id",
+				],
+			],
+			"toolbar_kata" => "",
+		])),
+		time(),
+		1,
+		4,
+		$db->qstr('sidebar'),
+		$db->qstr('')
+	));
+
+	$db->ExecuteMaster(sprintf("INSERT INTO profile_widget (name, profile_tab_id, extension_id, extension_params_json, updated_at, pos, width_units, zone, options_kata) ".
+		"VALUES (%s, %d, %s, %s, %d, %d, %d, %s, %s)",
+		$db->qstr('Jobs'),
+		$new_profile_tab_id,
+		$db->qstr('cerb.profile.tab.widget.worklist'),
+		$db->qstr(json_encode([
+			"context" => "cerb.contexts.queue.job",
+			"query_required" => "queue:{{record_name}}",
+			"query" => "sort:-created",
+			"render_limit" => "25",
+			"header_color" => "#6a87db",
+			"columns" => [
+				"qj_name",
+				"qj_status_id",
+				"qj_created_at",
+			],
+		])),
+		time(),
+		2,
+		4,
+		$db->qstr('content'),
+		$db->qstr('')
+	));
+
+	$db->ExecuteMaster(sprintf("INSERT INTO profile_widget (name, profile_tab_id, extension_id, extension_params_json, updated_at, pos, width_units, zone, options_kata) ".
+		"VALUES (%s, %d, %s, %s, %d, %d, %d, %s, %s)",
+		$db->qstr('Discussion'),
+		$new_profile_tab_id,
+		$db->qstr('cerb.profile.tab.widget.comments'),
+		$db->qstr(json_encode([
+			"context" => "cerb.contexts.queue",
+			"context_id" => "{{record_id}}",
+			"height" => "",
+		])),
+		time(),
+		3,
+		4,
+		$db->qstr('content'),
+		$db->qstr('')
+	));
+}
+
+// ===========================================================================
 // Add `retention_days` to `metric`
 
 list($columns, ) = $db->metaTable('metric');
