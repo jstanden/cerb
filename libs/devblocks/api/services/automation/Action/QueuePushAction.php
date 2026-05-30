61a44c52516cf5cc11858bc4cbbb70d71a4dd871
Jeff Standen <jeff@webgroupmedia.com>
2026-05-29 19:02:24 -0700

    11.2: * [Automations/Queues] In automations, the `output:` parameter of `queue.push:` is now optional.

--- libs/devblocks/api/services/automation/Action/QueuePushAction.php
+++ libs/devblocks/api/services/automation/Action/QueuePushAction.php
@@ -28,7 +28,6 @@ class QueuePushAction extends AbstractAction {
 			
 			$validation->addField('output', 'output:')
 				->string()
-				->setRequired(true)
 				;
 			
 			if(false === ($validation->validateAll($params, $error)))
